405193d2186f0ac634b8416ed631e536fef410d3
Jin <quaggan02@gmail..com>
2014-08-16 02:19:38 +0900

    기존 작성하던 Obj Importer 제거 및 tiny obj loader 추가 re #17

--- SOCEngine/Importer/ObjImporter.h
+++ SOCEngine/Importer/ObjImporter.h
@@ -12,22 +12,8 @@ namespace Importer
 {
 	class ObjImporter
 	{
-	private:
-		struct ObjTriangle
-		{
-			int vertex[3];
-			int normal[3];
-			int uvs[3];
-		};
-
 	public:
 		ObjImporter();
 		~ObjImporter();
-
-	private:
-		bool LoadMaterials(const std::string& fileName, const std::string& fileFolderPath, Rendering::Material::MaterialManager& materialMgr, Rendering::Texture::TextureManager& textureMgr);
-
-	public:
-		bool Load(const std::string& fileName, const std::string& folderPath, Rendering::Material::MaterialManager& materialMgr, Rendering::Texture::TextureManager& textureMgr);
 	};
 }
