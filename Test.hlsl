3d7d8ea300bbc90985e9213dd198e4dea7d04218
Jin02 <quaggan02@gmail.com>
2014-08-20 14:25:12 +0900

    1. .gitignore 업데이트 2. 프로젝트 분할함 3. sponza 추가, 용량이 크므로 텍스쳐는 포함하지 않음

---
+++ Test.hlsl
@@ -0,0 +1,33 @@
+
+cbuffer Transform : register( b0 )
+{
+	matrix world;
+	matrix view;
+	matrix proj;
+};
+
+struct VS_INPUT
+{
+	float4 Pos : POSITION;
+};
+
+struct PS_INPUT
+{
+	float4 Pos : SV_POSITION;
+};
+
+PS_INPUT VS( VS_INPUT input )
+{
+	PS_INPUT ps;
+	ps.Pos = mul( input.Pos, world );
+	ps.Pos = mul( ps.Pos, view);
+	ps.Pos = mul( ps.Pos, proj);
+	//ps.Pos = input.Pos;
+
+    return ps;
+}
+
+float4 PS( PS_INPUT input ) : SV_Target
+{
+    return float4( 1.0f, 0.0f, 0.0f, 1.0f );    // Yellow, with Alpha = 1
+}
