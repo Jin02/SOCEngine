d863f8233fa6ffb39d5e3716abf389a18b668ed4
Jin <quaggan02@gmail.com>
2014-10-30 02:13:51 +0900

    1. ShaderFactoryTemplate에 쉽게 inputElemnt 추가하는 함수 추가 2. 스크립트에 함수 인자 입력 추가 및 함수 체크 추가 3. 1번으로 인하여 스크립트 코드 정리
    re #23

--- SOCEngine/Template/ShaderFactoryTemplate.h
+++ SOCEngine/Template/ShaderFactoryTemplate.h
@@ -26,6 +26,24 @@ public:
 		std::string folderPath = "";
 		std::vector<D3D11_INPUT_ELEMENT_DESC> vertexDeclations;
 		D3D11_INPUT_ELEMENT_DESC desc;
+		auto AddInputElementDesc = [&](const std::string& semanticName, uint semanticIndex, DXGI_FORMAT format, uint alignedByteOffset, D3D11_INPUT_CLASSIFICATION inputSlotClass, uint inputSlot, uint instanceDataStepRate)
+		{
+			typedef unsigned int uint;
+			auto MakeInputElementDesc = [&](D3D11_INPUT_ELEMENT_DESC& out)
+			{
+				out.SemanticName = semanticName;
+				out.SemanticIndex = semanticIndex;
+				out.AlignedByteOffset = alignedByteOffset;
+				out.Format = format;
+				out.InputSlotClass = inputSlotClass;
+				out.InputSlot = inputSlot;
+				out.InstanceDataStepRate = instanceDataStepRate;
+			}
+		
+			D3D11_INPUT_ELEMENT_DESC desc;
+			MakeInputElementDesc(desc);
+			vertexDeclations.push_back(desc);
+		};
 		/** Script Begin **/
 		/** Script End **/
 		const std::string baseCommand = shaderName+":";
