fdfe90ee1420bd170a7bae229f37ea1eee198f94
Jin <quaggan02@gmail.com>
2014-12-03 23:27:53 +0900

    1. multisampler 2. disableculling state 3. depth buffer 세팅
    #32

--- SOCEngine/Platform/DirectX/DirectX.cpp
+++ SOCEngine/Platform/DirectX/DirectX.cpp
@@ -6,35 +6,13 @@ using namespace Device;
 DirectX::DirectX(void) :
 	_device(nullptr), _swapChain(nullptr), _immediateContext(nullptr),
 	_depthStencil(nullptr), _depthStencilView(nullptr),
-	_renderTargetView(nullptr), _rasterizerState(nullptr),
-	_multiSample(1)
+	_renderTargetView(nullptr), _disableCulling(nullptr), _depthStencilSRV(nullptr)
 {
 
 }
 
 DirectX::~DirectX(void)
 {
-}
-
-bool DirectX::CreateRasterize()
-{
-	//D3D11_RASTERIZER_DESC rsState;
-	//rsState.FillMode = D3D11_FILL_SOLID;
-	//rsState.CullMode = D3D11_CULL_BACK;
-	//rsState.FrontCounterClockwise = true;
-	//rsState.DepthBias = 0;
-	//rsState.DepthBiasClamp = 0;
-	//rsState.SlopeScaledDepthBias = 0;
-	//rsState.DepthClipEnable = false;
-	//rsState.ScissorEnable = false;
-	//rsState.MultisampleEnable = _multiSample > 1;
-	//rsState.AntialiasedLineEnable = false;
-
-	//if( FAILED(_device->CreateRasterizerState(&rsState, &_rasterizerState)) )
-	//	return false;
-
-	//_immediateContext->RSSetState( _rasterizerState );
-	return true;
 }
 
 bool DirectX::CreateRenderTargetView()
@@ -55,72 +33,66 @@ bool DirectX::CreateRenderTargetView()
 	return true;
 }
 
-bool DirectX::CreateDefaultDepthStencilView(const Math::Size<int>& winSize)
+bool DirectX::CreateDepthStencilRenderTarget(const Math::Size<int>& winSize, IDXGISwapChain* swapChain)
 {
 	D3D11_TEXTURE2D_DESC descDepth;
 	memset(&descDepth, 0, sizeof(D3D11_TEXTURE2D_DESC));
-	descDepth.Width = winSize.w;
-	descDepth.Height = winSize.h;
-	descDepth.MipLevels = 1;
-	descDepth.ArraySize = 1;
-	descDepth.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
-	descDepth.SampleDesc.Count = 1;
-	descDepth.SampleDesc.Quality = 0;
-	descDepth.Usage = D3D11_USAGE_DEFAULT;
-	descDepth.BindFlags = D3D11_BIND_DEPTH_STENCIL;
-	descDepth.CPUAccessFlags = 0;
-	descDepth.MiscFlags = 0;
+	descDepth.Width					= winSize.w;
+	descDepth.Height				= winSize.h;
+	descDepth.MipLevels				= 1;
+	descDepth.ArraySize				= 1;
+	descDepth.Format				= DXGI_FORMAT_R32_TYPELESS;
+	descDepth.Usage					= D3D11_USAGE_DEFAULT;
+	descDepth.BindFlags				= D3D11_BIND_DEPTH_STENCIL | D3D11_BIND_SHADER_RESOURCE;
+	descDepth.CPUAccessFlags		= 0;
+	descDepth.MiscFlags				= 0;
+
+	//multisampler
+	{
+		DXGI_SWAP_CHAIN_DESC swapChainDesc;
+		swapChain->GetDesc(&swapChainDesc);
+		descDepth.SampleDesc.Count		= swapChainDesc.SampleDesc.Count;
+		descDepth.SampleDesc.Quality	= swapChainDesc.SampleDesc.Quality;
+	}
 
 	HRESULT hr = _device->CreateTexture2D( &descDepth, nullptr, &_depthStencil );
 	if( FAILED( hr ) )
+	{
+		ASSERT("not create depthStencil Texture");
 		return false;
+	}
 
 	D3D11_DEPTH_STENCIL_VIEW_DESC descDSV;
 	memset(&descDSV, 0, sizeof(D3D11_DEPTH_STENCIL_VIEW_DESC));
+	descDSV.Format = DXGI_FORMAT_D32_FLOAT;
 
-	switch( descDepth.Format )
-	{
-	case DXGI_FORMAT_R8_TYPELESS:
-		descDSV.Format = DXGI_FORMAT_R8_UNORM;
-		break;
-	case DXGI_FORMAT_R16_TYPELESS:
-		descDSV.Format = DXGI_FORMAT_D16_UNORM;
-		break;
-	case DXGI_FORMAT_R32_TYPELESS:
-		descDSV.Format = DXGI_FORMAT_D32_FLOAT;
-		break;
-	case DXGI_FORMAT_R24G8_TYPELESS:
-		descDSV.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
-		break;
-	default:
-		descDSV.Format = descDepth.Format;
-	}
-
-	descDSV.ViewDimension = _multiSample > 1 ? 
-D3D11_DSV_DIMENSION_TEXTURE2DMS : D3D11_DSV_DIMENSION_TEXTURE2D;
+	descDSV.ViewDimension = descDepth.SampleDesc.Count > 1 ? D3D11_DSV_DIMENSION_TEXTURE2DMS : D3D11_DSV_DIMENSION_TEXTURE2D;
 	descDSV.Texture2D.MipSlice = 0;
 
 	hr = _device->CreateDepthStencilView( _depthStencil, &descDSV, &_depthStencilView );
 	if( FAILED(hr) )
+	{
+		ASSERT("Not Create Depth Stencil View");
 		return false;
+	}
 
-	//D3D11_DEPTH_STENCIL_DESC dsState;
-	//memset(&dsState, 0, sizeof(D3D11_DEPTH_STENCIL_DESC));
-	//dsState.DepthEnable = true;
-	//dsState.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
-	//dsState.DepthFunc = D3D11_COMPARISON_LESS;
-	//dsState.StencilEnable = false;
+	D3D11_SHADER_RESOURCE_VIEW_DESC srdesc;
+	srdesc.Format = DXGI_FORMAT_R32_FLOAT;
+	srdesc.ViewDimension = descDepth.SampleDesc.Count > 1 ? D3D11_SRV_DIMENSION_TEXTURE2DMS : D3D11_SRV_DIMENSION_TEXTURE2D;
+	srdesc.Texture2D.MostDetailedMip = 0;
+	srdesc.Texture2D.MipLevels = 1;
 
-	//ID3D11DepthStencilState* depthStencilState = nullptr;
-	//if( FAILED(_device->CreateDepthStencilState( &dsState, &depthStencilState)) )
-	//	return false;
-
-	//_immediateContext->OMSetDepthStencilState(depthStencilState, 0);
+	hr = _device->CreateShaderResourceView(_depthStencil, &srdesc, &_depthStencilSRV);
+	if( FAILED(hr) )
+	{
+		ASSERT("Error");
+		return false;
+	}
 
 	return true;
 }
 
-bool DirectX::CreateDeviceAndSwapChain(const Win32* win)
+bool DirectX::CreateDeviceAndSwapChain(const Win32* win, const DXGI_SAMPLE_DESC* multiSampler)
 {
 	//swapChain setting
 	DXGI_SWAP_CHAIN_DESC	sd;
@@ -138,13 +110,22 @@ bool DirectX::CreateDeviceAndSwapChain(const Win32* win)
 	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
 	sd.OutputWindow = win->GetHandle();
 	sd.Windowed = win->GetIsWindowMode();
-	//sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
-	//sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+	sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
 
 	//msaa
-	sd.SampleDesc.Count = 1;
-	sd.SampleDesc.Quality = 0;
-
+	if(multiSampler == nullptr)
+	{
+		sd.SampleDesc.Count = 1;
+		sd.SampleDesc.Quality = 0;
+	}
+	else
+	{
+		HRESULT hr = _device->CheckMultisampleQualityLevels( sd.BufferDesc.Format, sd.SampleDesc.Count, &sd.SampleDesc.Quality);
+		if( FAILED(hr) || sd.SampleDesc.Quality <= 0)
+			ASSERT("Invalid multisampler count");
+		sd.SampleDesc.Quality -= 1;
+	}
 
 	D3D_DRIVER_TYPE driverTypes[] =
 	{
@@ -201,20 +182,22 @@ bool DirectX::CreateViewport(const Math::Size<int>& winSize)
 	return true;
 }
 
-void DirectX::CalcMaxMultiSampler(unsigned int& outAbleMultiSample, unsigned int& outQualityLevel, DXGI_FORMAT format)
+void DirectX::CheckAbleMultiSampler(std::vector<DXGI_SAMPLE_DESC>& outDescs, DXGI_FORMAT format)
 {
-	unsigned int qualityLevel = 0;
 	unsigned int ableMultiSample = D3D11_MAX_MULTISAMPLE_SAMPLE_COUNT;
 	for(ableMultiSample; ableMultiSample >= 0; ableMultiSample/=2)
 	{
-		HRESULT hr = _device->CheckMultisampleQualityLevels( format, ableMultiSample, &qualityLevel);
+		DXGI_SAMPLE_DESC desc;
+		HRESULT hr = _device->CheckMultisampleQualityLevels( format, ableMultiSample, &desc.Quality);
 
-		if( SUCCEEDED(hr) )
+		if( FAILED(hr) )
 			break;
-	}
 
-	outAbleMultiSample = ableMultiSample;
-	outQualityLevel = qualityLevel;
+		desc.Count = ableMultiSample;
+		desc.Quality -= 1;
+
+		outDescs.push_back(desc);
+	}
 }
 
 bool DirectX::InitDevice(const Win32* win)
@@ -225,7 +208,7 @@ bool DirectX::InitDevice(const Win32* win)
 	if( CreateRenderTargetView() == false )
 		return false;
 
-	if( CreateDefaultDepthStencilView(win->GetSize()) == false)
+	if( CreateDepthStencilRenderTarget(win->GetSize(), _swapChain) == false)
 		return false;
 
 	_immediateContext->OMSetRenderTargets(1, &_renderTargetView, _depthStencilView);
@@ -233,8 +216,23 @@ bool DirectX::InitDevice(const Win32* win)
 
 	CreateViewport(win->GetSize());
 
-	if( CreateRasterize() == false )
-		return false;
+	//Create disableCulling rasterizer State
+	{
+		D3D11_RASTERIZER_DESC rsState;
+		rsState.FillMode				= D3D11_FILL_SOLID;
+		rsState.CullMode				= D3D11_CULL_NONE;		//ÄÃ¸µ ²û
+		rsState.FrontCounterClockwise	= true;
+		rsState.DepthBias				= 0;
+		rsState.DepthBiasClamp			= 0.0f;
+		rsState.SlopeScaledDepthBias	= 0.0f;
+		rsState.DepthClipEnable			= true;
+		rsState.ScissorEnable			= false;
+		rsState.MultisampleEnable		= false;
+		rsState.AntialiasedLineEnable	= false;
+
+		if( FAILED(_device->CreateRasterizerState(&rsState, &_disableCulling)) )
+			return false;
+	}
 
 	return true;
 }
--- SOCEngine/Platform/DirectX/DirectX.h
+++ SOCEngine/Platform/DirectX/DirectX.h
@@ -3,6 +3,7 @@
 #include <D3D11.h>
 #include <D3DX11.h>
 #include <D3Dcompiler.h>
+#include <vector>
 
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "d3dx11.lib")
@@ -16,35 +17,32 @@ namespace Device
 	class DirectX
 	{
 	private:
-		ID3D11Device			*_device;
-		IDXGISwapChain			*_swapChain;
-		ID3D11DeviceContext		*_immediateContext;
-		//IDXGIAdapter			*_adapter;
+		ID3D11Device				*_device;
+		IDXGISwapChain				*_swapChain;
+		ID3D11DeviceContext			*_immediateContext;
 
-		ID3D11Texture2D			*_depthStencil;
-		ID3D11DepthStencilView	*_depthStencilView;
+		ID3D11Texture2D				*_depthStencil;
+		ID3D11DepthStencilView		*_depthStencilView;
+		ID3D11ShaderResourceView	*_depthStencilSRV;
 
 		ID3D11RenderTargetView	*_renderTargetView;
-		ID3D11RasterizerState	*_rasterizerState;
+		ID3D11RasterizerState	*_disableCulling;
 
 		D3D_FEATURE_LEVEL		 _featureLevel;
 		D3D_DRIVER_TYPE			 _driverType;
-
-	private:
-		int _multiSample;
 
 	public:
 		DirectX();
 		~DirectX(void);
 
 	private:
-		bool CreateRasterize();
 		bool CreateRenderTargetView();
-		bool CreateDefaultDepthStencilView(const Math::Size<int>& winSize);
-		bool CreateDeviceAndSwapChain(const Win32* win);
+		bool CreateDepthStencilRenderTarget(const Math::Size<int>& winSize, IDXGISwapChain* swapChain);
+		bool CreateDeviceAndSwapChain(const Win32* win, const DXGI_SAMPLE_DESC* multiSampler = nullptr);
 		bool CreateViewport(const Math::Size<int>& winSize);
 		bool CreateSwapChain(const Win32* win);
-		void CalcMaxMultiSampler(unsigned int& outAbleMultiSample, unsigned int& outQualityLevel, DXGI_FORMAT format);		
+	
+		void CheckAbleMultiSampler(std::vector<DXGI_SAMPLE_DESC>& outDescs, DXGI_FORMAT format);		
 
 	public:
 		bool InitDevice(const Win32* win);
@@ -60,6 +58,5 @@ namespace Device
 		GET_ACCESSOR(DepthStencilView, ID3D11DepthStencilView*, _depthStencilView);
 
 		GET_ACCESSOR(RenderTarget, ID3D11RenderTargetView*, _renderTargetView);
-		GET_ACCESSOR(RasterizerState, ID3D11RasterizerState*, _rasterizerState);
 	};
 }
