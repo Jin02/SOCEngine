5c45e1390ded005226a7d30d4d8b498e80573eca
Jin <quaggan02@gmail.com>
2014-12-13 17:20:39 +0900

    ComputeShader 기본 기능 구현 #18

--- SOCEngine/SOCEngine/GPGPU/ComputeShader.h
+++ SOCEngine/SOCEngine/GPGPU/ComputeShader.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "BaseShader.h"
+#include "CSInputBuffer.h"
+#include "CSOutputBuffer.h"
 
 namespace GPGPU
 {
@@ -8,14 +10,46 @@ namespace GPGPU
 	{
 		class ComputeShader : public Rendering::Shader::BaseShader
 		{
+		public:
+			struct InputBuffer
+			{
+				unsigned int idx;
+				CSInputBuffer* buffer;
+				InputBuffer() : idx(0), buffer(nullptr){}
+				~InputBuffer(){}
+			};
+			struct OutputBuffer
+			{
+				unsigned int idx;
+				CSOutputBuffer* buffer;
+				OutputBuffer() : idx(0), buffer(nullptr){}
+				~OutputBuffer(){}
+			};
+			struct ThreadGroup
+			{
+				unsigned int x, y, z;
+				ThreadGroup() : x(0), y(0), z(0){}
+				~ThreadGroup(){}
+			};
+
 		private:
-			ID3D11ComputeShader*	_shader;
+			std::vector<InputBuffer>	_inputBuffers;
+			std::vector<OutputBuffer>	_outputBuffers;
+			ID3D11ComputeShader*		_shader;
+			ThreadGroup					_threadGroup;
 
 		public:
-			ComputeShader(ID3DBlob* blob);
+			ComputeShader(const ThreadGroup& threadGroup, ID3DBlob* blob);
 			~ComputeShader(void);
+
 		public:
 			bool Create();
+			void Dispatch(ID3D11DeviceContext* context);
+
+		public:
+			GET_ACCESSOR(ThreadGroupInfo, const ThreadGroup&, _threadGroup);
+			GET_SET_ACCESSOR(InputBuffers, const std::vector<InputBuffer>&, _inputBuffers);
+			GET_SET_ACCESSOR(OutputBuffers, const std::vector<OutputBuffer>&, _outputBuffers);
 		};
 	}
 }
