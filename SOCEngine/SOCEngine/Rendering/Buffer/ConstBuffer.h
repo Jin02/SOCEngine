ee2932633b41d767a229b0fb15da36ab688ccd25
Jin <quaggan02@gmail.com>
2014-12-13 17:19:13 +0900

    ConstBuffer 기능을 최소화 #18

--- SOCEngine/SOCEngine/Rendering/Buffer/ConstBuffer.h
+++ SOCEngine/SOCEngine/Rendering/Buffer/ConstBuffer.h
@@ -7,26 +7,14 @@ namespace Rendering
 	namespace Buffer
 	{
 		class ConstBuffer : public BaseBuffer
-		{	
-		public:
-			enum Usage
-			{
-				VertexShader,
-				PixelShader,
-				Staging
-			};
-
+		{
 		public:
 			ConstBuffer();
 			~ConstBuffer();
 
-		private:
-			bool CreateUsageVSPS(unsigned int size);
-			bool CreateUsageStaging(unsigned int size, unsigned int stride);
-
 		public:
-			bool Create(unsigned int size, unsigned int stride, Usage usage, const void* sysMem = nullptr);
-			void UpdateSubresource(ID3D11DeviceContext* context, const void* data);
+			bool Create(unsigned int size);
 		};
+
 	}
 }
