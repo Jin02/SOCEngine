bbaa35c0bee48d43cc1d52f731c9cc88f823d992
Jin <quaggan02@gmail.com>
2014-12-14 15:10:48 +0900

    PixelShader에서 불필요한 구조 제거 #18

--- SOCEngine/SOCEngine/Rendering/Shader/PixelShader.h
+++ SOCEngine/SOCEngine/Rendering/Shader/PixelShader.h
@@ -18,9 +18,10 @@ namespace Rendering
 
 		public:
 			bool CreateShader();
-
+			
 			typedef std::pair<int, const Sampler*> SamplerType;
-			void UpdateShader(ID3D11DeviceContext* context, const std::vector<BufferType>* rendererConstBuffers, const std::vector<BufferType>* materialConstBuffers, const std::vector<TextureType>* textures, const SamplerType& sampler);
+			void UpdateShader(ID3D11DeviceContext* context, const std::vector<BufferType>* constBuffers, const std::vector<TextureType>* textures, const std::vector<SamplerType>& samplers);
+
 			void ClearResource(ID3D11DeviceContext* context, const std::vector<TextureType>* textures);
 		};
 	}
