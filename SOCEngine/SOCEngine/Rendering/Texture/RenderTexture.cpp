2b64cac0bc78a284bf68f1bd31b9fc21354f1bab
Jin <quaggan02@gmail.com>
2014-12-20 21:19:40 +0900

    RenderTexture 정리 #37

--- SOCEngine/SOCEngine/Rendering/Texture/RenderTexture.cpp
+++ SOCEngine/SOCEngine/Rendering/Texture/RenderTexture.cpp
@@ -1,6 +1,6 @@
 #include "RenderTexture.h"
-#include "DirectX.h"
-#include "Common.h"
+#include "Director.h"
+#include "DepthBuffer.h"
 
 using namespace Rendering::Texture;
 using namespace Device;
@@ -12,51 +12,88 @@ RenderTexture::RenderTexture() :_texture(nullptr), _renderTargetView(nullptr), _
 
 RenderTexture::~RenderTexture()
 {
+	Destroy();
 }
 
-bool RenderTexture::Create(const DirectX* dx, int width, int height)
+bool RenderTexture::_Create(const Math::Size<unsigned int>& size, DXGI_FORMAT format, unsigned int bindFlags, bool depthBuffer)
 {
+	const DirectX* dx = Device::Director::GetInstance()->GetDirectX();
 	ID3D11Device* device = dx->GetDevice();
+
 
 	D3D11_TEXTURE2D_DESC textureDesc;
 	memset(&textureDesc, 0, sizeof(D3D11_TEXTURE2D_DESC));
 
 	// Setup the render target texture description.
-	textureDesc.Width = width;
-	textureDesc.Height = height;
+	textureDesc.Width = size.w;
+	textureDesc.Height = size.h;
 	textureDesc.MipLevels = 1;
 	textureDesc.ArraySize = 1;
-	textureDesc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
-	textureDesc.SampleDesc.Count = 1;
+	auto GetDepthBufferTexDesc = [](DXGI_FORMAT foramt)
+	{
+		switch( foramt )
+		{
+		case DXGI_FORMAT_D32_FLOAT:
+			return DXGI_FORMAT_R32_TYPELESS;
+		case DXGI_FORMAT_D24_UNORM_S8_UINT:
+			return DXGI_FORMAT_R24G8_TYPELESS;
+		case DXGI_FORMAT_D16_UNORM:
+			return DXGI_FORMAT_R16_TYPELESS;
+		}
+
+		return DXGI_FORMAT_UNKNOWN;
+	};
+	textureDesc.Format = depthBuffer ?  GetDepthBufferTexDesc(format) : format;
 	textureDesc.Usage = D3D11_USAGE_DEFAULT;
-	textureDesc.BindFlags = D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE;
+	textureDesc.BindFlags = bindFlags | D3D11_BIND_SHADER_RESOURCE;
 	textureDesc.CPUAccessFlags = 0;
 	textureDesc.MiscFlags = 0;
 
-	if(FAILED(device->CreateTexture2D(&textureDesc, NULL, &_texture)))
+	//multisampler
+	{
+		DXGI_SWAP_CHAIN_DESC swapChainDesc;
+		dx->GetSwapChain()->GetDesc(&swapChainDesc);
+		textureDesc.SampleDesc.Count	= swapChainDesc.SampleDesc.Count;
+		textureDesc.SampleDesc.Quality	= swapChainDesc.SampleDesc.Quality;
+	}
+
+	HRESULT hr = device->CreateTexture2D(&textureDesc, NULL, &_texture);
+	if(FAILED(hr))
+	{
+		ASSERT("Error, not create texture");
 		return false;
+	}
+
+	D3D11_SHADER_RESOURCE_VIEW_DESC srdesc;
+	srdesc.Format = format;
+	srdesc.ViewDimension = textureDesc.SampleDesc.Count > 1 ? D3D11_SRV_DIMENSION_TEXTURE2DMS : D3D11_SRV_DIMENSION_TEXTURE2D;
+	srdesc.Texture2D.MostDetailedMip = 0;
+	srdesc.Texture2D.MipLevels = 1;
+
+	hr = device->CreateShaderResourceView(_texture, &srdesc, &_shaderResourceView);
+	if( FAILED(hr) )
+	{
+		ASSERT("Error");
+		return false;
+	}
 
 	D3D11_RENDER_TARGET_VIEW_DESC renderTargetViewDesc;
-	renderTargetViewDesc.Format = textureDesc.Format;
+	renderTargetViewDesc.Format = format;
 	renderTargetViewDesc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
 	renderTargetViewDesc.Texture2D.MipSlice = 0;
 
 	if(FAILED(device->CreateRenderTargetView(_texture, &renderTargetViewDesc, &_renderTargetView)))
+	{
+		ASSERT("E");
 		return false;
-
-	D3D11_SHADER_RESOURCE_VIEW_DESC shaderResourceViewDesc;
-
-	// Setup the description of the shader resource view.
-	shaderResourceViewDesc.Format = textureDesc.Format;
-	shaderResourceViewDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
-	shaderResourceViewDesc.Texture2D.MostDetailedMip = 0;
-	shaderResourceViewDesc.Texture2D.MipLevels = 1;
-
-	// Create the shader resource view.
-	if(FAILED(device->CreateShaderResourceView(_texture, &shaderResourceViewDesc, &_shaderResourceView)))
-		return false;
+	}
 
 	return true;
+}
+
+bool RenderTexture::Create(const Math::Size<unsigned int>& size)
+{
+	return _Create(size, DXGI_FORMAT_R32G32B32A32_FLOAT, D3D11_BIND_RENDER_TARGET, false);
 }
 
 void RenderTexture::Destroy()
@@ -66,21 +103,22 @@ void RenderTexture::Destroy()
 	SAFE_RELEASE(_shaderResourceView);
 }
 
-void RenderTexture::SetRenderTarget(const DirectX* dx)
-{		
-	dx->GetContext()->OMSetRenderTargets(1, &_renderTargetView, dx->GetDepthStencilView());
+void RenderTexture::SetRenderTarget(const DepthBuffer* depthBuffer, const DirectX* dx)
+{
+	if(dx == nullptr)
+		dx = Director::GetInstance()->GetDirectX();
+
+	dx->GetContext()->OMSetRenderTargets(1, &_renderTargetView, depthBuffer->GetDepthStencilView());
 }
 
-void RenderTexture::Clear(const DirectX* dx, const Rendering::Color& color)
+void RenderTexture::Clear(const Rendering::Color& color, const DirectX* dx)
 {
+	if(dx == nullptr)
+		dx = Director::GetInstance()->GetDirectX();
+
 	ID3D11DeviceContext* context = dx->GetContext();
 
 	// Clear the back buffer.
 	const float colorValue[] = {color.r, color.g, color.b, 1.0f};
 	context->ClearRenderTargetView(_renderTargetView, colorValue);
-
-	// Clear the depth buffer.
-	context->ClearDepthStencilView(dx->GetDepthStencilView(), D3D11_CLEAR_DEPTH, 1.0f, 0);
-
-	return;
 }
--- SOCEngine/SOCEngine/Rendering/Texture/RenderTexture.h
+++ SOCEngine/SOCEngine/Rendering/Texture/RenderTexture.h
@@ -2,6 +2,7 @@
 
 #include <d3d11.h>
 #include "Color.h"
+#include "Size.h"
 
 namespace Device
 {
@@ -12,9 +13,10 @@ namespace Rendering
 {
 	namespace Texture
 	{
+		class DepthBuffer;
 		class RenderTexture
 		{
-		private:
+		protected:
 			ID3D11Texture2D*			_texture;
 			ID3D11RenderTargetView*		_renderTargetView;
 			ID3D11ShaderResourceView*	_shaderResourceView;
@@ -23,11 +25,19 @@ namespace Rendering
 			RenderTexture();
 			~RenderTexture();
 
+		protected:
+			bool _Create(const Math::Size<unsigned int>& size, DXGI_FORMAT format, unsigned int bindFlags, bool depthBuffer);
+
 		public:
-			bool Create(const Device::DirectX* dx, int width, int height);
+			bool Create(const Math::Size<unsigned int>& size);
 			void Destroy();
-			void SetRenderTarget(const Device::DirectX* dx);
-			void Clear(const Device::DirectX* dx, const Color& color);
+			void SetRenderTarget(const DepthBuffer* depthBuffer, const Device::DirectX* dx = nullptr);
+			void Clear(const Color& color, const Device::DirectX* dx = nullptr);
+
+		public:
+			GET_ACCESSOR(Texture, ID3D11Texture2D*, _texture);
+			GET_ACCESSOR(ShaderResourceView, ID3D11ShaderResourceView*, _shaderResourceView);
+			GET_ACCESSOR(RenderTargetView, ID3D11RenderTargetView*, _renderTargetView);
 		};
 	}
 }
