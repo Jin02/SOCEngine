f47be8961a7ab7340ebb441684c948d0cfc2cd16
Jin <quaggan02@gmail.com>
2015-01-06 00:24:26 +0900

    Light - 이전 코드 제거 #37

--- SOCEngine/SOCEngine/Rendering/Light/LightForm.cpp
+++ SOCEngine/SOCEngine/Rendering/Light/LightForm.cpp
@@ -7,12 +7,6 @@ namespace Rendering
 	{
 		LightForm::LightForm( )
 		{
-			range = 10.0f;
-			specularPower = 20.0f;
-
-			ambient = Color(0.1f, 0.1f, 0.1f);
-			diffuse = Color::white();
-			specular = Color::white();
 		}
 
 		LightForm::~LightForm()
--- SOCEngine/SOCEngine/Rendering/Light/SpotLight.cpp
+++ SOCEngine/SOCEngine/Rendering/Light/SpotLight.cpp
@@ -20,17 +20,18 @@ namespace Rendering
 
 		bool SpotLight::Intersects(Intersection::Sphere &sphere)
 		{
-			float angle = spotAngle;
+			//float angle = spotAngle;
 
-			if(angle < 1)				angle = 1;
-			else if(angle > 179)		angle = 179;
+			//if(angle < 1)				angle = 1;
+			//else if(angle > 179)		angle = 179;
 
-			Math::Vector3 wp;
-			_owner->GetTransform()->WorldPosition(wp);
-			
-			Cone cone(angle, range, _owner->GetTransform()->GetForward(), wp);
+			//Math::Vector3 wp;
+			//_owner->GetTransform()->WorldPosition(wp);
+			//
+			//Cone cone(angle, range, _owner->GetTransform()->GetForward(), wp);
 
-			return cone.Intersects(sphere);
+			//return cone.Intersects(sphere);
+			return false;
 		}
 	}
 }
