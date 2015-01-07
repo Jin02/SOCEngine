2d932f99d2fbb53fd32a59bef901888e8d112101
Jin02 <quaggan02@gmail.com>
2015-01-08 02:01:31 +0900

    LightCulling - InputTexture 추가. depthBuffer 연결 시킴. 아직 Output쪽은 작업 중

--- SOCEngine/SOCEngine/Rendering/Light/Culling/LightCulling.cpp
+++ SOCEngine/SOCEngine/Rendering/Light/Culling/LightCulling.cpp
@@ -10,6 +10,22 @@ using namespace GPGPU::DirectCompute;
 
 #define TILE_SIZE 16
 
+enum InputBuffer : unsigned int
+{
+	PointLightRadiusWithCenter	= 0,
+	SpotLightRadiusWithCenter	= 1
+};
+
+enum InputTexture : unsigned int
+{
+	LinearDepthBuffer = 2
+};
+
+enum OutputBuffer : unsigned int
+{
+	LightIndexBuffer = 0
+};
+
 LightCulling::LightCulling() : _computeShader(nullptr), _globalDataBuffer(nullptr)
 {
 
@@ -21,8 +37,9 @@ LightCulling::~LightCulling()
 	SAFE_DELETE(_globalDataBuffer);
 }
 
-void LightCulling::Init(const std::string& folderPath, const std::string& fileName)
+void LightCulling::Init(const std::string& folderPath, const std::string& fileName, const Texture::RenderTexture* linearDepth)
 {
+	Director* director = Director::GetInstance();
 	Scene* scene = Director::GetInstance()->GetCurrentScene();
 	auto shaderMgr = scene->GetShaderManager();
 
@@ -45,7 +62,7 @@ void LightCulling::Init(const std::string& folderPath, const std::string& fileNa
 		{
 			CSInputBuffer* pointLightCenterWithRadius = new CSInputBuffer;
 			assert(pointLightCenterWithRadius->Create(sizeof(Math::Vector4), POINT_LIGHT_LIMIT_NUM));
-			inputBufferElement.idx = 0;
+			inputBufferElement.idx = InputBuffer::PointLightRadiusWithCenter;
 			inputBufferElement.buffer = pointLightCenterWithRadius;
 			_inputBuffers.push_back(inputBufferElement);
 		}
@@ -54,27 +71,32 @@ void LightCulling::Init(const std::string& folderPath, const std::string& fileNa
 		{
 			CSInputBuffer* spotLightCenterWithRadius = new CSInputBuffer;
 			assert(spotLightCenterWithRadius->Create(sizeof(Math::Vector4), POINT_LIGHT_LIMIT_NUM));
-			inputBufferElement.idx = 1;
+			inputBufferElement.idx = InputBuffer::SpotLightRadiusWithCenter;
 			inputBufferElement.buffer = spotLightCenterWithRadius;
 			_inputBuffers.push_back(inputBufferElement);
 		}
 
 		// depth buffer
-		{			
-			inputBufferElement.idx = 2;
-			inputBufferElement.buffer = nullptr;
+		{
+			ComputeShader::InputTexture inputTex;
+			inputTex.idx		= InputTexture::LinearDepthBuffer;
+			inputTex.texture	= linearDepth;
+			_inputTextures.push_back(inputTex);
 		}
 
 		_computeShader->SetInputBuffers(_inputBuffers);
+		_computeShader->SetInputTextures(_inputTextures);
 	}
 
 	// Ouput Buffer Setting
 	{
-		CSOutputBuffer* lightIndexBuffer = new CSOutputBuffer;
+		CSOutputBuffer_ReadCPU* lightIndexBuffer = new CSOutputBuffer_ReadCPU;
+		Math::Size<unsigned int> size = director->GetWindowSize();
+
 		assert(lightIndexBuffer->Create(0, 0));
 		ComputeShader::OutputBuffer outputBuffer;
 		{
-			outputBuffer.idx = 0;
+			outputBuffer.idx = OutputBuffer::LightIndexBuffer;
 			outputBuffer.buffer = lightIndexBuffer;
 		}
 		_outputBuffers.push_back(outputBuffer);
@@ -82,7 +104,7 @@ void LightCulling::Init(const std::string& folderPath, const std::string& fileNa
 	}
 }
 
-void LightCulling::UpdateBuffer(const CullingConstBuffer& cbData, const std::array<Math::Vector4, POINT_LIGHT_LIMIT_NUM>& pointLightCenterWithRadius, const std::array<Math::Vector4, SPOT_LIGHT_LIMIT_NUM>& spotLightCenterWithRadius, const Texture::RenderTexture* linearDepth)
+void LightCulling::UpdateInputBuffer(const CullingConstBuffer& cbData, const std::array<Math::Vector4, POINT_LIGHT_LIMIT_NUM>& pointLightCenterWithRadius, const std::array<Math::Vector4, SPOT_LIGHT_LIMIT_NUM>& spotLightCenterWithRadius)
 {
 	const Director* director = Device::Director::GetInstance();
 	ID3D11DeviceContext* context = director->GetDirectX()->GetContext();
@@ -96,10 +118,6 @@ void LightCulling::UpdateBuffer(const CullingConstBuffer& cbData, const std::arr
 		_globalDataBuffer->Update(context, &cbData);
 		_inputBuffers[0].buffer->Update(context, pointLightCenterWithRadius.data());
 		_inputBuffers[1].buffer->Update(context, spotLightCenterWithRadius.data());
-	}
-
-	// Depth Buffer Setting
-	{
 	}
 }
 
--- SOCEngine/SOCEngine/Rendering/Light/Culling/LightCulling.h
+++ SOCEngine/SOCEngine/Rendering/Light/Culling/LightCulling.h
@@ -36,6 +36,10 @@ namespace Rendering
 			Rendering::Buffer::ConstBuffer*			_globalDataBuffer;
 
 			std::vector<GPGPU::DirectCompute::ComputeShader::InputBuffer>	_inputBuffers;
+
+			//현재 하나만 사용. 추후 Blended용 버퍼 혹은 다양한 것들이 추가 예정
+			std::vector<GPGPU::DirectCompute::ComputeShader::InputTexture>	_inputTextures;
+
 			std::vector<GPGPU::DirectCompute::ComputeShader::OutputBuffer>	_outputBuffers;
 
 		public:
@@ -43,8 +47,8 @@ namespace Rendering
 			~LightCulling();
 
 		public:
-			void Init(const std::string& folderPath, const std::string& fileName);
-			void UpdateBuffer(const CullingConstBuffer& cbData, const std::array<Math::Vector4, POINT_LIGHT_LIMIT_NUM>& pointLightCenterWithRadius, const std::array<Math::Vector4, SPOT_LIGHT_LIMIT_NUM>& spotLightCenterWithRadius, const Texture::RenderTexture* linearDepth);
+			void Init(const std::string& folderPath, const std::string& fileName, const Texture::RenderTexture* linearDepth);
+			void UpdateInputBuffer(const CullingConstBuffer& cbData, const std::array<Math::Vector4, POINT_LIGHT_LIMIT_NUM>& pointLightCenterWithRadius, const std::array<Math::Vector4, SPOT_LIGHT_LIMIT_NUM>& spotLightCenterWithRadius);
 			void Dispatch(ID3D11DeviceContext* context);
 
 		private:
