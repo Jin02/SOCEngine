77342e31f2296e5af8adebe16047337d6081393b
Jin <quaggan02@gmail.com>
2015-08-16 18:46:30 +0900

    1. VectorHashMap 추가 2. LightManager     1. Light 버퍼를 쉽게 관리하려고 해둔 VectorMap을 VectorHashMap으로 변경(생각보다 검색할 양과 횟수가 많을 것 같다)     2. Transform과 연계하여 사용할 UpdateCounter들 추가(d, s, p Light 모두)     3. UpdateBuffer 실행시, transform에서 사용되는 counter와 이 객체의 counter를 이용하여 일을 좀 덜하게 만듬 -_ 속도는 거의 차이가 없다.
    #67

---
+++ SOCEngine/SOCEngine/Structure/VectorHashMap.h
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <vector>
+#include <functional>
+#include <hash_map>
+#include "Utility.h"
+
+#include "Common.h"
+
+namespace Structure
+{
+	template <typename Key, class Object>
+	class VectorHashMap
+	{
+	protected:
+		std::vector<Object>										_vector;
+
+		//pair second value is vector index
+		std::hash_map<Key, std::pair<Object*, uint>>			_map;
+
+	public:
+		VectorHashMap(void){}
+		virtual ~VectorHashMap(void)
+		{
+			DeleteAll();
+		}
+	
+	public:
+		void Add(const Key& key, Object& object)
+		{
+			_vector.push_back(object);
+
+			std::pair<Object*, uint> p;
+			p.first = &object;
+			p.second = _vector.size() - 1;
+
+			_map.insert(std::make_pair(key, p));
+		}
+
+		Object* Find(const Key& key)
+		{
+			std::hash_map<Key, std::pair<Object*, uint>>::iterator iter = _map.find(key);
+			return iter == _map.end() ? nullptr :iter->second.first;
+		}
+
+		Object& Get(unsigned int index)
+		{
+			ASSERT_COND_MSG(index < _vector.size(), "Out index");
+			return _vector[index];
+		}
+
+		void Delete(const Key& key)
+		{
+			std::hash_map<Key, std::pair<Object*, uint>>::iterator iter = _map.find(key);
+			
+			if( iter == _map.end() )
+				return;
+
+			_vector.erase(_vector.begin() + iter->second.second);
+			_map.erase(iter);
+		}
+
+		void DeleteAll()
+		{
+			_vector.clear();
+			_map.clear();
+		}
+
+		
+		void Iterate(const std::function<void(const Key& key, Object* obj)>& recvFunc) const
+		{
+			for(auto iter = _map.cbegin(); iter != _map.cend(); ++iter)
+				recvFunc(iter.first, iter.second.first);
+		}
+		
+		void IterateContent(const std::function<void(Object* obj)>& recvFunc) const
+		{
+			for(auto iter = _vector.cbegin(); iter != _vector.cend(); ++iter)
+				recvFunc( (*iter) );
+		}
+
+		GET_ACCESSOR(Vector, const std::vector<Object>&, _vector);
+		inline std::hash_map<Key, Object*>& GetHashMap() const { return _map; }
+
+		GET_ACCESSOR(Size, unsigned int, _vector.size());
+	};
+}
