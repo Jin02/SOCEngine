3c402f70b27418c13ed603964e94342d65bdf493
Jin <quaggan02@gmail.com>
2015-08-16 20:07:25 +0900

    PhysicallyBasedMaterial - 1. GBufferParam에 mainColor 변경(r,g,b만 처리) 2. Texture -> Texture2D
    #65

--- SOCEngine/SOCEngine/Rendering/Material/PhysicallyBasedMaterial.h
+++ SOCEngine/SOCEngine/Rendering/Material/PhysicallyBasedMaterial.h
@@ -21,7 +21,8 @@ namespace Rendering
 	public:
 		struct GBufferParam
 		{
-			Color			mainColor;
+			struct MainColor { float r, g, b; };
+			MainColor		mainColor;
 			float			metallic;
 			float			roughness;
 			float			fresnel0;
@@ -44,13 +45,13 @@ namespace Rendering
 	public:
 		void UpdateMainColor(const Color& color);
 
-		void UpdateDiffuseMap(const Rendering::Texture::Texture* tex);
-		void UpdateNormalMap(const Rendering::Texture::Texture* tex);
-		void UpdateSpecularMap(const Rendering::Texture::Texture* tex);
-		void UpdateOpacityMap(const Rendering::Texture::Texture* tex);
+		void UpdateDiffuseMap(const Rendering::Texture::Texture2D* tex);
+		void UpdateNormalMap(const Rendering::Texture::Texture2D* tex);
+		void UpdateSpecularMap(const Rendering::Texture::Texture2D* tex);
+		void UpdateOpacityMap(const Rendering::Texture::Texture2D* tex);
 
 	public:
-		GET_ACCESSOR_MATERIAL(MainColor,		Color);
+		GET_ACCESSOR_MATERIAL(MainColor,		GBufferParam::MainColor);
 		GET_SET_ACCESSOR_MATERIAL(Metallic,		float);
 		GET_SET_ACCESSOR_MATERIAL(Roughness,	float);
 		GET_SET_ACCESSOR_MATERIAL(Fresnel_0,	float);
