77a51e876a72fff371dd0db9a94a4be53ec6fb0a
Jin <quaggan02@gmail.com>
2015-08-22 01:29:01 +0900

    CSInputBuffer -> ShaderResourceBuffer 이름 변경 #65

---
+++ SOCEngine/SOCEngine/Rendering/Buffer/ShaderResourceBuffer.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "BaseShader.h"
+#include "BaseBuffer.h"
+
+namespace Rendering
+{
+	namespace Buffer
+	{
+		class ShaderResourceBuffer : public Rendering::Buffer::BaseBuffer
+		{
+		private:
+			ID3D11ShaderResourceView*	_srv;
+
+		public:
+			ShaderResourceBuffer();
+			virtual ~ShaderResourceBuffer();
+
+		public:
+			void Initialize(uint stride, uint num, DXGI_FORMAT format, const void* sysMem, D3D11_USAGE usage = D3D11_USAGE_DEFAULT);
+
+		public:
+			GET_ACCESSOR(ShaderResourceView, ID3D11ShaderResourceView* const*, &_srv);
+		};
+	}
+}
