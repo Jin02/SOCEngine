6dc6b1c9cb3b403d35e56542e43d5e0c00441df6
Jin <quaggan02@gmail.com>
2015-08-28 01:20:01 +0900

    OffScreen - 불필요한 코드 제거 #64

--- SOCEngine/SOCEngine/Rendering/Shading/OffScreen.cpp
+++ SOCEngine/SOCEngine/Rendering/Shading/OffScreen.cpp
@@ -4,6 +4,7 @@
 using namespace Rendering::DeferredShading;
 using namespace Rendering::PostProcessing;
 using namespace Rendering::Shader;
+using namespace GPGPU::DirectCompute;
 
 OffScreen::OffScreen()
 	: FullScreen()
@@ -14,32 +15,17 @@ OffScreen::~OffScreen()
 {
 }
 
-void OffScreen::Initialize(const Rendering::Texture::RenderTexture* offScreenRT)
+void OffScreen::Initialize(const CSRWTexture* offScreen)
 {
-	_offScreenRT = offScreenRT;
-	FullScreen::Initialize("DeferredMainRT", "PS");
+	FullScreen::Initialize("DeferredMainOffScreen", "PS");
 
 	std::vector<BaseShader::TextureType> inputTextures;
 	{
-		BaseShader::TextureType offScreen(0, offScreenRT, false, false, false, true);
-		inputTextures.push_back(offScreen);
+		auto texture = offScreen->GetRenderTexture();
+
+		BaseShader::TextureType input(0, texture, false, false, false, true);
+		inputTextures.push_back(input);
 	}
 
 	SetInputPSTextures(inputTextures);
-}
-
-void OffScreen::Render()
-{
-	if(_offScreenRT)
-		FullScreen::Render(_offScreenRT);
-	else
-	{
-		DEBUG_LOG("OffScreen is null. Plz Check your code");
-	}
-}
-
-void OffScreen::Destroy()
-{
-	_offScreenRT = nullptr;
-	FullScreen::Destroy();
 }
--- SOCEngine/SOCEngine/Rendering/Shading/OffScreen.h
+++ SOCEngine/SOCEngine/Rendering/Shading/OffScreen.h
@@ -1,24 +1,20 @@
 #pragma once
 
 #include "FullScreen.h"
+#include "CSRWTexture.h"
 
 namespace Rendering
 {
 	namespace DeferredShading
 	{
-		class OffScreen : private PostProcessing::FullScreen
+		class OffScreen : public PostProcessing::FullScreen
 		{
-		private:
-			const Rendering::Texture::RenderTexture* _offScreenRT;
-
 		public:
 			OffScreen();
 			virtual ~OffScreen();
 
 		public:
-			void Initialize(const Rendering::Texture::RenderTexture* offScreenRT);
-			void Render();
-			void Destroy();
+			void Initialize(const GPGPU::DirectCompute::CSRWTexture* offScreen);
 		};
 	}
 }
