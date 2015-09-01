57f86ef44a0926a6f319efab48f597580f181606
Jin <quaggan02@gmail.com>
2015-09-02 05:31:05 +0900

    1. GlobalSetting -> RenderingCommon으로 변경 2. Rendering::RenderType 추가
    #64

---
+++ SOCEngine/SOCEngine/Rendering/RenderingCommon.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define DEPRECATED_MESH_RENDERER
+
+#define TEMP_FONT_DATA_PATH "../../SOCEngine/SOCEngine/Rendering/UI/Resource/fontdata.txt"
+#define TEMP_FONT_TEXTURE_PATH "../../SOCEngine/SOCEngine/Rendering/UI/Resource/font.dds"
+
+namespace Rendering
+{
+	enum class RenderType
+	{
+		TBDR,
+		ForwardPlus
+	};
+}
