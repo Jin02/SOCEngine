da8ce44eb3e7176803f3b754a1862917fd075a06
Jin <quaggan02@gmail.com>
2015-09-06 05:28:50 +0900

    FinalSceneMerger 구현 중 #64

--- SOCEngine/SOCEngine/ShaderCodes/FinalScreenMerger.hlsl
+++ SOCEngine/SOCEngine/ShaderCodes/FinalScreenMerger.hlsl
@@ -3,7 +3,12 @@
 #include "FullScreenShader.h"
 
 SamplerState renderSceneSamplerState	: register( s0 );
-Texture2D<float4> renderScene			: register( t0 );
+
+#if (MSAA_SAMPLES_COUNT > 1)
+Texture2DMS<float4, MSAA_SAMPLES_COUNT>	renderScene		: register( t0 );
+#else
+Texture2D<float4>						renderScene		: register( t0 );
+#endif
 
 #if defined(USE_UI)
 SamplerState uiSceneSamplerState		: register( s1 );
@@ -13,7 +18,12 @@ Texture2D<float4> uiScene				: register( t1 );
 float4 PS( PS_INPUT input ) : SV_Target
 {
 	float4 finalColor = float4(0.0f, 0.0f, 0.0f, 0.0f);
+#if (MSAA_SAMPLES_COUNT > 1)
+	int2 texCoord = int2((int)(input.uv.x * 0), (int)(input.uv.y * 0));
+	float4 renderSceneColor = renderScene.Load(texCoord.xy, input.sampleIdx);
+#else
 	float4 renderSceneColor = renderScene.Sample(renderSceneSamplerState, input.uv);
+#endif
 
 #if defined(USE_UI)
 	float4 uiSceneColor = uiScene.Sample(uiSceneSamplerState, input.uv);
