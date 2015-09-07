8de73834fefb7a855704ee9a8c13477556468f67
Jin <quaggan02@gmail.com>
2015-09-07 15:15:44 +0900

    RenderingCommon.h 제거, vcxproj 업데이트 #64

--- SOCEngine/SOCEngine/UI/FontLoader.h
+++ SOCEngine/SOCEngine/UI/FontLoader.h
@@ -6,6 +6,8 @@
 #include "Singleton.h"
 
 #define BASIC_FONT_COUNT 96
+#define TEMP_FONT_DATA_PATH "../../SOCEngine/SOCEngine/Rendering/UI/Resource/fontdata.txt"
+#define TEMP_FONT_TEXTURE_PATH "../../SOCEngine/SOCEngine/Rendering/UI/Resource/font.dds"
 
 namespace UI
 {
