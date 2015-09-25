4da300e2e8047fdb76c4ee90572fa67cd8fa35f2
Jin <quaggan02@gmail.com>
2015-09-25 22:52:53 +0900

    1. g3djImporter를 구현 중. VB, IB, Material 처리는 끝냄. 2. Object Hierachy 구현 중
    #85

--- SOCEngine/SOCEngine/Importer/MeshImporter.cpp
+++ SOCEngine/SOCEngine/Importer/MeshImporter.cpp
@@ -1,12 +1,27 @@
 #include "MeshImporter.h"
 #include "Utility.h"
 #include <fstream>
+#include "ResourceManager.h"
+#include "PhysicallyBasedMaterial.h"
+#include "Director.h"
+#include "Scene.h"
+#include "RenderManager.h"
+#include "ShaderForm.h"
+#include "BufferManager.h"
+#include "ImporterUtility.h"
 
 using namespace Importer;
 using namespace Core;
 using namespace Utility;
 using namespace rapidjson;
 using namespace Math;
+using namespace Resource;
+using namespace Device;
+using namespace Rendering::Manager;
+using namespace Rendering::Texture;
+using namespace Rendering::Shader;
+using namespace Rendering::Buffer;
+using namespace Rendering;
 
 MeshImporter::MeshImporter()
 {
@@ -127,9 +142,9 @@ void MeshImporter::ParseNode(Node& outNodes, const rapidjson::Value& node)
 	outNodes = currentNode;
 }
 
-void MeshImporter::ParseMaterial(Material& outMaterial, const rapidjson::Value& matNode)
+void MeshImporter::ParseMaterial(Importer::Material& outMaterial, const rapidjson::Value& matNode)
 {
-	Material material;
+	Importer::Material material;
 
 	material.id = matNode["id"].GetString();
 	auto ParseVec3Types = [](Vector3& out, const Value& matValue, const std::string& name)
@@ -163,7 +178,19 @@ void MeshImporter::ParseMaterial(Material& outMaterial, const rapidjson::Value& 
 			Material::Texture texInfo;
 			texInfo.id			= texInfoNode["id"].GetString();
 			texInfo.fileName	= texInfoNode["filename"].GetString();
-			texInfo.type		= texInfoNode["type"].GetString();
+
+			const std::string& type = texInfoNode["type"].GetString();
+			if(type == "DIFFUSE")			texInfo.type = Material::Texture::Type::Diffuse;
+			else if(type == "AMBIENT")		texInfo.type = Material::Texture::Type::Ambient;
+			else if(type == "NORMAL" || 
+				type == "BUMP")				texInfo.type = Material::Texture::Type::Normal;
+			else if(type == "EMISSIVE")		texInfo.type = Material::Texture::Type::Emissive;
+			else if(type == "Reflection")	texInfo.type = Material::Texture::Type::Reflection;
+			else if(type == "SHININESS")	texInfo.type = Material::Texture::Type::Shininess;
+			else if(type == "TRANSPARENCY")	texInfo.type = Material::Texture::Type::Transparency;
+			else if(type == "SPECULAR")		texInfo.type = Material::Texture::Type::Specular;
+			else							texInfo.type = Material::Texture::Type::Undefined;
+
 			outMaterial.textures.push_back(texInfo);
 		}
 	}
@@ -171,7 +198,7 @@ void MeshImporter::ParseMaterial(Material& outMaterial, const rapidjson::Value& 
 	outMaterial = material;
 }
 
-void MeshImporter::ParseMesh(Mesh& outMesh, const rapidjson::Value& meshNode)
+void MeshImporter::ParseMesh(Importer::Mesh& outMesh, const rapidjson::Value& meshNode)
 {
 	// Setting Attributes
 	{
@@ -214,7 +241,7 @@ void MeshImporter::ParseMesh(Mesh& outMesh, const rapidjson::Value& meshNode)
 	}
 }
 
-void MeshImporter::ParseJson(std::vector<Mesh>& outMeshes, std::vector<Material>& outMaterials, std::vector<Node>& outNodes, const char* buffer)
+void MeshImporter::ParseJson(std::vector<Importer::Mesh>& outMeshes, std::vector<Importer::Material>& outMaterials, std::vector<Node>& outNodes, const char* buffer)
 {
 	Document document;
 	document.Parse(buffer);
@@ -257,12 +284,12 @@ void MeshImporter::ParseJson(std::vector<Mesh>& outMeshes, std::vector<Material>
 	}
 }
 
-void MeshImporter::ParseBinary(std::vector<Mesh>& outMeshes, std::vector<Material>& outMaterials, std::vector<Node>& outNodes, const void* buffer, uint size)
+void MeshImporter::ParseBinary(std::vector<Importer::Mesh>& outMeshes, std::vector<Importer::Material>& outMaterials, std::vector<Node>& outNodes, const void* buffer, uint size)
 {
 	ASSERT_MSG("can't supported format");
 }
 
-Object* MeshImporter::Load(const std::string& fileDir, Rendering::Material::Type materialType, bool isDynamicMesh)
+Object* MeshImporter::Load(const std::string& fileDir, bool useDynamicVB, bool useDynamicIB, Rendering::Material::Type materialType)
 {
 	std::string fileName, fileExtension, folderDir;
 	if( String::ParseDirectory(fileDir, folderDir, fileName, fileExtension) == false )
@@ -304,6 +331,419 @@ Object* MeshImporter::Load(const std::string& fileDir, Rendering::Material::Type
 
 	delete buffer;
 
+	BuildMesh(meshes, materials, nodes, fileName, useDynamicVB, useDynamicIB);
 
 	return nullptr;
+}
+
+Core::Object* MeshImporter::BuildMesh(std::vector<Importer::Mesh>& meshes, const std::vector<Importer::Material>& materials, const std::vector<Node>& nodes, const std::string& meshFileName, bool useDynamicVB, bool useDynamicIB)
+{
+	std::set<std::string> normalMapMaterialKeys;
+	MakeMaterials(normalMapMaterialKeys, materials, meshFileName);
+
+	BufferManager* bufferMgr = ResourceManager::GetInstance()->GetBufferManager();
+	MaterialManager* materialManager = Director::GetInstance()->GetCurrentScene()->GetMaterialManager();
+
+	// key is meshPartId, second value is materialId
+	std::hash_map<std::string, std::string> meshMaterialIdInAllParts;
+	//auto FetchAllPartsInHashMap = [&]()
+	{
+		for(auto iter = nodes.begin(); iter != nodes.end(); ++iter)
+			FetchAllPartsInHashMap_Recursive(meshMaterialIdInAllParts, *iter);
+	};
+
+	// Setting VB, IB
+	{
+		uint meshIterIdx = 0;
+		for(auto meshIter = meshes.begin(); meshIter != meshes.end(); ++meshIter, ++meshIterIdx)
+		{
+			std::vector<std::string> meshPartIdKeys;
+
+			// Make IndexBuffer
+			{
+				const auto& parts = meshIter->parts;
+				for(auto partsIter = parts.begin(); partsIter != parts.end(); ++partsIter)
+				{
+					const auto& indices = partsIter->indices;
+					IndexBuffer* indexBuffer = new IndexBuffer;
+
+					bool success = indexBuffer->Initialize(indices.data(), sizeof(ENGINE_INDEX_TYPE) * indices.size(), useDynamicIB);
+					ASSERT_COND_MSG(success, "Error, Can't create index buffer");
+
+					bufferMgr->Add(meshFileName, partsIter->meshPartId, indexBuffer);
+
+					meshPartIdKeys.push_back(partsIter->meshPartId);
+				}
+			}
+
+			bool hasNormalMap = false;
+			for(auto iter = meshPartIdKeys.begin(); iter != meshPartIdKeys.end(); ++iter)
+			{
+				auto findIter = meshMaterialIdInAllParts.find(*iter);
+
+				if(findIter == meshMaterialIdInAllParts.end())
+					ASSERT_MSG("Error, Invalid meshPartId");
+
+				const std::string& materialId	= findIter->second;
+				auto normalMapMatFindIter		= normalMapMaterialKeys.find(materialId);
+
+				if(normalMapMatFindIter != normalMapMaterialKeys.end())
+				{
+					hasNormalMap = true;
+					break;
+				}
+			}
+
+			std::vector<VertexBuffer::SemanticInfo> semantics; // attributes
+			// Setting VB
+			{
+				auto& attributes = meshIter->attributes;
+
+				uint stride	= 0;
+				uint uv0Pos	= 0;
+				{
+					uint prevStride = 0;
+					for(auto iter = attributes.begin(); iter != attributes.end(); ++iter)
+					{
+						const std::string& attr = *iter;
+
+						if(attr == "POSITION")		stride += sizeof(Vector3);
+						else if(attr == "NORMAL")	stride += sizeof(Vector3);
+						else if(attr == "COLOR")	stride += sizeof(Vector4);
+						else
+						{
+							auto IsValidAttribute = [](const std::string& attr, const std::string& getAttrStr)
+							{
+								return ( attr.find(getAttrStr) == 0) && ( attr.size() > getAttrStr.size() );
+							};
+
+							if(IsValidAttribute(attr, "TEXCOORD"))
+							{
+								if(uv0Pos == 0)
+									uv0Pos = stride / sizeof(float);
+
+								stride += sizeof(Vector2);
+							}
+							if(IsValidAttribute(attr, "BONEWEIGHT"))	stride += sizeof(Vector2);
+						}
+
+						VertexBuffer::SemanticInfo semantic;
+						{
+							semantic.name = attr;
+							semantic.size = stride - prevStride;
+						}
+						semantics.push_back(semantic);
+
+						prevStride = stride;
+					}
+				}
+
+				if(hasNormalMap)
+				{
+					uint originStride = stride;
+					stride += sizeof(Vector3);
+					auto& vertexDatas = meshIter->vertexDatas;
+
+					std::vector<Vector3> tangents;
+					CalculateTangents(tangents, meshIter->parts, meshIter->vertexDatas, originStride, uv0Pos);
+
+					// Tangent를 쓴다는건, 이미 앞에 Normal을 사용한다는 것과 같다.
+					// 그래서 그냥 고정값임. 사실은 귀찮기도하고, 그래 귀찮다.
+					const uint tangentOrder = (uint)Attribute::Tangent;
+
+					//const uint originElemCount = originStride / sizeof(float);
+					const uint next = originStride / sizeof(float);
+					uint size = vertexDatas.size();
+
+					uint tanIdx = 0;
+					uint start = (sizeof(Vector3) * 2) / sizeof(float);
+					for(uint i= start; i<size; i+=next)
+					{
+						const Vector3& tangent = tangents[tanIdx++];
+
+						vertexDatas.insert( vertexDatas.begin() + i, tangent.z );	//z
+						vertexDatas.insert( vertexDatas.begin() + i, tangent.y );	//y
+						vertexDatas.insert( vertexDatas.begin() + i, tangent.x );	//x
+
+						size = vertexDatas.size();
+					}
+
+					VertexBuffer::SemanticInfo info;
+					{
+						info.name = "TANGENT";
+						info.size = sizeof(Vector3);
+					}
+					semantics.insert(semantics.begin() + tangentOrder, info);
+				}
+
+				// Make Vertex Buffer
+				{
+					const auto& vertices = meshIter->vertexDatas;
+					VertexBuffer* vertexBuffer = new VertexBuffer;
+
+					uint count = vertices.size() / (stride / 4);
+
+					bool success = 
+						vertexBuffer->Initialize(vertices.data(), stride, count,
+						useDynamicVB, nullptr);
+					ASSERT_COND_MSG(success, "Error, Can't create index buffer");
+
+					std::string key = "Chunk" + std::to_string(meshIterIdx);
+					bufferMgr->Add(meshFileName, key, vertexBuffer);
+				}
+			}
+		}
+	}
+
+	// Make Hierachy
+	{
+		Object* root = new Object(meshFileName, nullptr);
+		
+		for(auto iter = nodes.begin(); iter != nodes.end(); ++iter)
+			MakeHierarchy(root, (*iter));
+	}
+
+	return nullptr;
+}
+
+void MeshImporter::FetchAllPartsInHashMap_Recursive(
+	std::hash_map<std::string, std::string>& outParts, const Node& node)
+{
+	const auto& parts = node.parts;
+	for(auto iter = parts.begin(); iter != parts.end(); ++iter)
+		outParts.insert(std::make_pair(iter->meshPartId, iter->materialId));
+
+	const auto& childs = node.childs;
+	for(auto iter = childs.begin(); iter != childs.end(); ++iter)
+		FetchAllPartsInHashMap_Recursive(outParts, *iter);
+}
+
+void MeshImporter::FetchNormalMapMeshKeyLists(
+	std::vector<std::pair<std::string, std::string>>& outNormalMapMeshes,
+	const Node& node,
+	const std::string& meshFileName)
+{
+	for(auto iter = node.parts.begin(); iter != node.parts.end(); ++iter)
+	{
+		const std::string& materialId = iter->materialId;
+	}
+}
+
+void MeshImporter::MakeMaterials(std::set<std::string>& outNormalMapMaterialKeys, const std::vector<Importer::Material>& materials, const std::string& meshFileName)
+{
+	const ResourceManager* resourceMgr	= ResourceManager::GetInstance();
+	const Scene* scene					= Director::GetInstance()->GetCurrentScene();
+	MaterialManager* materialMgr		= scene->GetMaterialManager();
+	TextureManager* textureMgr			= resourceMgr->GetTextureManager();
+
+	auto MakeMaterial = [&](const Material& impMat, Rendering::Material::Type materialType)
+	{
+		const std::string materialName = impMat.id;
+		ASSERT_COND_MSG(materialName.empty() == false, "Material has not key");
+
+		Rendering::Material* material = materialMgr->Find(meshFileName, materialName);
+
+		if(material == nullptr)
+		{
+			if(materialType == Rendering::Material::Type::PhysicallyBasedModel)
+				material = new PhysicallyBasedMaterial(materialName);			
+			else
+			{
+				DEBUG_LOG("Warning, can't support material type.");
+				material = new Rendering::Material(materialName, materialType);
+			}
+
+			material->Initialize();
+
+			if(materialType == Rendering::Material::Type::PhysicallyBasedModel)
+			{
+				PhysicallyBasedMaterial* pbm = dynamic_cast<PhysicallyBasedMaterial*>(material);
+				pbm->UpdateMainColor(Color(impMat.diffuse[0], impMat.diffuse[1], impMat.diffuse[2], impMat.opacity));
+
+				const auto& textures = impMat.textures;
+				for(auto iter = textures.begin(); iter != textures.end(); ++iter)
+				{
+					Texture::Texture2D* texture = textureMgr->LoadTextureFromFile(meshFileName + ":" + iter->fileName, false);
+
+					if(iter->type == Material::Texture::Type::Diffuse)
+						pbm->UpdateDiffuseMap(texture);
+					else if( (iter->type == Material::Texture::Type::Normal) )
+					{
+						pbm->UpdateNormalMap(texture);
+						outNormalMapMaterialKeys.insert(materialName);
+					}
+					else if(iter->type == Material::Texture::Type::Specular)
+						pbm->UpdateSpecularMap(texture);
+					else
+					{
+						DEBUG_LOG("Warning, Unsupported Texture Type.");
+					}
+				}
+			}
+			else
+			{
+				DEBUG_LOG("Warning, can't support material type.");
+
+				Color diffuseColor;
+				diffuseColor.r = impMat.diffuse[0];
+				diffuseColor.g = impMat.diffuse[1];
+				diffuseColor.b = impMat.diffuse[2];
+				diffuseColor.a = impMat.opacity;
+
+				material->SetVariable("MainColor", diffuseColor);
+
+				if( impMat.shininess > 0.0f )
+					material->SetVariable("Shininess", impMat.shininess);
+
+				const auto& textures = impMat.textures;
+				for(auto iter = textures.begin(); iter != textures.end(); ++iter)
+				{
+					Texture::Texture2D* texture = textureMgr->LoadTextureFromFile(meshFileName + ":" + iter->fileName, false);
+
+					if(iter->type == Material::Texture::Type::Diffuse)
+					{
+						const uint shaderSlotIndex = (uint)PhysicallyBasedMaterial::InputTextureShaderIndex::Diffuse;
+						material->SetTextureUseShaderSlotIndex(shaderSlotIndex, texture, ShaderForm::Usage(false, false, false, true));
+					}
+					else if( (iter->type == Material::Texture::Type::Normal) )
+					{
+						const uint shaderSlotIndex = (uint)PhysicallyBasedMaterial::InputTextureShaderIndex::Normal;
+						material->SetTextureUseShaderSlotIndex(shaderSlotIndex, texture, ShaderForm::Usage(false, false, false, true));
+						outNormalMapMaterialKeys.insert(materialName);
+					}
+					else if(iter->type == Material::Texture::Type::Specular)
+					{
+						const uint shaderSlotIndex = (uint)PhysicallyBasedMaterial::InputTextureShaderIndex::Specular;
+						material->SetTextureUseShaderSlotIndex(shaderSlotIndex, texture, ShaderForm::Usage(false, false, false, true));
+					}
+					else
+					{
+						DEBUG_LOG("Warning, Unsupported Texture Type.");
+					}
+				}
+			}
+
+			materialMgr->Add(meshFileName, materialName, material);
+		}
+		else
+		{
+			DEBUG_LOG("Material Manager already has new mateiral. Please check key from new material");
+		}
+	};
+
+	for(auto iter = materials.begin(); iter != materials.end(); ++iter)
+		MakeMaterial(*iter, Rendering::Material::Type::PhysicallyBasedModel);
+}
+
+void MeshImporter::MakeHierarchy(Core::Object* parent, const Node& node)
+{
+	Object* object = new Object(node.id, parent);
+	parent->AddChild(object);
+
+	// Setting Transform
+	{
+		Transform* tf = object->GetTransform();
+		tf->UpdatePosition(node.translation);
+		tf->UpdateRotation(node.rotation);
+		tf->UpdateScale(node.scale);
+	}
+
+	// Setting SubMesh
+	{
+		const auto& parts =  node.parts;
+		uint size = parts.size();
+		if(size > 1)
+		{
+			for(auto iter = parts.begin(); iter != parts.end(); ++iter)
+			{
+				const std::string& subMeshId = iter->meshPartId;
+				Object* subMeshObj = new Object(subMeshId, object);
+			}
+		}
+	}
+
+	auto& childs = node.childs;
+	for(auto iter = childs.begin(); iter != childs.end(); ++iter)
+		MakeHierarchy(object, *iter);
+}
+
+void MeshImporter::CalculateTangents(
+	std::vector<Math::Vector3>& outTangents, 
+	const std::vector<Importer::Mesh::Part>& parts,
+	const std::vector<float>& vertexDatas,
+	uint originStrideSize, uint uv0PosInAttributes)
+{
+	uint elemCountInStride = originStrideSize / sizeof(float);
+	uint totalSize = vertexDatas.size() / elemCountInStride;
+#if 0
+	std::vector<std::pair<Math::Vector3, uint>> tangents(totalSize);
+#else
+	outTangents.clear();
+	outTangents.resize(totalSize);
+#endif
+
+	for(auto iter = parts.begin(); iter != parts.end(); ++iter)
+	{
+		const auto& indices = iter->indices;
+		uint size = indices.size();
+		for(uint i=0; i<size; i+=3)
+		{
+			uint idx0 = indices[i + 0];
+			uint idx1 = indices[i + 1];
+			uint idx2 = indices[i + 2];
+
+			std::array<Math::Vector3, 3> vertices;
+			{
+				vertices[0].x = vertexDatas[idx0 * elemCountInStride + 0];
+				vertices[0].y = vertexDatas[idx0 * elemCountInStride + 1];
+				vertices[0].z = vertexDatas[idx0 * elemCountInStride + 2];
+
+				vertices[1].x = vertexDatas[idx1 * elemCountInStride + 0];
+				vertices[1].y = vertexDatas[idx1 * elemCountInStride + 1];
+				vertices[1].z = vertexDatas[idx1 * elemCountInStride + 2];
+
+				vertices[2].x = vertexDatas[idx2 * elemCountInStride + 0];
+				vertices[2].y = vertexDatas[idx2 * elemCountInStride + 1];
+				vertices[2].z = vertexDatas[idx2 * elemCountInStride + 2];
+			}
+
+			std::array<Math::Vector2, 3> uvs;
+			{
+				uvs[0].x = vertexDatas[idx0 * elemCountInStride + uv0PosInAttributes + 0];
+				uvs[0].y = vertexDatas[idx0 * elemCountInStride + uv0PosInAttributes + 1];
+
+				uvs[1].x = vertexDatas[idx1 * elemCountInStride + uv0PosInAttributes + 0];
+				uvs[1].y = vertexDatas[idx1 * elemCountInStride + uv0PosInAttributes + 1];
+
+				uvs[2].x = vertexDatas[idx2 * elemCountInStride + uv0PosInAttributes + 0];
+				uvs[2].y = vertexDatas[idx2 * elemCountInStride + uv0PosInAttributes + 1];
+			}
+
+			Math::Vector3 tangent;
+			ImporterUtility::CalculateTangent(tangent, vertices, uvs);
+#if 0
+			tangents[idx0].first += tangent;	tangents[idx0].second++;
+			tangents[idx1].first += tangent;	tangents[idx1].second++;
+			tangents[idx2].first += tangent;	tangents[idx2].second++;
+#else
+			outTangents[idx0] = tangent;
+			outTangents[idx1] = tangent;
+			outTangents[idx2] = tangent;
+#endif
+		}
+	}
+
+	// Normalize Tangents and Save ouput
+	{
+#if 0
+		for(auto iter = tangents.begin(); iter != tangents.end(); ++iter)
+		{
+			uint count = iter->second;
+			Math::Vector3 tangent = iter->first / (float)count;
+			tangent = tangent.Normalize();
+			outTangents.push_back(tangent);
+		}
+#endif
+	}
+
 }
--- SOCEngine/SOCEngine/Importer/MeshImporter.h
+++ SOCEngine/SOCEngine/Importer/MeshImporter.h
@@ -6,6 +6,9 @@
 #include "Material.h"
 #include "rapidjson/document.h"
 #include "G3djDataTypes.h"
+#include "MaterialManager.h"
+#include <set>
+#include <hash_map>
 
 namespace Importer
 {
@@ -13,8 +16,6 @@ namespace Importer
 	{
 	private:
 		Structure::VectorMap<std::string, Core::Object*>	_originObjects;
-
-	private:
 
 	public:
 		MeshImporter();
@@ -28,13 +29,25 @@ namespace Importer
 		void ParseNode(Node& outNodes, const rapidjson::Value& node);
 		void ParseMaterial(Material& outMaterial, const rapidjson::Value& matNode);
 		void ParseMesh(Mesh& outMesh, const rapidjson::Value& meshNode);
+		void CalculateTangents(
+			std::vector<Math::Vector3>& outTangents,
+			const std::vector<Mesh::Part>& parts, 
+			const std::vector<float>& vertices, 
+			uint originStrideSize, uint uv0PosInAttributes);
 
 	private:
+		void MakeMaterials(std::set<std::string>& outNormalMapMaterialKeys, const std::vector<Material>& materials, const std::string& meshFileName);
+		void MakeHierarchy(Core::Object* parent, const Node& node);
+		Core::Object* BuildMesh(std::vector<Mesh>& meshes, const std::vector<Material>& materials, const std::vector<Node>& nodes, const std::string& meshFileName, bool useDynamicVB, bool useDynamicIB);
+		void FetchNormalMapMeshKeyLists(std::vector<std::pair<std::string, std::string>>& outNormalMapMeshes, const Node& node, const std::string& meshFileName);
+
+		// key is meshPartId, second value is materialId
+		void FetchAllPartsInHashMap_Recursive(std::hash_map<std::string, std::string>& outParts, const Node& node);
 
 	public:
 		void ParseJson(std::vector<Mesh>& outMeshes, std::vector<Material>& outMaterials, std::vector<Node>& outNodes, const char* buffer);
 		void ParseBinary(std::vector<Mesh>& outMeshes, std::vector<Material>& outMaterials, std::vector<Node>& outNodes, const void* buffer, uint size);
-		Core::Object* Load(const std::string& fileDir, Rendering::Material::Type materialType = Rendering::Material::Type::PhysicallyBasedModel, bool isDynamicMesh = false);
+		Core::Object* Load(const std::string& fileDir, bool useDynamicVB = false, bool useDynamicIB = false, Rendering::Material::Type materialType = Rendering::Material::Type::PhysicallyBasedModel);
 
 	public:
 		Core::Object* Find(const std::string& key);
