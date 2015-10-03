fd524e89a52a89dd71517d10f8cc630ed7142795
Jin <quaggan02@gmail.com>
2015-10-03 13:32:19 +0900

    POSITION 시멘틱의 경우 allignByteOffset이 4 감소 되던 버그 수정 #89

--- SOCEngine/SOCEngine/Script/HLSLMetaDataGenerator.py
+++ SOCEngine/SOCEngine/Script/HLSLMetaDataGenerator.py
@@ -262,9 +262,6 @@ class ParseCode:
 
 			alignedByteOffset = 4 * num + beforeLayout.alignedByteOffset
 
-			if beforeLayout.semanticName == 'POSITION':
-				alignedByteOffset -= 4
-
 			return alignedByteOffset		
 
 		def MakeInputLayout(name, variableType, index, offset, usingType):
