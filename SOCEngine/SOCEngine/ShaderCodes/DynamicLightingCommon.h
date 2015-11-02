0eddcce0d584786d1817db14bd850f0d9c049848
Jin <quaggan02@gmail.com>
2015-11-02 15:13:29 +0900

    Shadow Mapping의 Render 함수만 구현.. Shadowing 할때는, 그냥 깊이만 체크할 것이 아닌 간단하게 블러도 추가했다. #96

--- SOCEngine/SOCEngine/ShaderCodes/DynamicLightingCommon.h
+++ SOCEngine/SOCEngine/ShaderCodes/DynamicLightingCommon.h
@@ -6,6 +6,114 @@
 #include "ShaderCommon.h"
 #include "BRDF.h"
 
+#define SHADOW_KERNEL_LEVEL		2
+#define SHADOW_KERNEL_WIDTH		2 * SHADOW_KERNEL_LEVEL + 1
+
+float Shadowing(Texture2D<float> atlas, float2 uv, float depth, SamplerComparisonState shadowSampler)
+{
+	float shadow = 0.0f;
+	for(int i=-SHADOW_KERNEL_LEVEL; i<=SHADOW_KERNEL_LEVEL; ++i)
+	{
+		for(int j=-SHADOW_KERNEL_LEVEL; j<=SHADOW_KERNEL_LEVEL; ++j)
+		{
+			//if(depth > atlas.Sample(shadowSampler, uv).x)
+			//	shadow += 0.5f;
+			shadow += atlas.SampleCmpLevelZero(shadowSampler, uv, depth, int2(i, j)).x;
+		}
+	}
+
+	shadow /= (float)(SHADOW_KERNEL_WIDTH * SHADOW_KERNEL_WIDTH);
+	return shadow;
+}
+
+float3 RenderSpotLightShadow(uint lightIndex, float3 vertexWorldPos, SamplerComparisonState shadowSampler)
+{
+	float4 shadowUV = mul( float4(vertexWorldPos, 1.0f), g_inputSpotLightShadowParams[lightIndex].viewProjMat );
+	shadowUV /= shadowUV.w;
+
+	shadowUV.x = (shadowUV.x / 2.0f) + 0.5f;
+	shadowUV.y = (shadowUV.y /-2.0f) + 0.5f;
+
+	uint shadowIndex = (uint)g_inputSpotLightShadowParams[lightIndex].index;
+	shadowUV.x += shadowIndex;
+
+	uint lightCount = GetNumOfSpotLight(shadowGlobalParam_packedNumOfShadowCastingLights);
+	shadowUV.x *= rcp((float)lightCount);//(1.0f / (float)lightCount);
+
+	float bias = (float)g_inputSpotLightShadowParams[lightIndex].bias;
+	float depth = shadowUV.z - bias;
+
+	return Shadowing(g_inputSpotLightShadowMapAtlas, shadowUV.xy, depth, shadowSampler).xxx;
+}
+
+float3 RenderDirectionalLightShadow(uint lightIndex, float3 vertexWorldPos, SamplerComparisonState shadowSampler)
+{
+	float4 shadowUV = mul( float4(vertexWorldPos, 1.0f), g_inputDirectionalLightShadowParams[lightIndex].viewProjMat );
+	shadowUV /= shadowUV.w;
+
+	shadowUV.x = (shadowUV.x / 2.0f) + 0.5f;
+	shadowUV.y = (shadowUV.y /-2.0f) + 0.5f;
+
+	uint shadowIndex = (uint)g_inputDirectionalLightShadowParams[lightIndex].index;
+	shadowUV.x += shadowIndex;
+
+	uint lightCount = GetNumOfDirectionalLight(shadowGlobalParam_packedNumOfShadowCastingLights);
+	shadowUV.x *= rcp((float)lightCount);//(1.0f / (float)lightCount);
+
+	float bias = (float)g_inputDirectionalLightShadowParams[lightIndex].bias;
+	float depth = shadowUV.z - bias;
+
+	return Shadowing(g_inputSpotLightShadowMapAtlas, shadowUV.xy, depth, shadowSampler).xxx;
+}
+
+uint ComputeFaceIndex(float3 dir)
+{
+	// 뭐, 별건 없고
+	// PointLight::ComputeViewProjMatrix에서 사용되는 forwards, ups 이거 인덱스 정하는 함수이다.
+	// 대체 뭔 방식으로 계산하는지 궁금하다면, 0, 0, 1이나 1, 0, 0 직접 머리속에 넣어서 계산하면된다.
+	// 그럼 PointLight::ComputeViewProjMatrix에서 사용 중인 인덱스와 동일하게 뜬다.
+
+	uint res = 0;
+
+	if( (abs(dir.x) > abs(dir.y)) && (abs(dir.x) > abs(dir.z)) )
+		res = dir.x >= 0.0f ? 2 : 3;
+	else if( (abs(dir.y) > abs(dir.x)) && (abs(dir.y) > abs(dir.z)) )
+		res = dir.y >= 0.0f ? 4 : 5;
+	else
+		res = dir.z >= 0.0f ? 0 : 1;
+
+	return res;
+}
+
+float3 RenderPointLightShadow(uint lightIndex, float3 vertexWorldPos, float3 lightDir, SamplerComparisonState shadowSampler)
+{
+	uint shadowIndex = (uint)g_inputDirectionalLightShadowParams[lightIndex].index;
+	uint lightCount = GetNumOfPointLight(shadowGlobalParam_packedNumOfShadowCastingLights);
+
+	float3 uv = float3(-lightDir.x, -lightDir.y, lightDir.z);
+	uint faceIndex = ComputeFaceIndex(uv);
+
+	float4 shadowUV = mul( float4(vertexWorldPos, 1.0f), g_inputPointLightShadowParams[lightIndex].viewProjMat[faceIndex] );
+	shadowUV /= shadowUV.w;
+
+	shadowUV.x = (shadowUV.x / 2.0f) + 0.5f;
+	shadowUV.y = (shadowUV.y /-2.0f) + 0.5f;
+
+	shadowUV.xy *= (shadowGlobalParam_pointLightTexelOffset).xx;
+	shadowUV.xy += (shadowGlobalParam_pointLightUnderscanScale).xx;
+
+	shadowUV.y += (float)faceIndex;
+	shadowUV.y *= rcp(6); //1.0f / 6.0f;
+
+	shadowUV.x += shadowIndex;
+	shadowUV.x *= rcp((float)lightCount);//(1.0f / (float)lightCount);
+
+	float bias = (float)g_inputDirectionalLightShadowParams[lightIndex].bias;
+	float depth = shadowUV.z - bias;
+
+	return Shadowing(g_inputSpotLightShadowMapAtlas, shadowUV.xy, depth, shadowSampler).xxx;
+}
+
 void RenderDirectionalLight(
 #if defined(RENDER_TRANSPARENCY)
 					out float3 resultFrontFaceDiffuseColor, out float3 resultFrontFaceSpecularColor,
@@ -84,7 +192,7 @@ void RenderPointLight(
 		commonParams.lightDir		= lightDir;
 
 		float lumen = g_inputPointLightColorBuffer[lightingParams.lightIndex].w * 12750.0f; //maximum lumen is 12,750f
-		float attenuation = 1.0f / (distanceOfLightWithVertex * distanceOfLightWithVertex) * lumen;
+		float attenuation = lumen / (distanceOfLightWithVertex * distanceOfLightWithVertex);
 
 #if defined(RENDER_TRANSPARENCY)
 		BRDFLighting(resultFrontFaceDiffuseColor, resultFrontFaceSpecularColor, lightingParams, commonParams);
