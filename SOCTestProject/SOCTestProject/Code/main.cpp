189061060ef6a941bf4371b4428b942f522820c0
Jin <quaggan02@gmail.com>
2015-11-06 03:21:29 +0900

    해상도 증가

--- SOCTestProject/SOCTestProject/Code/main.cpp
+++ SOCTestProject/SOCTestProject/Code/main.cpp
@@ -4,7 +4,7 @@
 
 INT WINAPI wWinMain( HINSTANCE hInst, HINSTANCE, LPWSTR, INT )
 {
-	auto rect = Math::Rect<unsigned int>(0, 0, 1280, 800);
+	auto rect = Math::Rect<unsigned int>(0, 0, 1600, 900);
 	Device::Director::GetInstance()->Initialize(rect, rect, hInst, "Test", true, false, 0);
 	Device::Director::GetInstance()->SetCurrentScene(new TestScene);
 	Device::Director::GetInstance()->Run();
