7d2bb3090c1b9eb287ae59474ffe8e012accade3
Jin <quaggan02@gmail.com>
2015-11-10 07:56:09 +0900

    Voxelization 추가 및 vcxproj 업데이트 #105

---
+++ SOCEngine/SOCEngine/Rendering/GI/Voxelization.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Texture3D.h"
+
+namespace Rendering
+{
+	namespace GI
+	{
+		class Voxelization
+		{
+		private:
+
+		public:
+			Voxelization();
+			~Voxelization();
+		};
+	}
+}
