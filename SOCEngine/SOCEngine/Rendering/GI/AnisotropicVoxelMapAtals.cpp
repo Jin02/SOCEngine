fabbc0a613ddf817bc16104dc2294135d10cabef
Jin <quaggan02@gmail.com>
2015-11-30 21:02:58 +0900

    AnisotropicVoxelMapAtlas - 1. Mipmap에 사용할 uav 생성 2. Bind, Unbind 에러 수정
    #107

--- SOCEngine/SOCEngine/Rendering/GI/AnisotropicVoxelMapAtals.cpp
+++ SOCEngine/SOCEngine/Rendering/GI/AnisotropicVoxelMapAtals.cpp
@@ -1,55 +1,66 @@
 #include "AnisotropicVoxelMapAtlas.h"
+#include "Director.h"
 
+using namespace Device;
 using namespace Math;
 using namespace Rendering::GI;
 using namespace Rendering::Texture;
 using namespace Rendering::Shader;
+using namespace Rendering::View;
 
 AnisotropicVoxelMapAtlas::AnisotropicVoxelMapAtlas()
-	: _voxelMapAtlas(nullptr), _uavBindIndex(-1)
+	: Texture3D(), _uavBindIndex(-1)
 {
 }
 
 AnisotropicVoxelMapAtlas::~AnisotropicVoxelMapAtlas()
 {
 	Destroy();
-
-	SAFE_DELETE(_voxelMapAtlas);
 }
 
 void AnisotropicVoxelMapAtlas::Initialize(
-	uint sideLength, uint maxNumOfCascade, DXGI_FORMAT format, uint mipmapLevels, uint uavBindIndex)
+	uint sideLength, uint maxNumOfCascade, DXGI_FORMAT format, uint mipmapCount, uint uavBindIndex)
 {
+	const uint bindFlags = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_UNORDERED_ACCESS;
+
 	_uavBindIndex	= uavBindIndex;
 	_sideLength		= sideLength;
-	_mipmapLevels	= mipmapLevels;
-
-	_voxelMapAtlas	= new VoxelMap;
+	_mipmapCount	= mipmapCount;
 
 	uint width		= sideLength * (uint)Direction::Num;
 	uint height		= sideLength * maxNumOfCascade;
-	_voxelMapAtlas->Initialize(width, height, sideLength, format);
-}
+	Texture3D::Initialize(width, height, sideLength, format, bindFlags, _mipmapCount);
 
-void AnisotropicVoxelMapAtlas::Destroy()
-{
-	_voxelMapAtlas->Destroy();
+	auto device = Director::GetInstance()->GetDirectX()->GetDevice();
+
+	for(uint i=0; i<mipmapCount - 1; ++i)
+	{
+		UnorderedAccessView* uav = new UnorderedAccessView;
+
+		uint halfWidth	= width / 2;
+		uint halfHeight	= height / 2;
+		uint halfDepth	= sideLength / 2;
+
+		uav->Initialize(format, halfWidth * halfHeight * halfDepth, _texture, D3D11_UAV_DIMENSION_TEXTURE3D, i, halfDepth);
+
+		_mipmapUAVs.push_back(uav);
+	}
 }
 
 void AnisotropicVoxelMapAtlas::BindUAVsToPixelShader(const Device::DirectX* dx)
 {
-	//ID3D11DeviceContext* context	= dx->GetContext();
-	//const uint initCounts[1]		= {0xFFFFFFFFu};		
-	//ID3D11UnorderedAccessView* view	= _voxelMapAtlas->GetUnorderedAccessView()->GetView();
+	ID3D11DeviceContext* context	= dx->GetContext();
+	const uint initCounts[1]		= {0xFFFFFFFFu};		
+	ID3D11UnorderedAccessView* view	= _uav->GetView();
 
-	//context->OMSetRenderTargetsAndUnorderedAccessViews(D3D11_KEEP_RENDER_TARGETS_AND_DEPTH_STENCIL, nullptr, nullptr, _uavBindIndex, 1, &view, initCounts);
+	context->OMSetRenderTargetsAndUnorderedAccessViews(D3D11_KEEP_RENDER_TARGETS_AND_DEPTH_STENCIL, nullptr, nullptr, _uavBindIndex, 1, &view, initCounts);
 }
 
 void AnisotropicVoxelMapAtlas::UnbindUAVs(const Device::DirectX* dx)
 {
-	//ID3D11DeviceContext* context = dx->GetContext();
-	//const uint initCounts[1] = {0xFFFFFFFFu};		
-	//ID3D11UnorderedAccessView* view = nullptr;
+	ID3D11DeviceContext* context = dx->GetContext();
+	const uint initCounts[1] = {0xFFFFFFFFu};		
+	ID3D11UnorderedAccessView* view = nullptr;
 
-	//context->OMSetRenderTargetsAndUnorderedAccessViews(D3D11_KEEP_RENDER_TARGETS_AND_DEPTH_STENCIL, nullptr, nullptr, _uavBindIndex, 1, &view, initCounts);
+	context->OMSetRenderTargetsAndUnorderedAccessViews(D3D11_KEEP_RENDER_TARGETS_AND_DEPTH_STENCIL, nullptr, nullptr, _uavBindIndex, 1, &view, initCounts);
 }
--- SOCEngine/SOCEngine/Rendering/GI/AnisotropicVoxelMapAtlas.h
+++ SOCEngine/SOCEngine/Rendering/GI/AnisotropicVoxelMapAtlas.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#include "VoxelMap.h"
+#include "Texture3D.h"
 
 namespace Rendering
 {
 	namespace GI
 	{
-		class AnisotropicVoxelMapAtlas
+		class AnisotropicVoxelMapAtlas : public Texture::Texture3D
 		{
 		public:
 			enum class Direction : uint
@@ -18,27 +18,26 @@ namespace Rendering
 			};
 
 		private:
-			VoxelMap*	_voxelMapAtlas;
-
-		private:
 			uint _sideLength;
-			uint _mipmapLevels;
+			uint _mipmapCount;
 			uint _uavBindIndex;
+
+			std::vector<View::UnorderedAccessView*>	_mipmapUAVs;
 
 		public:
 			AnisotropicVoxelMapAtlas();
 			~AnisotropicVoxelMapAtlas();
 
 		public:
-			void Initialize(uint sideLength, uint maxNumOfCascade, DXGI_FORMAT format, uint mipmapLevels, uint uavBindIndex);
+			void Initialize(uint sideLength, uint maxNumOfCascade, DXGI_FORMAT format, uint mipmapCount, uint uavBindIndex);
 			void Destroy();
 
 			void BindUAVsToPixelShader(const Device::DirectX* dx);
 			void UnbindUAVs(const Device::DirectX* dx);
 
 		public:
-			GET_ACCESSOR(SideLength, uint, _sideLength);
-			GET_ACCESSOR(MipmapLabels, uint, _mipmapLevels);
+			GET_ACCESSOR(SideLength,	uint,	_sideLength);
+			GET_ACCESSOR(MipMapCount,	uint,	_mipmapCount);
 		};
 	}
 }
