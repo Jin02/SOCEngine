81d3ea44cd36565a391c1e43eea311e0927310e1
Jin <quaggan02@gmail.com>
2015-12-08 07:07:16 +0900

    Injection_Common.h, InjectRadianceFromDirectionalLight.hlsl 추가 #109

---
+++ SOCEngine/SOCEngine/ShaderCodes/InjectRadianceFromDirectionalLight.hlsl
@@ -0,0 +1,66 @@
+//EMPTY_META_DATA
+
+#include "Injection_Common.h"
+
+[numthreads(8, 8, 8)]
+void InjectRadianceDirectionalLightsCS(	uint3 globalIdx	: SV_DispatchThreadID, 
+										uint3 localIdx	: SV_GroupThreadID,
+										uint3 groupIdx	: SV_GroupID)
+{
+	uint capacity			= GetNumOfDirectionalLight(shadowGlobalParam_packedNumOfShadowAtlasCapacity);
+	float perShadowMapRes	= (float)(1 << GetNumOfDirectionalLight(shadowGlobalParam_packedPowerOfTwoShadowAtlasSize));
+	uint2 shadowMapSize		= uint2(perShadowMapRes * capacity, perShadowMapRes * capacity);
+
+	uint shadowIndex	= globalIdx.x / (uint)perShadowMapRes;
+	uint lightIndex		= g_inputDirectionalLightShadowIndexToLightIndex[shadowIndex];
+
+	if( (globalIdx.x >= shadowMapSize.x || globalIdx.y >= shadowMapSize.y) ||
+		(((uint)g_inputDirectionalLightShadowParams[lightIndex].index - 1) != shadowIndex) )
+		return;
+
+	float2 shadowMapPos	= float2(globalIdx.x % perShadowMapRes, globalIdx.y % perShadowMapRes);
+	float2 shadowMapUV	= shadowMapPos.xy / perShadowMapRes;
+
+	float depth = g_inputDirectionalLightShadowMapAtlas.Load(uint3(globalIdx.xy, 0), 0);
+
+	float4 screenSpaceCoord = float4( shadowMapUV.x * 2.0f - 1.0f,
+									-(shadowMapUV.y * 2.0f - 1.0f),
+									depth, 1.0f );
+
+	float4 voxelSpaceCoord = mul(screenSpaceCoord, injection_volumeProj);
+	voxelSpaceCoord /= voxelSpaceCoord.w;
+	voxelSpaceCoord.xyz = voxelSpaceCoord.xyz * 0.5f + 0.5f;
+	uint3 voxelIdx = uint3(voxelSpaceCoord.xyz * voxelization_dimension);
+
+	float4 lightCenterWithDirZ	= g_inputDirectionalLightTransformWithDirZBuffer[lightIndex];
+	float2 lightParam			= g_inputDirectionalLightParamBuffer[lightIndex];
+	float3 lightDir				= -float3(lightParam.x, lightParam.y, lightCenterWithDirZ.w);
+
+	float3 albedo	= GetColor(AnistropicVoxelAlbedoTexture, voxelIdx, lightDir, voxelization_currentCascade);
+	float3 normal	= GetNormal(AnistropicVoxelNormalTexture, voxelIdx, lightDir, voxelization_currentCascade);
+	float3 emission	= GetColor(AnistropicVoxelEmissionTexture, voxelIdx, lightDir, voxelization_currentCascade);
+
+	float3 lightColor	= g_inputDirectionalLightColorBuffer[lightIndex].rgb;
+	float3 lambert		= albedo * dot(normal, lightDir);
+	float intensity		= g_inputDirectionalLightColorBuffer[lightIndex].a * 10.0f;
+	float4 outputColor	= float4(lambert * lightColor * intensity, 1.0f);
+
+//	outputColor.rgb *= RenderDirectionalLightShadow(lightIndex, voxelCenterPos);
+
+	float anisotropicNormals[6] = {
+		 normal.x,
+		-normal.x,
+		 normal.y,
+		-normal.y,
+		 normal.z,
+		-normal.z
+	};
+
+	voxelIdx.y += (float)voxelization_currentCascade * voxelization_dimension;
+
+	for(int faceIndex=0; faceIndex<6; ++faceIndex)
+	{
+		voxelIdx.x += (float)faceIndex * voxelization_dimension;
+		StoreVoxelMapAtomicColorMax(OutAnistropicVoxelColorTexture, voxelIdx, float4(outputColor.xyz * max(anisotropicNormals[faceIndex], 0.0f), outputColor.a));
+	}
+}
---
+++ SOCEngine/SOCEngine/ShaderCodes/Injection_Common.h
@@ -0,0 +1,73 @@
+//EMPTY_META_DATA
+
+#ifndef __SOC_INJECTION_COMMON_H__
+#define __SOC_INJECTION_COMMON_H__
+
+#include "DynamicLightingCommon.h"
+#include "PhysicallyBased_Common.h"
+#include "Voxelization_Common.h"
+
+cbuffer Injection_Info_CB : register(b7)
+{
+	matrix injection_volumeProj;
+};
+
+Texture3D<float4>	AnistropicVoxelAlbedoTexture		: register( t12 );
+Texture3D<float>	AnistropicVoxelNormalTexture		: register( t13 );
+Texture3D<float4>	AnistropicVoxelEmissionTexture		: register( t14 );
+
+RWTexture3D<uint> OutAnistropicVoxelColorTexture	: register( u0 );
+
+float3 GetColor(Texture3D<float4> anisotropicVoxelTexture, uint3 voxelIdx, float3 dir, uint cascade)
+{
+	uint3 idx = voxelIdx;
+	idx.y += cascade * voxelization_dimension;
+
+	float4 colorAxisX = (dir.x > 0.0f) ? 
+		anisotropicVoxelTexture.Load(int4(idx, 0)) :
+		anisotropicVoxelTexture.Load(int4(idx.x + voxelization_dimension, idx.yz, 0));
+
+	float4 colorAxisY = (dir.y > 0.0f) ?
+		anisotropicVoxelTexture.Load(int4(idx.x + (voxelization_dimension * 2), idx.yz, 0)) :
+		anisotropicVoxelTexture.Load(int4(idx.x + (voxelization_dimension * 3), idx.yz, 0));
+	
+	float4 colorAxisZ = (dir.z > 0.0f) ?
+		anisotropicVoxelTexture.Load(int4(idx.x + (voxelization_dimension * 4), idx.yz, 0)) :
+		anisotropicVoxelTexture.Load(int4(idx.x + (voxelization_dimension * 5), idx.yz, 0));
+
+	dir = abs(dir);
+	return ((dir.x * colorAxisX) + (dir.y * colorAxisY) + (dir.z * colorAxisZ)).rgb;
+}
+
+float3 GetNormal(Texture3D<float> anisotropicVoxelNormalMap, uint3 voxelIdx, float3 dir, uint cascade)
+{
+	uint3 idx = voxelIdx;
+	idx.y += cascade * voxelization_dimension;
+
+	float normalAxisX = (dir.x > 0.0f) ? 
+		anisotropicVoxelNormalMap.Load(int4(idx, 0)) :
+	   -anisotropicVoxelNormalMap.Load(int4(idx.x + voxelization_dimension, idx.yz, 0));
+
+	float normalAxisY = (dir.y > 0.0f) ?
+		anisotropicVoxelNormalMap.Load(int4(idx.x + (voxelization_dimension * 2), idx.yz, 0))	:
+	   -anisotropicVoxelNormalMap.Load(int4(idx.x + (voxelization_dimension * 3), idx.yz, 0));
+	
+	float normalAxisZ = (dir.z > 0.0f) ?
+		anisotropicVoxelNormalMap.Load(int4(idx.x + (voxelization_dimension * 4), idx.yz, 0))	:
+	   -anisotropicVoxelNormalMap.Load(int4(idx.x + (voxelization_dimension * 5), idx.yz, 0));
+
+	return normalize( float3(normalAxisX, normalAxisY, normalAxisZ) );
+}
+
+float3 GetVoxelCenterPos(uint3 voxelIdx)
+{
+	float3 voxelCenter;
+
+	voxelCenter.x = voxelIdx.x * voxelization_voxelSize + voxelization_voxelSize * 0.5f + voxelization_minPos.x;
+	voxelCenter.y = voxelIdx.y * voxelization_voxelSize + voxelization_voxelSize * 0.5f + voxelization_minPos.y;
+	voxelCenter.z = voxelIdx.z * voxelization_voxelSize + voxelization_voxelSize * 0.5f + voxelization_minPos.z;
+	
+	return voxelCenter;
+}
+
+#endif
