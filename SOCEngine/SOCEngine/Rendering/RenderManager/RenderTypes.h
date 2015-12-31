64a98abfb616b01517910398b41ae85ffb9557a5
Jin <quaggan02@gmail.com>
2016-01-01 07:33:17 +0900

    RenderType - 코드 정리

--- SOCEngine/SOCEngine/Rendering/RenderManager/RenderTypes.h
+++ SOCEngine/SOCEngine/Rendering/RenderManager/RenderTypes.h
@@ -4,12 +4,12 @@ namespace Rendering
 {
 	enum class RenderType
 	{
-		GBuffer_AlphaBlend,
-		Forward_AlphaTest,
-		GBuffer_Opaque,
-		Forward_Transparency,
-		Forward_DepthOnly,
-		Voxelization,
+		GBuffer_AlphaBlend				= 0,
+		GBuffer_Opaque					= 1,
+		Forward_AlphaTestWithDiffuse	= 2,
+		Forward_Transparency			= 3,
+		Forward_DepthOnly				= 4,
+		Voxelization					= 5,
 		MAX_NUM
 	};
 }
