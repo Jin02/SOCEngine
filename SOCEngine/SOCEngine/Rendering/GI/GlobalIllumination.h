0f94048b2eb93c86d5a88f193bfded43b0dfa3a8
Jin <quaggan02@gmail.com>
2016-01-09 08:26:35 +0900

    Injection, Voxelization, Cone Tracing의 관리를 담당할 GlobalIllumination 추가. Init만 간단하게 구성해둠 #2

---
+++ SOCEngine/SOCEngine/Rendering/GI/GlobalIllumination.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "ComputeShader.h"
+#include "ConstBuffer.h"
+#include "GlobalIlluminationCommon.h"
+
+#include "Voxelization.h"
+
+#include "InjectRadianceFromDirectionalLIght.h"
+#include "InjectRadianceFromPointLIght.h"
+#include "InjectRadianceFromSpotLIght.h"
+
+namespace Rendering
+{
+	namespace GI
+	{
+		class GlobalIllumination
+		{
+		private:
+			GlobalInfo				_prevInfo;
+			Buffer::ConstBuffer*	_giGlobalInfoCB;
+
+		public:
+			GlobalIllumination();
+			~GlobalIllumination();
+
+		public:
+			void Initialize();
+		};
+	}
+}
