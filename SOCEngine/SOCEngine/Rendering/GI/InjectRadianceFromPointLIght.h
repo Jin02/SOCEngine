7b2037a52f8985e99c95f7a71f4aa4f59ce088f2
Jin <quaggan02@gmail.com>
2016-01-09 08:25:35 +0900

    InjectPointLight -> InjectRadianceFromPointLight #2

---
+++ SOCEngine/SOCEngine/Rendering/GI/InjectRadianceFromPointLIght.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace Rendering
+{
+	namespace GI
+	{
+		class InjectRadianceFromPointLIght
+		{
+		public:
+			InjectRadianceFromPointLIght();
+			~InjectRadianceFromPointLIght();
+		};
+	}
+}
