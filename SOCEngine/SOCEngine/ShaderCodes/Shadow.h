de0c53a5a9e18444ed229b5de02f49db9e1bf8bb
Jin <quaggan02@gmail.com>
2016-01-14 07:52:51 +0900

    Shadow - 1.VSM 처리 변경 2. 모든 Shadow 처리 재설계 및 재구현 3. 코드 정리
    #6

--- SOCEngine/SOCEngine/ShaderCodes/Shadow.h
+++ SOCEngine/SOCEngine/ShaderCodes/Shadow.h
@@ -5,8 +5,9 @@
 
 #include "ShaderCommon.h"
 
-#define SHADOW_KERNEL_LEVEL		4
-#define SHADOW_KERNEL_WIDTH		2 * SHADOW_KERNEL_LEVEL + 1
+#define SHADOW_KERNEL_LEVEL				4
+#define SHADOW_KERNEL_WIDTH				2 * SHADOW_KERNEL_LEVEL + 1
+#define SHADOW_PARAM_FLAG_USE_VSM		1
 
 SamplerComparisonState shadowSamplerCmpState	:	register( s2 );
 SamplerState shadowSamplerState					:	register( s3 );
@@ -65,72 +66,74 @@ float VarianceShadow(Texture2D<float4> momentShadowMapAtlas, float2 uv, float de
 
 float3 RenderSpotLightShadow(uint lightIndex, float3 vertexWorldPos, float shadowDistanceTerm)
 {
-	float4 shadowUV = mul( float4(vertexWorldPos, 1.0f), g_inputSpotLightShadowParams[lightIndex].viewProjMat );
+	uint shadowIndex = g_inputSpotLightShadowIndex[lightIndex];
+
+	float4 shadowUV = mul( float4(vertexWorldPos, 1.0f), g_inputSpotLightShadowViewProjMatrix[shadowIndex].mat);
 	shadowUV /= shadowUV.w;
 
-	shadowUV.x = (shadowUV.x / 2.0f) + 0.5f;
+	uint lightCount = GetNumOfSpotLight(shadowGlobalParam_packedNumOfShadowAtlasCapacity);
+	shadowUV.x = ((shadowUV.x / 2.0f) + 0.5f + float(shadowIndex)) * rcp((float)lightCount);
 	shadowUV.y = (shadowUV.y /-2.0f) + 0.5f;
 
-	uint shadowIndex = (uint)g_inputSpotLightShadowParams[lightIndex].index - 1;
-	shadowUV.x += shadowIndex;
+	ParsedShadowParam shadowParam;
+	ParseShadowParam(shadowParam, g_inputSpotLightShadowParams[shadowIndex]);
 
-	uint lightCount = GetNumOfSpotLight(shadowGlobalParam_packedNumOfShadowAtlasCapacity);
-	shadowUV.x *= rcp((float)lightCount);//(1.0f / (float)lightCount);
-
-	float bias = (float)g_inputSpotLightShadowParams[lightIndex].bias;
-	float depth = shadowUV.z
-#if defined(USE_SHADOW_INVERTED_DEPTH) && !defined(USE_VSM)
-		+
-#else
-		-
+	float bias	= lerp(10.0f, 1.0f, saturate(5 * shadowDistanceTerm)) * shadowParam.bias;
+	float depth	= shadowUV.z;
+	
+	float shadow = 1.0f;
+#ifndef NEVER_USE_VSM
+	if(shadowParam.flag & SHADOW_PARAM_FLAG_USE_VSM)
+		shadow = saturate( VarianceShadow(g_inputSpotLightMomentShadowMapAtlas, shadowUV.xy, depth) );
+	else
 #endif
-		lerp(10.0f, 1.0f, saturate(5 * shadowDistanceTerm)) * bias;
-
-#if defined(USE_VSM)
-	float shadow = saturate( VarianceShadow(g_inputSpotLightMomentShadowMapAtlas, shadowUV.xy, depth) );
+#ifdef USE_SHADOW_INVERTED_DEPTH
+		shadow = saturate( Shadowing(g_inputSpotLightShadowMapAtlas, shadowUV.xy, depth + bias) );
 #else
-	float shadow = saturate( Shadowing(g_inputSpotLightShadowMapAtlas, shadowUV.xy, depth) );
+		shadow = saturate( Shadowing(g_inputSpotLightShadowMapAtlas, shadowUV.xy, depth - bias) );
 #endif
 
-	float3 shadowColor = g_inputSpotLightShadowColors[lightIndex].rgb;
+	float3 shadowColor = shadowParam.color.rgb;
 	float3 result = lerp((float3(1.0f, 1.0f, 1.0f) - shadow.xxx) * shadowColor, float3(1.0f, 1.0f, 1.0f), shadow);
 
-	float shadowStrength = g_inputSpotLightShadowColors[lightIndex].a;
+	float shadowStrength = shadowParam.color.a;
 	return lerp(float3(1.0f, 1.0f, 1.0f), result, shadowStrength);
 }
 
 float3 RenderDirectionalLightShadow(uint lightIndex, float3 vertexWorldPos)
 {
-	float4 shadowUV = mul( float4(vertexWorldPos, 1.0f), g_inputDirectionalLightShadowParams[lightIndex].viewProjMat );
+	uint shadowIndex = g_inputDirectionalLightShadowIndex[lightIndex];
+
+	float4 shadowUV = mul( float4(vertexWorldPos, 1.0f), g_inputDirectionalLightShadowViewProjMatrix[shadowIndex].mat );
 	shadowUV /= shadowUV.w;
 
-	shadowUV.x = (shadowUV.x / 2.0f) + 0.5f;
+	uint lightCount = GetNumOfDirectionalLight(shadowGlobalParam_packedNumOfShadowAtlasCapacity);
+
+	shadowUV.x = ((shadowUV.x / 2.0f) + 0.5f + float(shadowIndex)) * rcp((float)lightCount);
 	shadowUV.y = (shadowUV.y /-2.0f) + 0.5f;
 
-	uint shadowIndex = (uint)g_inputDirectionalLightShadowParams[lightIndex].index - 1;
-	shadowUV.x += shadowIndex;
+	ParsedShadowParam shadowParam;
+	ParseShadowParam(shadowParam, g_inputDirectionalLightShadowParams[shadowIndex]);
 
-	uint lightCount = GetNumOfDirectionalLight(shadowGlobalParam_packedNumOfShadowAtlasCapacity);
-	shadowUV.x *= rcp((float)lightCount);//(1.0f / (float)lightCount);
-
-	float bias = (float)g_inputDirectionalLightShadowParams[lightIndex].bias;
-	float depth = shadowUV.z
-#if defined(USE_SHADOW_INVERTED_DEPTH) && !defined(USE_VSM)
-		+
-#else
-		-
+	float bias	= shadowParam.bias;
+	float depth	= shadowUV.z;
+	
+	float shadow = 1.0f;
+#ifndef NEVER_USE_VSM
+	if(shadowParam.flag & SHADOW_PARAM_FLAG_USE_VSM)
+		shadow = saturate( VarianceShadow(g_inputDirectionalLightMomentShadowMapAtlas, shadowUV.xy, depth) );
+	else
 #endif
-		bias;
-
-#if defined(USE_VSM)
-	float shadow = saturate( VarianceShadow(g_inputDirectionalLightMomentShadowMapAtlas, shadowUV.xy, depth) );		
+#ifdef USE_SHADOW_INVERTED_DEPTH
+		shadow = saturate( Shadowing(g_inputDirectionalLightShadowMapAtlas, shadowUV.xy, depth + bias) );
 #else
-	float shadow = saturate( Shadowing(g_inputDirectionalLightShadowMapAtlas, shadowUV.xy, depth) );
+		shadow = saturate( Shadowing(g_inputDirectionalLightShadowMapAtlas, shadowUV.xy, depth - bias) );
 #endif
-	float3 shadowColor = g_inputDirectionalLightShadowColors[lightIndex].rgb;
+
+	float3 shadowColor = shadowParam.color.rgb;
 	float3 result = lerp((float3(1.0f, 1.0f, 1.0f) - shadow.xxx) * shadowColor, float3(1.0f, 1.0f, 1.0f), shadow);
 
-	float shadowStrength = g_inputDirectionalLightShadowColors[lightIndex].a;
+	float shadowStrength = shadowParam.color.a;
 	return lerp(float3(1.0f, 1.0f, 1.0f), result, shadowStrength);
 }
 
@@ -155,50 +158,51 @@ uint ComputeFaceIndex(float3 dir)
 
 float3 RenderPointLightShadow(uint lightIndex, float3 vertexWorldPos, float3 lightDir, float shadowDistanceTerm)
 {
-	uint faceIndex = ComputeFaceIndex(-lightDir);
+	uint faceIndex		= ComputeFaceIndex(-lightDir);
+	uint shadowIndex	= g_inputPointLightShadowIndex[lightIndex];
 
-	float4 shadowUV = mul( float4(vertexWorldPos, 1.0f), g_inputPointLightShadowParams[lightIndex].viewProjMat[faceIndex] );
+	float4 shadowUV = mul( float4(vertexWorldPos, 1.0f), g_inputPointLightShadowViewProjMatrix[shadowIndex].mat[faceIndex] );
 	shadowUV /= shadowUV.w;
 
 	shadowUV.x = (shadowUV.x / 2.0f) + 0.5f;
 	shadowUV.y = (shadowUV.y /-2.0f) + 0.5f;
 
 	float oneShadowMapSize	= float(1 << GetNumOfPointLight(shadowGlobalParam_packedPowerOfTwoShadowResolution));
-	float underScanSize		= g_inputPointLightShadowParams[lightIndex].underScanSize;
+	float underScanSize		= asfloat(g_inputPointLightShadowParams[shadowIndex].z);
+
+	ParsedShadowParam shadowParam;
+	ParseShadowParam(shadowParam, g_inputPointLightShadowParams[shadowIndex].xy);
+
 	shadowUV.xy *= ((oneShadowMapSize - (2.0f * underScanSize)) / oneShadowMapSize).xx;
 	shadowUV.xy += (underScanSize / oneShadowMapSize).xx;
 
 	shadowUV.y += (float)faceIndex;
 	shadowUV.y *= rcp(6); //1.0f / 6.0f;
 
-	uint idxWithBias = g_inputPointLightShadowParams[lightIndex].indexWithBias;
-
-	uint shadowIndex = (idxWithBias >> 16) - 1;
-	shadowUV.x += (float)shadowIndex;
-
 	uint lightCount = GetNumOfPointLight(shadowGlobalParam_packedNumOfShadowAtlasCapacity);
+
+	shadowUV.x += (float)shadowIndex;
 	shadowUV.x *= rcp((float)lightCount);//(1.0f / (float)lightCount);
 
-	// 65536 is 2^16
-	float bias = float(idxWithBias & 0x0000ffff) * rcp(65536.0f);
-	float depth = shadowUV.z
-#if defined(USE_SHADOW_INVERTED_DEPTH) && !defined(USE_VSM)
-		+
-#else
-		-
+	float bias	= lerp(10.0f, 1.0f, saturate(5 * shadowDistanceTerm)) * shadowParam.bias;
+	float depth	= shadowUV.z;
+	
+	float shadow = 1.0f;
+#ifndef NEVER_USE_VSM
+	if(shadowParam.flag & SHADOW_PARAM_FLAG_USE_VSM)
+		shadow = saturate( VarianceShadow(g_inputPointLightMomentShadowMapAtlas, shadowUV.xy, depth) );
+	else
 #endif
-		lerp(10.0f, 1.0f, saturate(5 * shadowDistanceTerm)) * bias;
-
-#if defined(USE_VSM)
-	float shadow = saturate( VarianceShadow(g_inputPointLightMomentShadowMapAtlas, shadowUV.xy, depth) );
+#ifdef USE_SHADOW_INVERTED_DEPTH
+		shadow = saturate( Shadowing(g_inputPointLightShadowMapAtlas, shadowUV.xy, depth + bias) );
 #else
-	float shadow = saturate( Shadowing(g_inputPointLightShadowMapAtlas, shadowUV.xy, depth) );
+		shadow = saturate( Shadowing(g_inputPointLightShadowMapAtlas, shadowUV.xy, depth - bias) );
 #endif
 
-	float3 shadowColor = g_inputPointLightShadowColors[lightIndex].rgb;
+	float3 shadowColor = shadowParam.color.rgb;
 	float3 result = lerp((float3(1.0f, 1.0f, 1.0f) - shadow.xxx) * shadowColor, float3(1.0f, 1.0f, 1.0f), shadow);
 
-	float shadowStrength = g_inputPointLightShadowColors[lightIndex].a;
+	float shadowStrength = shadowParam.color.a;
 	return lerp(float3(1.0f, 1.0f, 1.0f), result, shadowStrength);
 }
 
