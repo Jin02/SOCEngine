38953967070d9e54f79eaae19390322b319786d7
Jin <quaggan02@gmail.com>
2016-01-14 07:29:32 +0900

    ShadowCommon - 1. CommonParam 변경    1. bias, index를 lightIndexWithBiasWithFlag, color로 변경 2. Projection 계산과 관련한 기능들 모두 추가 3. MakeParam 변경
    #6

--- SOCEngine/SOCEngine/Rendering/Shadow/ShadowCommon.cpp
+++ SOCEngine/SOCEngine/Rendering/Shadow/ShadowCommon.cpp
@@ -9,8 +9,9 @@ using namespace Rendering::Shadow;
 using namespace Core;
 using namespace Rendering;
 
-ShadowCommon::ShadowCommon(const LightForm* owner, const std::function<void()>& ownerUpdateCounter) 
-	: _owner(owner), _bias(0.001f), _ownerAddUpdateCounter(ownerUpdateCounter)
+ShadowCommon::ShadowCommon(const LightForm* owner) 
+	: _owner(owner), _bias(0.001f), _projNear(0.1f),
+	_paramUpdateCounter(0), _transformUpdateCounter(0), _useVSM(false)
 {
 	_color = Color::Black();
 	_color.a = 0.5f;
@@ -24,29 +25,36 @@ void ShadowCommon::Initialize()
 {
 }
 
-void ShadowCommon::MakeParam(CommonParam& outParam) const
+void ShadowCommon::MakeParam(CommonParam& outParam, uint lightIndex) const
 {
-	outParam.bias		= _bias;//Common::FloatToHalf(_bias);
-	outParam.index		= FetchShadowCastingLightIndex() + 1;//Common::FloatToHalf( (float)FetchShadowCastingLightIndex() );
-}
+	uint index	= lightIndex << 16;
+	uint bias	= (uint(_bias * 4096.0f) & 0x0fff) << 4;
+	uint flag	= uint(_useVSM) & 0xf;
 
-ushort ShadowCommon::FetchShadowCastingLightIndex() const
-{
-	Scene* scene = Device::Director::SharedInstance()->GetCurrentScene();
-	ShadowRenderer* shadowManager = scene->GetShadowManager();
-
-	const LightForm* owner = _owner;
-	return shadowManager->FetchShadowCastingLightIndex(owner);
+	outParam.lightIndexWithBiasWithFlag = index | bias | flag;
+	outParam.color						= _color.Get32BitUintColor();
 }
 
 void ShadowCommon::SetBias(float bias)
 {
 	_bias = bias;
-	_ownerAddUpdateCounter();
+	++_paramUpdateCounter;
+}
+
+void ShadowCommon::SetUseVSM(bool b)
+{
+	_useVSM = b;
+	++_paramUpdateCounter;
 }
 
 void ShadowCommon::SetColor(const Color& color)
 {
 	_color = color;
-	_ownerAddUpdateCounter();
+	++_paramUpdateCounter;
+}
+
+void ShadowCommon::SetProjNear(float n)
+{
+	_projNear = n;
+	++_transformUpdateCounter;
 }
--- SOCEngine/SOCEngine/Rendering/Shadow/ShadowCommon.h
+++ SOCEngine/SOCEngine/Rendering/Shadow/ShadowCommon.h
@@ -7,6 +7,8 @@
 
 #include "GlobalDefine.h"
 
+#include "BoundBox.h"
+
 namespace Rendering
 {
 	namespace Light
@@ -21,39 +23,52 @@ namespace Rendering
 		public:
 			struct CommonParam
 			{
-				float	bias;
-				uint	index;
-
-				CommonParam() : index(0), bias(0) {}
-				~CommonParam() {}
+				uint lightIndexWithBiasWithFlag;
+				uint color;
 			};
-
-		private:
-			std::function<void()>		_ownerAddUpdateCounter;
 
 		private:
 			Color						_color; //a is strength
 			float						_bias;
+			float						_projNear;
+			uint						_paramUpdateCounter;
+			bool						_useVSM;
 
 		protected:
 			const Light::LightForm*		_owner;
+			Math::Matrix				_invNearFarViewProjMat;
+			Math::Matrix				_viewProjMat;
+			Math::Matrix				_invViewProjViewportMat;
+
+			uint						_transformUpdateCounter;
 
 		public:
-			ShadowCommon(const Light::LightForm* owner, const std::function<void()>& ownerUpdateCounter);
+			ShadowCommon(const Light::LightForm* owner);
 			~ShadowCommon();
-
-		protected:
-			void MakeParam(CommonParam& outParam) const;
 
 		public:
 			void Initialize();
-			ushort FetchShadowCastingLightIndex() const;
 
+		public:
+			void MakeParam(CommonParam& outParam, uint lightIndex) const;
+
+		public:
 			void SetBias(float bias);
-			GET_ACCESSOR(Bias, float, _bias);
-
+			void SetProjNear(float n);
 			void SetColor(const Color& color);
-			GET_ACCESSOR(Color, const Color&, _color);
+			void SetUseVSM(bool b);
+
+			GET_ACCESSOR(Bias,								float,						_bias);
+			GET_ACCESSOR(Color,								const Color&,				_color);
+			GET_ACCESSOR(ProjectionNear,					float,						_projNear);
+			GET_ACCESSOR(UseVSM,							bool,						_useVSM);
+
+			GET_ACCESSOR(InvNearFarViewProjectionMatrix,	const Math::Matrix&,		_invNearFarViewProjMat);
+			GET_ACCESSOR(ViewProjectionMatrix,				const Math::Matrix&,		_viewProjMat);
+			GET_ACCESSOR(Owner,								const Light::LightForm*,	_owner);
+
+			GET_ACCESSOR(ParamUpdateCounter,				uint,						_paramUpdateCounter);
+			GET_ACCESSOR(TransformUpdateCounter,			uint,						_transformUpdateCounter);
 		};
 	}
 }
