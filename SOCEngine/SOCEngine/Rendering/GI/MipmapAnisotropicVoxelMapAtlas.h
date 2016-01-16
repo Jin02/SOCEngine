1e3573106529501c0bedda34537bee9c87f70867
Jin <quaggan02@gmail.com>
2016-01-17 00:16:26 +0900

    MipmapAnisotropicVoxelMapAtlas 추가 #2

---
+++ SOCEngine/SOCEngine/Rendering/GI/MipmapAnisotropicVoxelMapAtlas.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "ComputeShader.h"
+#include "ConstBuffer.h"
+#include "GlobalIlluminationCommon.h"
+#include "AnisotropicVoxelMapAtlas.h"
+#include "InjectRadiance.h"
+
+namespace Rendering
+{
+	namespace GI
+	{
+		class MipmapAnisotropicVoxelMapAtlas
+		{
+		public:
+			struct InfoCB
+			{
+				uint sourceDimension;
+				uint currentCascade;
+				uint dummy1, dummy2;
+			};
+
+		private:
+			GPGPU::DirectCompute::ComputeShader*		_shader;
+			Buffer::ConstBuffer*						_infoCB;
+
+		public:
+			MipmapAnisotropicVoxelMapAtlas();
+			~MipmapAnisotropicVoxelMapAtlas();
+
+		public:
+			void Initialize();
+			void Mipmapping(const Device::DirectX* dx, const InjectRadiance* injection, uint maxNumOfCascade);
+			void Destroy();
+		};
+	}
+}
