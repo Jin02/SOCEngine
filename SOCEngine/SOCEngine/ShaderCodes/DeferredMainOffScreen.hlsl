1e92b177825f66d4c00cb3b3b93bdfbc97197ae7
Jin <quaggan02@gmail.com>
2016-01-20 09:05:18 +0900

    DeferredMainOffScreen - IndirectColorMap 추가, 우선 테스트를 위해 USE_GI가 정의되있다면 indirectColorMap만 샘플링해서 넘기도록 구현함 #2

--- SOCEngine/SOCEngine/ShaderCodes/DeferredMainOffScreen.hlsl
+++ SOCEngine/SOCEngine/ShaderCodes/DeferredMainOffScreen.hlsl
@@ -2,10 +2,21 @@
 
 #include "FullScreenShader.h"
 
-SamplerState linerSamplerState : register( s0 );
-Texture2D<float4> offScreenBuffer : register( t0 );
+SamplerState linerSamplerState			: register( s0 );
+Texture2D<float4> directLightColorMap	: register( t0 );
+
+#ifdef USE_GI
+Texture2D<float4> indirectLightColorMap	: register( t1 );
+#endif
 
 float4 PS( PS_INPUT input ) : SV_Target
 {
-	return offScreenBuffer.Sample(linerSamplerState, input.uv);
+#ifdef USE_GI
+	float4 direct	= directLightColorMap.Sample(linerSamplerState, input.uv);
+	float4 indirect	= indirectLightColorMap.Sample(linerSamplerState, input.uv);
+
+	return indirect;//test
+#else
+	return directLightColorMap.Sample(linerSamplerState, input.uv);
+#endif
 }
