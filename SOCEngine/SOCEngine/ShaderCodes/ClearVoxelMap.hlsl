d64535091ace7bd016aecd924ba92ed43410705b
Jin <quaggan02@gmail.com>
2016-01-22 00:02:43 +0900

    ClearVoxelMap - 기존 3개씩 제거하던 VoxelMap에서 한개만 클리어 하도록 수정

--- SOCEngine/SOCEngine/ShaderCodes/ClearVoxelMap.hlsl
+++ SOCEngine/SOCEngine/ShaderCodes/ClearVoxelMap.hlsl
@@ -1,15 +1,11 @@
 //EMPTY_META_DATA
 
-#define USE_OUT_ANISOTROPIC_VOXEL_TEXTURES
-
-#include "Voxelization_Common.h"
+RWTexture3D<uint> target : register( u0 );
 
 [numthreads(8, 8, 8)]
-void ClearVoxelMapCS(	uint3 globalIdx : SV_DispatchThreadID, 
-						uint3 localIdx	: SV_GroupThreadID,
-						uint3 groupIdx	: SV_GroupID)
+void CS(uint3 globalIdx : SV_DispatchThreadID, 
+		uint3 localIdx	: SV_GroupThreadID,
+		uint3 groupIdx	: SV_GroupID)
 {
-	OutAnistropicVoxelAlbedoTexture[globalIdx]		= 0;
-	OutAnistropicVoxelNormalTexture[globalIdx]		= 0;
-	OutAnistropicVoxelEmissionTexture[globalIdx]	= 0;
+	target[globalIdx] = 0;
 }
