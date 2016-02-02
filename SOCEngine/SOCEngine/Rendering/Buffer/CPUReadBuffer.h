e06ae4f8e1a4627129eba0dc8244b43d833de51b
Jin <quaggan02@gmail.com>
2016-02-02 09:38:45 +0900

    CPUReadBuffer - UAV, read 전용 버퍼 생성

--- SOCEngine/SOCEngine/Rendering/Buffer/CPUReadBuffer.h
+++ SOCEngine/SOCEngine/Rendering/Buffer/CPUReadBuffer.h
@@ -2,6 +2,7 @@
 
 #include "BaseBuffer.h"
 #include <functional>
+#include "UnorderedAccessView.h"
 
 namespace Rendering
 {
@@ -11,19 +12,24 @@ namespace Rendering
 
 		class CPUReadBuffer : private BaseBuffer
 		{
+		private:
+			ID3D11Buffer*				_readBuffer;
+			View::UnorderedAccessView*	_uav;
+
 		public:
 			CPUReadBuffer();
 			virtual ~CPUReadBuffer();
 
 		public:
-			bool Initialize(unsigned int stride, unsigned int num);
-			void Read(ID3D11DeviceContext* context, ID3D11Resource* target, const std::function<void(const void* dataRecive)>& dataReceiveFunc);
+			bool Initialize(uint stride, uint num, DXGI_FORMAT format);
+			void Read(ID3D11DeviceContext* context, const std::function<void(const void* dataRecive)>& dataReceiveFunc);
 
 		private:
 			void Update(ID3D11DeviceContext* context, const void* data){}
 
 		public:
-			GET_ACCESSOR(Buffer, ID3D11Buffer*, _buffer);
+			GET_ACCESSOR(Buffer,	ID3D11Buffer*,							_buffer);
+			GET_ACCESSOR(UAV,		const View::UnorderedAccessView*,		_uav);
 		};
 
 	}
