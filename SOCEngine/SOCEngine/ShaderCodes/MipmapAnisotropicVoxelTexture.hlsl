ed1c8d4d2557020b573b7ed1ff367ef5574e75d9
Jin <quaggan02@gmail.com>
2016-02-08 14:57:53 +0900

    MipmapAnisotropicVoxelTexture - 코드 정리 및 Blending 버그 수정

--- SOCEngine/SOCEngine/ShaderCodes/MipmapAnisotropicVoxelTexture.hlsl
+++ SOCEngine/SOCEngine/ShaderCodes/MipmapAnisotropicVoxelTexture.hlsl
@@ -19,7 +19,7 @@ float4 AlphaBlending(float4 front, float4 back)
 	//알파는 블랜딩 시키지 않음.
 	float alpha = (front.a + back.a) / 2.0f;
 
-	return float4(blending * alpha, alpha);
+	return float4(blending, alpha);
 }
 
 uint AlphaBledningAnisotropicVoxelMap
@@ -31,14 +31,22 @@ uint AlphaBledningAnisotropicVoxelMap
 	float4 color2 = AlphaBlending(front2, back2);
 	float4 color3 = AlphaBlending(front3, back3);
 
-	return Float4ColorToUint( (color0 + color1 + color2 + color3) * 0.25f );
+	color0.rgb *= color0.a;
+	color1.rgb *= color1.a;
+	color2.rgb *= color2.a;
+	color3.rgb *= color3.a;
+
+	float4 result = color0 + color1 + color2 + color3;
+	result.rgb /= result.a;
+	result.a /= 4.0f;
+
+	return Float4ColorToUint( result );
+//	return Float4ColorToUint( (color0 + color1 + color2 + color3) * 0.25f );
 }
 
 float4 GetColorFromVoxelMap(uint3 voxelIdx, uniform uint faceIndex)
 {
-#if defined(ANISOTROPIC_MIPMAP)
 	voxelIdx.x += (faceIndex * mipmapInfo_sourceDimension);
-#endif
 	voxelIdx.y += (mipmapInfo_currentCascade * mipmapInfo_sourceDimension);
 	return RGBA8UintColorToFloat4(g_inputVoxelMap[voxelIdx]);
 }
@@ -62,32 +70,32 @@ void MipmapAnisotropicVoxelMapCS(uint3 globalIdx : SV_DispatchThreadID,
 		( lowerMipIdx + uint3(1, 1, 1) ) 
 	};
 
-	uint posx = AlphaBledningAnisotropicVoxelMap(
+	uint negx = AlphaBledningAnisotropicVoxelMap(
 		GetColorFromVoxelMap(sampleIdx[0], 0), GetColorFromVoxelMap(sampleIdx[2], 0), GetColorFromVoxelMap(sampleIdx[4], 0), GetColorFromVoxelMap(sampleIdx[6], 0),	//Fronts
 		GetColorFromVoxelMap(sampleIdx[1], 0), GetColorFromVoxelMap(sampleIdx[3], 0), GetColorFromVoxelMap(sampleIdx[5], 0), GetColorFromVoxelMap(sampleIdx[7], 0)	//Backs
 	);
 
-	uint negx = AlphaBledningAnisotropicVoxelMap(
+	uint posx = AlphaBledningAnisotropicVoxelMap(
 		GetColorFromVoxelMap(sampleIdx[1], 1), GetColorFromVoxelMap(sampleIdx[3], 1), GetColorFromVoxelMap(sampleIdx[5], 1), GetColorFromVoxelMap(sampleIdx[7], 1),	//Fronts
 		GetColorFromVoxelMap(sampleIdx[0], 1), GetColorFromVoxelMap(sampleIdx[2], 1), GetColorFromVoxelMap(sampleIdx[4], 1), GetColorFromVoxelMap(sampleIdx[6], 1)	//Backs
 	);
 
-	uint posy = AlphaBledningAnisotropicVoxelMap(
+	uint negy = AlphaBledningAnisotropicVoxelMap(
 		GetColorFromVoxelMap(sampleIdx[0], 2), GetColorFromVoxelMap(sampleIdx[1], 2), GetColorFromVoxelMap(sampleIdx[4], 2), GetColorFromVoxelMap(sampleIdx[5], 2),	//Fronts
 		GetColorFromVoxelMap(sampleIdx[2], 2), GetColorFromVoxelMap(sampleIdx[3], 2), GetColorFromVoxelMap(sampleIdx[6], 2), GetColorFromVoxelMap(sampleIdx[7], 2)	//Backs
 	);
 	
-	uint negy = AlphaBledningAnisotropicVoxelMap(
+	uint posy = AlphaBledningAnisotropicVoxelMap(
 		GetColorFromVoxelMap(sampleIdx[2], 3), GetColorFromVoxelMap(sampleIdx[3], 3), GetColorFromVoxelMap(sampleIdx[6], 3), GetColorFromVoxelMap(sampleIdx[7], 3),	//Fronts
 		GetColorFromVoxelMap(sampleIdx[0], 3), GetColorFromVoxelMap(sampleIdx[1], 3), GetColorFromVoxelMap(sampleIdx[4], 3), GetColorFromVoxelMap(sampleIdx[5], 3)	//Backs
 	);
 
-	uint posz = AlphaBledningAnisotropicVoxelMap(
+	uint negz = AlphaBledningAnisotropicVoxelMap(
 		GetColorFromVoxelMap(sampleIdx[0], 4), GetColorFromVoxelMap(sampleIdx[1], 4), GetColorFromVoxelMap(sampleIdx[2], 4), GetColorFromVoxelMap(sampleIdx[3], 4),	//Fronts
 		GetColorFromVoxelMap(sampleIdx[4], 4), GetColorFromVoxelMap(sampleIdx[5], 4), GetColorFromVoxelMap(sampleIdx[6], 4), GetColorFromVoxelMap(sampleIdx[7], 4)	//Backs
 	);
 	
-	uint negz = AlphaBledningAnisotropicVoxelMap(
+	uint posz = AlphaBledningAnisotropicVoxelMap(
 		GetColorFromVoxelMap(sampleIdx[4], 5), GetColorFromVoxelMap(sampleIdx[5], 5), GetColorFromVoxelMap(sampleIdx[6], 5), GetColorFromVoxelMap(sampleIdx[7], 5),	//Fronts
 		GetColorFromVoxelMap(sampleIdx[0], 5), GetColorFromVoxelMap(sampleIdx[1], 5), GetColorFromVoxelMap(sampleIdx[2], 5), GetColorFromVoxelMap(sampleIdx[3], 5)	//Backs
 	);
@@ -96,10 +104,10 @@ void MipmapAnisotropicVoxelMapCS(uint3 globalIdx : SV_DispatchThreadID,
 	uint destDimension = mipmapInfo_sourceDimension / 2;
 	globalIdx.y += mipmapInfo_currentCascade * destDimension;
 
-	g_outputMipMap[uint3(globalIdx.x,						globalIdx.yz)]	= posx;
-	g_outputMipMap[uint3(globalIdx.x + (1 * destDimension),	globalIdx.yz)]	= negx;
-	g_outputMipMap[uint3(globalIdx.x + (2 * destDimension),	globalIdx.yz)]	= posy;
-	g_outputMipMap[uint3(globalIdx.x + (3 * destDimension),	globalIdx.yz)]	= negy;
-	g_outputMipMap[uint3(globalIdx.x + (4 * destDimension),	globalIdx.yz)]	= posz;
-	g_outputMipMap[uint3(globalIdx.x + (5 * destDimension),	globalIdx.yz)]	= negz;
+	g_outputMipMap[uint3(globalIdx.x,						globalIdx.yz)]	= negx;
+	g_outputMipMap[uint3(globalIdx.x + (1 * destDimension),	globalIdx.yz)]	= posx;
+	g_outputMipMap[uint3(globalIdx.x + (2 * destDimension),	globalIdx.yz)]	= negy;
+	g_outputMipMap[uint3(globalIdx.x + (3 * destDimension),	globalIdx.yz)]	= posy;
+	g_outputMipMap[uint3(globalIdx.x + (4 * destDimension),	globalIdx.yz)]	= negz;
+	g_outputMipMap[uint3(globalIdx.x + (5 * destDimension),	globalIdx.yz)]	= posz;
 }
