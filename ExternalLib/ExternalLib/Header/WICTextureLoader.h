2746cc6097f461fb43be76fc14f80c85869bf3a6
Jin <quaggan02@gmail.com>
2016-02-21 02:01:35 +0900

    SOCEngine/External에 있던 코드들을 따로 프로젝트로 만듬

--- ExternalLib/ExternalLib/Header/WICTextureLoader.h
+++ ExternalLib/ExternalLib/Header/WICTextureLoader.h
@@ -27,6 +27,9 @@
 
 #pragma once
 
+#include <WinSDKVer.h>
+#include <SDKDDKVer.h>
+
 #if defined(WINAPI_FAMILY) && (WINAPI_FAMILY == WINAPI_FAMILY_PHONE_APP) && (_WIN32_WINNT <= _WIN32_WINNT_WIN8)
 #error WIC is not supported on Windows Phone 8.0
 #endif
