25d59c21325e51c2559fa2412d32a6a40b4b1eba
Jin <quaggan02@gmail.com>
2016-03-03 15:53:59 +0900

    IBLPass.hlsl - 구현 중

--- SOCEngine/SOCEngine/ShaderCodes/IBLPass.hlsl
+++ SOCEngine/SOCEngine/ShaderCodes/IBLPass.hlsl
@@ -1,10 +1,16 @@
-//NOT_CREATE_META_DATA
+//EMPTY_META_DATA
+
+#define USE_IMPORTANCE_DIFFUSE_IBL
+#define USE_APPROXIMATE_SPECULAR_IBL
 
 #include "FullScreenShader.h"
 #include "GBufferParser.h"
 #include "EnvIBL.h"
 
-Texture2D<float2> preIntegrateEnvBRDFMap : register( t29 );
+Texture2D<float2> preIntegrateEnvBRDFMap	: register( t29 );
+Texture2D<float4> illuminationMap			: register( t30 );
+
+SamplerState linerSamplerState				: register( s0 );
 
 float ComputeCubeMapMipFromRoughness(float roughness, float mipCount)
 {
@@ -23,21 +29,22 @@ float4 PS( PS_INPUT input ) : SV_Target
 	ParseGBufferSurface(surface, screenPt, 0);
 #endif
 
-	float3 normal	= surface.normal;
-	float3 viewDir	= normalize( tbrParam_cameraWorldPosition - surface.worldPos );
+	float3 normal		= surface.normal;
+	float3 viewDir		= normalize( tbrParam_cameraWorldPosition - surface.worldPos );
+	float3 reflectDir	= reflect(-viewDir, normal);
 
-	float3 diffuse	= 0;
-	float3 specular	= 0;
+	float3 diffuse	= float3(0.0f, 0.0f, 0.0f);
+	float3 specular	= float3(0.0f, 0.0f, 0.0f);
 
 	// Diffuse
 	{
-#ifndef USE_IMPORTANCE_DIFFUSE
+#ifdef USE_IMPORTANCE_DIFFUSE_IBL
+		diffuse = DiffuseIBL(surface.albedo, surface.roughness, surface.normal, viewDir, BRDF_SAMPLES);
+#else
 		float absDiffuseMip = skyMapInfoParam_maxMipCount;
 		float3 diffsueLookUp = skyCubeMap.SampleLevel(skyCubeMapSampler, normal, absDiffuseMip).rgb;
 		
 		diffuse = surface.albedo * diffsueLookUp;
-#else
-		diffuse = DiffuseIBL(surface.albedo, surface.roughness, surface.normal, viewDir, BRDF_SAMPLES);
 #endif
 	}
 
@@ -45,14 +52,17 @@ float4 PS( PS_INPUT input ) : SV_Target
 	{
 #ifdef USE_APPROXIMATE_SPECULAR_IBL
 		specular = ApproximateSpecularIBL(preIntegrateEnvBRDFMap, surface.specular,
-											surface.roughness, surface.normal, viewDir, true);
+											surface.roughness, surface.normal, viewDir,
+											true);
 #else
 		specular = SpecularIBL(surface.specular, surface.roughness, surface.normal, viewDir, BRDF_SAMPLES);
 #endif
 	}
 
 	// float ao = surface.ao * screenSpace.ao;
+	bool existDepth = surface.depth > 0.0f;
 
-	float3 result = (diffuse + specular);
+	float4 illumination = illuminationMap.Sample(linerSamplerState, input.uv);
+	float3 result = (diffuse + specular) * float(existDepth);
 	return float4(result, 1.0f);
 }
