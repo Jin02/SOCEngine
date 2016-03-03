815eb0b192e4d413a235c7b5b97f62dccb116d34
Jin <quaggan02@gmail.com>
2016-03-03 15:53:29 +0900

    FinalScreenMerger - MSAA 관련 코드 제거

--- SOCEngine/SOCEngine/ShaderCodes/FinalScreenMerger.hlsl
+++ SOCEngine/SOCEngine/ShaderCodes/FinalScreenMerger.hlsl
@@ -3,13 +3,8 @@
 #include "FullScreenShader.h"
 #include "ShaderCommon.h"
 
-SamplerState renderSceneSamplerState	: register( s0 );
-
-#if (MSAA_SAMPLES_COUNT > 1)
-Texture2DMS<float4, MSAA_SAMPLES_COUNT>	renderScene		: register( t0 );
-#else
-Texture2D<float4>						renderScene		: register( t0 );
-#endif
+SamplerState		renderSceneSamplerState	: register( s0 );
+Texture2D<float4>	renderScene				: register( t0 );
 
 #if defined(USE_UI)
 SamplerState uiSceneSamplerState		: register( s1 );
@@ -19,12 +14,7 @@ Texture2D<float4> uiScene				: register( t1 );
 float4 PS( PS_INPUT input ) : SV_Target
 {
 	float4 finalColor = float4(0.0f, 0.0f, 0.0f, 0.0f);
-#if (MSAA_SAMPLES_COUNT > 1)
-	int2 texCoord = int2((int)(input.uv.x * GetViewportSize().x), (int)(input.uv.y * GetViewportSize().y));
-	float4 renderSceneColor = renderScene.Load(texCoord.xy, input.sampleIdx);
-#else
 	float4 renderSceneColor = renderScene.Sample(renderSceneSamplerState, input.uv);
-#endif
 
 #if defined(USE_UI)
 	float4 uiSceneColor = uiScene.Sample(uiSceneSamplerState, input.uv);
