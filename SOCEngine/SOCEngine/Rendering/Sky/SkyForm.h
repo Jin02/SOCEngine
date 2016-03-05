5f9a46d3ce7d8c44dc0bb48a77ad59c9d3e7f081
Jin <quaggan02@gmail.com>
2016-03-06 08:07:16 +0900

    SkyForm - 1. Mesh를 사용하지 않고 MeshFilter만 사용 2. Type 추가 3. ReflectionProbe를 이용한 Render 추가 4. _Render는 직접 메테리얼을 받아와 렌더링 할 수 있도록 수정함

--- SOCEngine/SOCEngine/Rendering/Sky/SkyForm.h
+++ SOCEngine/SOCEngine/Rendering/Sky/SkyForm.h
@@ -6,6 +6,10 @@
 #include "RenderTexture.h"
 #include "DepthBuffer.h"
 
+#include "DepthBufferCube.h"
+#include "TextureCube.h"
+#include "ReflectionProbe.h"
+
 namespace Rendering
 {
 	namespace Sky
@@ -29,7 +33,7 @@ namespace Rendering
 			};
 
 		private:
-			Geometry::Mesh*				_mesh;
+			Geometry::MeshFilter*		_meshFilter;
 			Buffer::ConstBuffer*		_skyMapInfoCB;
 
 			SkyMapInfoCBData			_prevCBData;
@@ -39,27 +43,29 @@ namespace Rendering
 			bool						_isSkyLightOn;
 			bool						_isDynamicSkyLight;
 			float						_blendFraction;
+			Type						_type;
 
 		public:
-			SkyForm();
+			SkyForm(Type type);
 			virtual ~SkyForm();
 
 		protected:
-			void Initialize(const Material* skyMaterial);
-			void _Render(const Device::DirectX* dx, const Texture::RenderTexture* renderTarget, const Texture::DepthBuffer* opaqueDepthBuffer);
+			void Initialize();
+			void _Render(const Device::DirectX* dx, const Material* material, ID3D11RenderTargetView* rtv, ID3D11DepthStencilView* dsv);
 
 		public:
 			virtual void Render(const Device::DirectX* dx, const Camera::CameraForm* camera, const Texture::RenderTexture* renderTarget, const Texture::DepthBuffer* opaqueDepthBuffer) = 0;
+			virtual void Render(const Device::DirectX* dx, const Camera::ReflectionProbe* probe, const Texture::TextureCube* renderTarget, const Texture::DepthBufferCube* opaqueDepthBuffer) = 0;
+
 			virtual void Destroy();
 
 			void UpdateConstBuffer(const Device::DirectX* dx);
 
 		public:
-			const Texture::Texture2D* GetSkyCubeMap() const;
-
 			GET_SET_ACCESSOR(BlendFraction,		float,					_blendFraction);
 			GET_SET_ACCESSOR(IsSkyOn,			bool,					_isSkyLightOn);
 			GET_ACCESSOR(SkyMapInfoConstBuffer,	Buffer::ConstBuffer*,	_skyMapInfoCB);
+			GET_ACCESSOR(Type,					Type,					_type);
 		};
 	}
 }
