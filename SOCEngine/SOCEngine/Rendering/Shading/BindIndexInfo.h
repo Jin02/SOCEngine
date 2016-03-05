b7070e6fc14a474aeba2b28641bb5ff730c1859d
Jin <quaggan02@gmail.com>
2016-03-05 18:01:51 +0900

    BindIndexInfo - ReflectionProbe_ViewProjs -> ReflectionProbe_Info

--- SOCEngine/SOCEngine/Rendering/Shading/BindIndexInfo.h
+++ SOCEngine/SOCEngine/Rendering/Shading/BindIndexInfo.h
@@ -75,7 +75,7 @@ namespace Rendering
 		PhysicallyBasedMaterial			= 3,
 		ShadowGlobalParam				= 4,
 
-		ReflectionProbe_ViewProjs		= 5,	
+		ReflectionProbe_Info			= 5,	
 
 		Voxelization_InfoCB				= 5,
 		GlobalIIllumination_InfoCB		= 6,
