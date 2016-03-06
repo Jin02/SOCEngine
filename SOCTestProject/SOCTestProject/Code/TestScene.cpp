0f5d6c42adb7f02238e3b6fe7d34c3851dd67fc8
Jin <quaggan02@gmail.com>
2016-03-06 23:14:10 +0900

    test

--- SOCTestProject/SOCTestProject/Code/TestScene.cpp
+++ SOCTestProject/SOCTestProject/Code/TestScene.cpp
@@ -34,159 +34,64 @@ TestScene::~TestScene(void)
 
 void TestScene::OnInitialize()
 {
-//	ActivateGI(true, 256, 40.0f);
-
-	// SkyBox
-	ActiveSkyBox("@Skybox", "Resources/CubeMap/desertcube1024.dds");
-
 	_camera = new Object("Default");
 	MeshCamera* cam = _camera->AddComponent<MeshCamera>();
-	_camera->GetTransform()->UpdatePosition(Vector3(0, 0, 0));
-//	_camera->GetTransform()->UpdatePosition(Vector3(0, -0.3f, -4.1f));
-	cam->SetFieldOfViewDegree(60.0f);
+
+#if 1 //GI Test
+	ActivateGI(true, 256, 50.0f);
 
 	const ResourceManager* resourceMgr	= ResourceManager::SharedInstance();
 	Importer::MeshImporter* importer	= resourceMgr->GetMeshImporter();
-
-#if 0
-	_testObject = importer->Load("./Resources/Capsule/capsule.obj");
-	_testObject->GetTransform()->UpdatePosition(Vector3(0, 0, 5));
-	AddObject(_testObject);
-#else
-	BasicGeometryGenerator gen;
-	uint flag = uint(RenderManager::DefaultVertexInputTypeFlag::UV0) |
-				uint(RenderManager::DefaultVertexInputTypeFlag::NORMAL);
-//	_testObject = gen.CreatePlane(0.5f, 0.5f, 4, 4, flag);
-//	_testObject = gen.CreateCylinder(0.5f, 0.5f, 0.5f, 30, 30, flag);
-//	_testObject = gen.CreateBox(Vector3(0.5f, 0.5f, 0.5f), flag);
-	_testObject = gen.CreateSphere(1.0f, 30, 30, flag);
-//	_testObject = importer->Load("./Resources/Sphere/sphere.obj", false);
-	_testObject->GetTransform()->UpdatePosition(Vector3(0, 0.0f, 11.0f));
-	_testObject->GetTransform()->UpdateEulerAngles(Vector3(0.0f, 0.0f, 0.0f));
-	_testObject->GetTransform()->UpdateScale(Vector3(5, 5, 5));
+	_testObject = importer->Load("./Resources/CornellBox/box.obj", false);
+	_testObject->GetTransform()->UpdatePosition(Vector3(0, -4.8f, 17.5f));
+	_testObject->GetTransform()->UpdateEulerAngles(Vector3(-90.0f, 0.0f, 180.0f));
+	_testObject->GetTransform()->UpdateScale(Vector3(5.0f, 5.0f, 5.0f));
 	AddObject(_testObject);
 
-	//_testObject = importer->Load("./Resources/House/SanFranciscoHouse.fbx");
-	//_camera->GetTransform()->UpdatePosition(Vector3(0, 5, -15));
-	//_testObject->GetTransform()->UpdateEulerAngles(Vector3(-90, -90, 0));
-
-	//_testObject = importer->Load("./Resources/CornellBox/box.obj", false);
-	//_testObject->GetTransform()->UpdatePosition(Vector3(0, 0, 0));
-	//_testObject->GetTransform()->UpdateEulerAngles(Vector3(-90, 180.0f, 180));
-	//_testObject->GetTransform()->UpdateScale(Vector3(5, 5, 5));
-	//AddObject(_testObject);
-
-	//_testObject2 = importer->Load("./Resources/CornellBox/Sphere.obj", false);
-	//_testObject2->GetTransform()->UpdatePosition(Vector3(0, -5.5f, 17.2f));
-	//_testObject2->GetTransform()->UpdateEulerAngles(Vector3(-90.0f, 0.0f, 180.0f));
-	//_testObject2->GetTransform()->UpdateScale(Vector3(10, 10, 10));
-	//AddObject(_testObject2);
-	//cam->SetFieldOfViewDegree(75.0f);
-
-	//_testObject2 = importer->Load("./Resources/Voxel/voxel.obj", false);
-	//_testObject2->GetTransform()->UpdatePosition(Vector3(-2.5f, 2.2f, 14.0f));
-	//_testObject2->GetTransform()->UpdateEulerAngles(Vector3(0.0f, 0.0f, 0.0f));
-	//_testObject2->GetTransform()->UpdateScale(Vector3(3, 3, 3));
-	//AddObject(_testObject2);
-
-	{
-		//PhysicallyBasedMaterial* left	= static_cast<PhysicallyBasedMaterial*>(_testObject2->GetChild(0)->GetChild(3)->GetComponent<Rendering::Geometry::Mesh>()->GetMeshRenderer()->GetMaterials().front());
-		//left->UpdateMainColor(Color(1.0f, 0.0f, 0.0f, 1.0f));
-
-		//PhysicallyBasedMaterial* right	= static_cast<PhysicallyBasedMaterial*>(_testObject2->GetChild(0)->GetChild(0)->GetComponent<Rendering::Geometry::Mesh>()->GetMeshRenderer()->GetMaterials().front());
-		//right->UpdateMainColor(Color(0.0f, 0.0f, 1.0f, 1.0f));
-
-		//PhysicallyBasedMaterial* back	= static_cast<PhysicallyBasedMaterial*>(_testObject2->GetChild(0)->GetChild(1)->GetComponent<Rendering::Geometry::Mesh>()->GetMeshRenderer()->GetMaterials().front());
-		//back->UpdateMainColor(Color(0.0f, 1.0f, 1.0f, 1.0f));
-
-		//PhysicallyBasedMaterial* front	= static_cast<PhysicallyBasedMaterial*>(_testObject2->GetChild(0)->GetChild(4)->GetComponent<Rendering::Geometry::Mesh>()->GetMeshRenderer()->GetMaterials().front());
-		//front->UpdateMainColor(Color(1.0f, 1.0f, 0.0f, 1.0f));
-
-		//PhysicallyBasedMaterial* top	= static_cast<PhysicallyBasedMaterial*>(_testObject2->GetChild(0)->GetChild(5)->GetComponent<Rendering::Geometry::Mesh>()->GetMeshRenderer()->GetMaterials().front());
-		//top->UpdateMainColor(Color(1.0f, 1.0f, 1.0f, 1.0f));
-
-		//PhysicallyBasedMaterial* bottom	= static_cast<PhysicallyBasedMaterial*>(_testObject2->GetChild(0)->GetChild(2)->GetComponent<Rendering::Geometry::Mesh>()->GetMeshRenderer()->GetMaterials().front());
-		//bottom->UpdateMainColor(Color(1.0f, 0.0f, 1.0f, 1.0f));
-	}
-
-#endif
-	
-#if 0
 	_light = new Object("Light");
-	//_light->GetTransform()->UpdateEulerAngles(Vector3(60, 60, 0));
-	//_light->GetTransform()->UpdatePosition(Vector3(-1, 1.5,  12));
-	_light->GetTransform()->UpdateEulerAngles(Vector3(0, 0, 0));
-	_light->GetTransform()->UpdatePosition(Vector3(0.5f, -0.8, 6.0f));
-
-	SpotLight* light = _light->AddComponent<SpotLight>();
-	light->SetLumen(150);
-	light->SetRadius(10.0f);
-	light->ActiveShadow(true);
-	light->SetSpotAngleDegree(120.0f);
-	light->GetShadow()->SetUseVSM(false);
-#elif 1
-	_light = new Object("Light");
-//	_light->GetTransform()->UpdateEulerAngles(Vector3(315, 340, 0));
-//	_light->GetTransform()->UpdateEulerAngles(Vector3(90, 0, 0));
-	_light->GetTransform()->UpdateEulerAngles(Vector3(90.0f, 0.0f, 0));
-
-//	_light->GetTransform()->UpdatePosition(Vector3(0, 30, 0));
-	_light->GetTransform()->UpdatePosition(_camera->GetTransform()->GetLocalPosition());
-
-	DirectionalLight* light = _light->AddComponent<DirectionalLight>();
-//	light->SetProjectionSize(10);
-//	light->SetProjectionSize(30);
-	light->SetIntensity(2.0f);
-	light->SetUseAutoProjectionLocation(true);
-	light->ActiveShadow(false);
-#elif 0
-	_light = new Object("Light");
-//	_light->GetTransform()->UpdatePosition(Vector3(0.0f, 0.9f, 8.0f));
-//	_light->GetTransform()->UpdatePosition(Vector3(0.0f, 3.4f, 10.0f));
-	_light->GetTransform()->UpdatePosition(Vector3(0.0f, 5.2f, 12.0f));
+	_light->GetTransform()->UpdatePosition(Vector3(0.0f, 2.0f, 16.0f));
 
 	PointLight* light = _light->AddComponent<PointLight>();
 	light->SetLumen(100);
 	light->SetRadius(30.0f);
 	light->ActiveShadow(true);
 	light->GetShadow()->SetUnderScanSize(0.0f);
-#endif
-
 	AddObject(_light);
+
+#else //IBL Test
+	// SkyBox
+	// ActiveSkyBox("@Skybox", "Resources/CubeMap/desertcube1024.dds");
+	_camera->GetTransform()->UpdatePosition(Vector3(0, 0, 0));
+	cam->SetFieldOfViewDegree(60.0f);	
+
+	BasicGeometryGenerator gen;
+	uint flag = uint(RenderManager::DefaultVertexInputTypeFlag::UV0) |
+				uint(RenderManager::DefaultVertexInputTypeFlag::NORMAL);
+	_testObject = gen.CreateSphere(1.0f, 30, 30, flag);
+	_testObject->GetTransform()->UpdatePosition(Vector3(0, 0.0f, 11.0f));
+	_testObject->GetTransform()->UpdateEulerAngles(Vector3(0.0f, 0.0f, 0.0f));
+	_testObject->GetTransform()->UpdateScale(Vector3(5, 5, 5));
+	AddObject(_testObject);
+
+	_light = new Object("Light");
+	_light->GetTransform()->UpdateEulerAngles(Vector3(90.0f, 0.0f, 0));
+	_light->GetTransform()->UpdatePosition(_camera->GetTransform()->GetLocalPosition());
+
+	DirectionalLight* light = _light->AddComponent<DirectionalLight>();
+	light->SetIntensity(2.0f);
+	light->SetUseAutoProjectionLocation(true);
+	light->ActiveShadow(false);
+	AddObject(_light);
+#endif
 }
 
 void TestScene::OnRenderPreview()
 {
-	if(_globalIllumination)
-	{
-		auto voxelViwer = _globalIllumination->GetDebugVoxelViewer();
-		
-		if(voxelViwer)
-		{
-			Object* debugVoxels = voxelViwer->GetVoxelsParent();
-			if(debugVoxels)
-			{
-				Object* exist = FindObject(debugVoxels->GetName());
-				if( exist == nullptr )
-				{
-					_testObject->SetUse(false);
-
-					AddObject(debugVoxels);
-					_testObject2 = debugVoxels;
-//#ifndef USE_ANISOTROPIC_VOXELIZATION
-					debugVoxels->GetTransform()->UpdatePosition(Vector3(0.0f, -0.3f, -4.1f));
-//#else
-//					debugVoxels->GetTransform()->UpdatePosition(Vector3(0.0f, 1.8f, -3.1f));
-//#endif
-				}
-			}
-		}
-	}
 }
 
 void TestScene::OnInput(const Device::Win32::Mouse& mouse, const  Device::Win32::Keyboard& keyboard)
 {
-	Transform* control = _camera->GetTransform();//_testObject2 ? _testObject2->GetTransform() : _camera->GetTransform();
+	Transform* control = _light->GetTransform();//_testObject2 ? _testObject2->GetTransform() : _camera->GetTransform();
 	const float scale = 0.1f;
 
 	if(keyboard.states['W'] == Win32::Keyboard::Type::Up)
@@ -253,20 +158,12 @@ void TestScene::OnInput(const Device::Win32::Mouse& mouse, const  Device::Win32:
 	}
 	if(keyboard.states['M'] == Win32::Keyboard::Type::Up)
 	{
-		//_testObject->SetUse(!_testObject->GetUse());
+		_testObject->SetUse(!_testObject->GetUse());
 	}
-
 }
 
 void TestScene::OnUpdate(float dt)
 {
-	Transform* tf = _testObject->GetTransform();
-
-	Vector3 euler = tf->GetLocalEulerAngle();
-	tf->UpdateEulerAngles(euler - Vector3(0, 0.1f, 0));
-
-	//Vector3 pos = tf->GetLocalPosition();
-	//tf->UpdatePosition(pos - Vector3(0, 0.2f, 0));
 }
 
 void TestScene::OnRenderPost()
--- SOCTestProject/SOCTestProject/Code/main.cpp
+++ SOCTestProject/SOCTestProject/Code/main.cpp
@@ -5,7 +5,7 @@
 INT WINAPI wWinMain( HINSTANCE hInst, HINSTANCE, LPWSTR, INT )
 {
 	auto rect = Math::Rect<unsigned int>(0, 0, 1600, 900);
-	Device::Director::SharedInstance()->Initialize(rect, rect, hInst, "Test", true, false, 0);
+	Device::Director::SharedInstance()->Initialize(rect, rect, hInst, "Test", true, false, 0, false);
 	Device::Director::SharedInstance()->SetCurrentScene(new TestScene);
 	Device::Director::SharedInstance()->Run();
 	Device::Director::Destroy();
