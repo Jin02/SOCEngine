ad59bdeb61a6f936ec664a5568ab9218e4717409
Jin Park <quaggan02@gmail.com>
2016-06-18 22:37:56 +0900

    Voxelization_TN_UV0.hlsl - 코드 정리 #19

--- SOCEngine/SOCEngine/ShaderCodes/Voxelization_TN_UV0.hlsl
+++ SOCEngine/SOCEngine/ShaderCodes/Voxelization_TN_UV0.hlsl
@@ -1,4 +1,4 @@
-#define USE_OUT_ANISOTROPIC_VOXEL_TEXTURES
+#define USE_OUT_VOXEL_MAP 
 
 #include "Voxelization_Common.h"
 #include "NormalMapping.h"
@@ -8,7 +8,7 @@ struct VS_INPUT
 	float3 position 			: POSITION;
 	float3 normal				: NORMAL;
 	float3 tangent				: TANGENT;
-	float2 uv					: TEXCOORD0;
+	float2 uv				: TEXCOORD0;
 };
 
 struct VS_OUTPUT
@@ -17,7 +17,7 @@ struct VS_OUTPUT
 	float3 normal 				: NORMAL;
 	float3 tangent				: TANGENT;
 
-	float2 uv					: TEXCOORD0;
+	float2 uv				: TEXCOORD0;
 };
 
 VS_OUTPUT VS( VS_INPUT input )
@@ -25,7 +25,7 @@ VS_OUTPUT VS( VS_INPUT input )
 	VS_OUTPUT output;
 
 	output.localPos		= input.position;
-	output.uv			= input.uv;
+	output.uv		= input.uv;
 	output.normal 		= normalize( mul(input.normal,	(float3x3)transform_worldInvTranspose) );
 	output.tangent		= normalize( mul(input.tangent,	(float3x3)transform_worldInvTranspose) );
  
@@ -41,7 +41,7 @@ struct GS_OUTPUT
 	float3	normal				: NORMAL;
 	float3	tangent				: TANGENT;
 
-	float2	uv					: TEXCOORD0;
+	float2	uv				: TEXCOORD0;
 	uint	axis				: AXIS_INDEX;
 };
 
@@ -69,9 +69,9 @@ void GS(triangle VS_OUTPUT input[3], inout TriangleStream<GS_OUTPUT> outputStrea
 		output.position_sv	= position[i];
 		output.worldPos		= worldPos[i];
 
-		output.axis			= axis[i];
+		output.axis		= axis[i];
 
-		output.uv			= input[i].uv;
+		output.uv		= input[i].uv;
 		output.normal		= input[i].normal;
 		output.tangent		= input[i].tangent;
 
@@ -84,7 +84,7 @@ void GS(triangle VS_OUTPUT input[3], inout TriangleStream<GS_OUTPUT> outputStrea
 void PS( GS_OUTPUT input )
 {
 	float4 normalTex	= normalMap.Sample(defaultSampler, input.uv);
-	float3 bumpedNormal = NormalMapping(normalTex.rgb, input.normal, input.tangent, input.uv);
+	float3 bumpedNormal	= NormalMapping(normalTex.rgb, input.normal, input.tangent, input.uv);
 	float3 normal		= lerp(input.normal, bumpedNormal, HasNormalMap());
 
 	VoxelizationInPSStage(normalize(normal), input.uv, input.position, input.worldPos, input.axis);
