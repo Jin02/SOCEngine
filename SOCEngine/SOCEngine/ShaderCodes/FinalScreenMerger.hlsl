361f6b2be0839334e7cdbd9359f407689a84ada2
Jin Park <quaggan02@gmail.com>
2016-06-18 23:53:28 +0900

    Update FinalScreenMerger.hlsl

--- SOCEngine/SOCEngine/ShaderCodes/FinalScreenMerger.hlsl
+++ SOCEngine/SOCEngine/ShaderCodes/FinalScreenMerger.hlsl
@@ -4,23 +4,23 @@
 #include "ShaderCommon.h"
 
 SamplerState		renderSceneSamplerState	: register( s0 );
-Texture2D<float4>	renderScene				: register( t0 );
+Texture2D<float4>	renderScene		: register( t0 );
 
 #if defined(USE_UI)
 SamplerState uiSceneSamplerState		: register( s1 );
-Texture2D<float4> uiScene				: register( t1 );
+Texture2D<float4> uiScene			: register( t1 );
 #endif
 
 float4 PS( PS_INPUT input ) : SV_Target
 {
-	float4 finalColor = float4(0.0f, 0.0f, 0.0f, 0.0f);
-	float4 renderSceneColor = renderScene.Sample(renderSceneSamplerState, input.uv);
+	float4 finalColor	= float4(0.0f, 0.0f, 0.0f, 0.0f);
+	float4 renderSceneColor	= renderScene.Sample(renderSceneSamplerState, input.uv);
 
 #if defined(USE_UI)
-	float4 uiSceneColor = uiScene.Sample(uiSceneSamplerState, input.uv);
-	finalColor = renderSceneColor + uiSceneColor;
+	float4 uiSceneColor	= uiScene.Sample(uiSceneSamplerState, input.uv);
+	finalColor		= renderSceneColor + uiSceneColor;
 #else
-	finalColor = renderSceneColor;
+	finalColor		= renderSceneColor;
 #endif
 
 	return finalColor;
