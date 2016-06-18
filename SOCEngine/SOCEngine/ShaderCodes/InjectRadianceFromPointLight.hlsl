f22433dc7310aa67b854b14dc0f488275853a7a2
Jin Park <quaggan02@gmail.com>
2016-06-18 22:53:01 +0900

    InjectRadianceFromPL- Texture -> Map 으로 명칭 변경 #19
    그리고 코드 정리

--- SOCEngine/SOCEngine/ShaderCodes/InjectRadianceFromPointLight.hlsl
+++ SOCEngine/SOCEngine/ShaderCodes/InjectRadianceFromPointLight.hlsl
@@ -6,14 +6,15 @@
 
 [numthreads(INJECTION_TILE_RES, INJECTION_TILE_RES, 1)]
 void CS(uint3 globalIdx	: SV_DispatchThreadID, 
-		uint3 localIdx	: SV_GroupThreadID,
-		uint3 groupIdx	: SV_GroupID)
+	uint3 localIdx	: SV_GroupThreadID,
+	uint3 groupIdx	: SV_GroupID)
 {
-	uint capacity			= GetNumOfPointLight(shadowGlobalParam_packedNumOfShadowAtlasCapacity);
+	uint capacity		= GetNumOfPointLight(shadowGlobalParam_packedNumOfShadowAtlasCapacity);
 	float perShadowMapRes	= (float)(1 << GetNumOfPointLight(shadowGlobalParam_packedPowerOfTwoShadowResolution));
 
 	uint shadowIndex	= globalIdx.x / (uint)perShadowMapRes;
 	uint faceIndex		= globalIdx.y / (uint)perShadowMapRes;
+
 	ParsedShadowParam shadowParam;
 	ParseShadowParam(shadowParam, g_inputPointLightShadowParams[shadowIndex].xy);
 	uint lightIndex		= shadowParam.lightIndex;
@@ -21,13 +22,12 @@ void CS(uint3 globalIdx	: SV_DispatchThreadID,
 	float2 shadowMapPos	= float2(globalIdx.x % uint(perShadowMapRes), globalIdx.y % uint(perShadowMapRes));
 	float2 shadowMapUV	= shadowMapPos.xy / perShadowMapRes;
 
-	float depth = g_inputPointLightShadowMapAtlas.Load(uint3(globalIdx.xy, 0), 0);
-
-	float4 worldPos = mul( float4(shadowMapPos.xy, depth, 1.0f), g_inputPointLightShadowInvVPVMatBuffer[shadowIndex].mat[faceIndex] );
+	float depth		= g_inputPointLightShadowMapAtlas.Load(uint3(globalIdx.xy, 0), 0);
+	float4 worldPos		= mul( float4(shadowMapPos.xy, depth, 1.0f), g_inputPointLightShadowInvVPVMatBuffer[shadowIndex].mat[faceIndex] );
 	worldPos /= worldPos.w;
 
 	float voxelizeSize	= GetVoxelizeSize(voxelization_currentCascade);
-	float3 voxelSpaceUV = (worldPos.xyz - voxelization_minPos) / voxelizeSize;
+	float3 voxelSpaceUV	= (worldPos.xyz - voxelization_minPos) / voxelizeSize;
 	int dimension		= (int)GetDimension();
 	int3 voxelIdx		= int3(voxelSpaceUV * dimension);
 
@@ -35,16 +35,16 @@ void CS(uint3 globalIdx	: SV_DispatchThreadID,
 		return;
 
 	float4 lightCenterWithRadius	= g_inputPointLightTransformBuffer[lightIndex];
-	float3 lightCenterWorldPos		= lightCenterWithRadius.xyz;
-	float lightRadius				= lightCenterWithRadius.a;
+	float3 lightCenterWorldPos	= lightCenterWithRadius.xyz;
+	float lightRadius		= lightCenterWithRadius.a;
 
-	float3 lightDir					= lightCenterWorldPos - worldPos.xyz;
+	float3 lightDir			= lightCenterWorldPos - worldPos.xyz;
 	float distanceOfLightWithVertex	= length(lightDir);
-	lightDir = normalize(lightDir);
+	lightDir			= normalize(lightDir);
 
-	float3 normal	= GetNormal(g_inputVoxelNormalTexture, voxelIdx, voxelization_currentCascade);
-	float4 albedo	= GetColor(g_inputVoxelAlbedoTexture, voxelIdx, voxelization_currentCascade);
-	float4 emission	= GetColor(g_inputVoxelEmissionTexture, voxelIdx, voxelization_currentCascade);
+	float3 normal			= GetNormal(g_inputVoxelNormalMap, voxelIdx, voxelization_currentCascade);
+	float4 albedo			= GetColor(g_inputVoxelAlbedoMap, voxelIdx, voxelization_currentCascade);
+	float4 emission			= GetColor(g_inputVoxelEmissionMap, voxelIdx, voxelization_currentCascade);
 
 	float3 radiosity = float3(0.0f, 0.0f, 0.0f);
 	if( distanceOfLightWithVertex < lightRadius )
@@ -52,7 +52,7 @@ void CS(uint3 globalIdx	: SV_DispatchThreadID,
 		float4 lightColorWithLm = g_inputPointLightColorBuffer[lightIndex];
 
 		float3 lightColor	= lightColorWithLm.rgb;
-		float lumen			= lightColorWithLm.a * float(MAXIMUM_LUMEN);
+		float lumen		= lightColorWithLm.a * float(MAXIMUM_LUMEN);
 		float attenuation	= lumen / (distanceOfLightWithVertex * distanceOfLightWithVertex);
 		float3 lambert		= albedo.rgb * saturate(dot(normal, lightDir));
 
