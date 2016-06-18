24b5e96f0669f1e9528cb0d851fbe3c1d3846d6d
Jin Park <quaggan02@gmail.com>
2016-06-18 22:40:15 +0900

    ClearVoxelMaps - 코드 정리 #19

--- SOCEngine/SOCEngine/ShaderCodes/ClearVoxelMaps.hlsl
+++ SOCEngine/SOCEngine/ShaderCodes/ClearVoxelMaps.hlsl
@@ -1,13 +1,13 @@
 //EMPTY_META_DATA
 
-#define USE_OUT_ANISOTROPIC_VOXEL_TEXTURES
+#define USE_OUT_VOXEL_MAP
 
 #include "Voxelization_Common.h"
 
 [numthreads(8, 8, 8)]
 void ClearVoxelMapCS(	uint3 globalIdx : SV_DispatchThreadID, 
-						uint3 localIdx	: SV_GroupThreadID,
-						uint3 groupIdx	: SV_GroupID)
+			uint3 localIdx	: SV_GroupThreadID,
+			uint3 groupIdx	: SV_GroupID	)
 {
 	OutVoxelAlbedoTexture[globalIdx]	= 0;
 	OutVoxelNormalTexture[globalIdx]	= 0;
