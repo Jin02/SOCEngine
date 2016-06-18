dd830f9b75837b703ae83209e66d1e4fc39ef2f0
Jin Park <quaggan02@gmail.com>
2016-06-18 23:53:06 +0900

    Update GBufferParser.h

--- SOCEngine/SOCEngine/ShaderCodes/GBufferParser.h
+++ SOCEngine/SOCEngine/ShaderCodes/GBufferParser.h
@@ -38,7 +38,7 @@ void ParseGBufferSurface(out Surface outSurface, uint2 globalIdx, uint sampleIdx
 	float3 normal = normal_roughness.xyz;
 	normal *= 2.0f; normal -= float3(1.0f, 1.0f, 1.0f);
 
-	outSurface.normal		= normal;
+	outSurface.normal	= normal;
 	outSurface.roughness	= normal_roughness.w;
 
 #if (MSAA_SAMPLES_COUNT > 1) //MSAA
@@ -47,20 +47,20 @@ void ParseGBufferSurface(out Surface outSurface, uint2 globalIdx, uint sampleIdx
 	float depth = g_tDepth.Load( uint3(globalIdx, 0) ).x;
 #endif
 
-	float4 worldPosition = mul( float4((float)globalIdx.x, (float)globalIdx.y, depth, 1.0), tbrParam_invViewProjViewportMat );
-	worldPosition /= worldPosition.w;
+	float4 worldPosition	= mul( float4((float)globalIdx.x, (float)globalIdx.y, depth, 1.0), tbrParam_invViewProjViewportMat );
+	worldPosition		/= worldPosition.w;
 
 	outSurface.depth	= depth;
 	outSurface.worldPos	= worldPosition.xyz;
 
 #if (MSAA_SAMPLES_COUNT > 1) // MSAA
-	float4 albedo_occlusion = g_tGBufferAlbedo_occlusion.Load( globalIdx, sampleIdx );
-	float4 motionXY_height_metallic = g_tGBufferMotionXY_height_metallic.Load( globalIdx, sampleIdx );
-	float4 emission_specularity = g_tGBufferEmission_specularity.Load(globalIdx, sampleIdx);
+	float4 albedo_occlusion		= g_tGBufferAlbedo_occlusion.Load( globalIdx, sampleIdx );
+	float4 motionXY_height_metallic	= g_tGBufferMotionXY_height_metallic.Load( globalIdx, sampleIdx );
+	float4 emission_specularity	= g_tGBufferEmission_specularity.Load(globalIdx, sampleIdx);
 #else
-	float4 albedo_occlusion = g_tGBufferAlbedo_occlusion.Load( uint3(globalIdx, 0) );
-	float4 motionXY_height_metallic = g_tGBufferMotionXY_height_metallic.Load( uint3(globalIdx, 0) );
-	float4 emission_specularity = g_tGBufferEmission_specularity.Load(uint3(globalIdx, 0));
+	float4 albedo_occlusion		= g_tGBufferAlbedo_occlusion.Load( uint3(globalIdx, 0) );
+	float4 motionXY_height_metallic	= g_tGBufferMotionXY_height_metallic.Load( uint3(globalIdx, 0) );
+	float4 emission_specularity	= g_tGBufferEmission_specularity.Load(uint3(globalIdx, 0));
 #endif
 
 	float specularity		= emission_specularity.a;
@@ -68,13 +68,13 @@ void ParseGBufferSurface(out Surface outSurface, uint2 globalIdx, uint sampleIdx
 	float metallic			= motionXY_height_metallic.a;
 
 	outSurface.albedo		= baseColor - baseColor * metallic;
-	outSurface.occlusion	= albedo_occlusion.a;
+	outSurface.occlusion		= albedo_occlusion.a;
 
 	outSurface.specular		= lerp(0.08f * specularity.xxx, baseColor, metallic);
 	outSurface.metallic		= metallic;
 
 	outSurface.emission		= emission_specularity.rgb;
-	outSurface.specularity	= specularity;
+	outSurface.specularity		= specularity;
 
 	outSurface.motion		= (motionXY_height_metallic.xy * 2.0f) - float2(1.0f, 1.0f);
 	outSurface.height		= motionXY_height_metallic.z;
