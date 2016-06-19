4b8810856faf0bde0b78ed277173c1c83c7fd84d
Jin Park <quaggan02@gmail.com>
2016-06-19 13:34:11 +0900

    Update PhysicallyBased_Forward_Common.h

--- SOCEngine/SOCEngine/ShaderCodes/PhysicallyBased_Forward_Common.h
+++ SOCEngine/SOCEngine/ShaderCodes/PhysicallyBased_Forward_Common.h
@@ -10,41 +10,41 @@
 #include "DynamicLighting.h"
 
 Buffer<uint> g_perLightIndicesInTile	: register( t13 );
-SamplerState defaultSampler				: register( s0 );
+SamplerState defaultSampler		: register( s0 );
 
 
 float4 Lighting(float3 normal, float3 vtxWorldPos, float2 SVPosition, float2 uv)
 {
-	float metallic				= GetMetallic(defaultSampler, uv);
-	float roughness				= GetRoughness(defaultSampler, uv);
-	float specularity			= GetMaterialSpecularity();
+	float metallic			= GetMetallic(defaultSampler, uv);
+	float roughness			= GetRoughness(defaultSampler, uv);
+	float specularity		= GetMaterialSpecularity();
 
 	float3 emissiveColor		= GetEmissiveColor(defaultSampler, uv);
-	float3 albedo				= GetAlbedo(defaultSampler, uv);
+	float3 albedo			= GetAlbedo(defaultSampler, uv);
 
 	LightingParams lightParams;
 
-	lightParams.viewDir			= normalize( tbrParam_cameraWorldPosition - vtxWorldPos );
-	lightParams.normal			= normal;
+	lightParams.viewDir		= normalize( tbrParam_cameraWorldPosition - vtxWorldPos );
+	lightParams.normal		= normal;
 	lightParams.roughness		= roughness;
 	lightParams.diffuseColor	= albedo - albedo * metallic;
 	lightParams.specularColor	= lerp(0.08f * specularity.xxx, albedo, metallic);
 
-	uint tileIdx				= GetTileIndex(SVPosition);
-	uint startIdx				= tileIdx * tbrParam_maxNumOfPerLightInTile + 1;
+	uint tileIdx			= GetTileIndex(SVPosition);
+	uint startIdx			= tileIdx * tbrParam_maxNumOfPerLightInTile + 1;
 
 	uint packedLightCountValue	= g_perLightIndicesInTile[startIdx - 1];
 	uint pointLightCount		= packedLightCountValue & 0x0000ffff;
-	uint spotLightCount			= packedLightCountValue >> 16;
+	uint spotLightCount		= packedLightCountValue >> 16;
 
 #if defined(RENDER_TRANSPARENCY)
-	float3 accumulativeFrontFaceDiffuse		= float3(0.0f, 0.0f, 0.0f);
+	float3 accumulativeFrontFaceDiffuse	= float3(0.0f, 0.0f, 0.0f);
 	float3 accumulativeFrontFaceSpecular	= float3(0.0f, 0.0f, 0.0f);
-	float3 accumulativeBackFaceDiffuse		= float3(0.0f, 0.0f, 0.0f);
-	float3 accumulativeBackFaceSpecular		= float3(0.0f, 0.0f, 0.0f);
+	float3 accumulativeBackFaceDiffuse	= float3(0.0f, 0.0f, 0.0f);
+	float3 accumulativeBackFaceSpecular	= float3(0.0f, 0.0f, 0.0f);
 #else
-	float3 accumulativeDiffuse	= float3(0.0f, 0.0f, 0.0f);
-	float3 accumulativeSpecular	= float3(0.0f, 0.0f, 0.0f);
+	float3 accumulativeDiffuse		= float3(0.0f, 0.0f, 0.0f);
+	float3 accumulativeSpecular		= float3(0.0f, 0.0f, 0.0f);
 #endif
 
 	uint endIdx = startIdx + pointLightCount;
@@ -63,13 +63,13 @@ float4 Lighting(float3 normal, float3 vtxWorldPos, float2 SVPosition, float2 uv)
 
 		accumulativeFrontFaceDiffuse	+= frontFaceDiffuseColor;
 		accumulativeFrontFaceSpecular	+= frontFaceSpecularColor;
-		accumulativeBackFaceDiffuse		+= backFaceDiffuseColor;
+		accumulativeBackFaceDiffuse	+= backFaceDiffuseColor;
 		accumulativeBackFaceSpecular	+= backFaceSpecularColor;
 #else
 		float3 diffuse, specular;
 		RenderPointLight(diffuse, specular, lightParams, vtxWorldPos);
 
-		accumulativeDiffuse		+= diffuse;
+		accumulativeDiffuse	+= diffuse;
 		accumulativeSpecular	+= specular;
 #endif
 	}
@@ -91,13 +91,13 @@ float4 Lighting(float3 normal, float3 vtxWorldPos, float2 SVPosition, float2 uv)
 
 		accumulativeFrontFaceDiffuse	+= frontFaceDiffuseColor;
 		accumulativeFrontFaceSpecular	+= frontFaceSpecularColor;
-		accumulativeBackFaceDiffuse		+= backFaceDiffuseColor;
+		accumulativeBackFaceDiffuse	+= backFaceDiffuseColor;
 		accumulativeBackFaceSpecular	+= backFaceSpecularColor;
 #else
 		float3 diffuse, specular;
 		RenderSpotLight(diffuse, specular, lightParams, vtxWorldPos);
 
-		accumulativeDiffuse		+= diffuse;
+		accumulativeDiffuse	+= diffuse;
 		accumulativeSpecular	+= specular;
 #endif
 	}
@@ -118,13 +118,13 @@ float4 Lighting(float3 normal, float3 vtxWorldPos, float2 SVPosition, float2 uv)
 
 		accumulativeFrontFaceDiffuse	+= frontFaceDiffuseColor;
 		accumulativeFrontFaceSpecular	+= frontFaceSpecularColor;
-		accumulativeBackFaceDiffuse		+= backFaceDiffuseColor;
+		accumulativeBackFaceDiffuse	+= backFaceDiffuseColor;
 		accumulativeBackFaceSpecular	+= backFaceSpecularColor;
 #else
 		float3 diffuse, specular;
 		RenderDirectionalLight(diffuse, specular, lightParams, vtxWorldPos);
 
-		accumulativeDiffuse		+= diffuse;
+		accumulativeDiffuse	+= diffuse;
 		accumulativeSpecular	+= specular;
 #endif
 	}
@@ -133,7 +133,7 @@ float4 Lighting(float3 normal, float3 vtxWorldPos, float2 SVPosition, float2 uv)
 	accumulativeFrontFaceSpecular = saturate(accumulativeFrontFaceSpecular);
 	accumulativeBackFaceSpecular = saturate(accumulativeBackFaceSpecular);
 
-	float3 back		= (accumulativeBackFaceDiffuse + accumulativeBackFaceSpecular) * TRANSPARENCY_BACK_FACE_WEIGHT;
+	float3 back	= (accumulativeBackFaceDiffuse + accumulativeBackFaceSpecular) * TRANSPARENCY_BACK_FACE_WEIGHT;
 	float3 front	= (accumulativeFrontFaceDiffuse + accumulativeFrontFaceSpecular);
 
 	float3	result	= front + back;
