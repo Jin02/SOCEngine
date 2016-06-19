49c967bcf26937aad6fe16e7e96fd47ff28abae3
Jin Park <quaggan02@gmail.com>
2016-06-19 20:14:24 +0900

    Update ReflectionProbeAlphaTestWithDiffuse.hlsl

--- SOCEngine/SOCEngine/ShaderCodes/ReflectionProbeAlphaTestWithDiffuse.hlsl
+++ SOCEngine/SOCEngine/ShaderCodes/ReflectionProbeAlphaTestWithDiffuse.hlsl
@@ -44,10 +44,10 @@ void OnlyAlpaTestWithDiffuseGS(triangle GS_ALPHA_TEST_DIFFUSE_INPUT input[3], in
 
 float4 OnlyAlpaTestWithDiffusePS( PS_ALPHA_TEST_DIFFUSE_INPUT input ) : SV_TARGET
 { 
-	float alpha = GetAlpha(defaultSampler, input.uv);
+	float alpha = GetAlpha(DefaultSampler, input.uv);
 
 	if(alpha < ALPHA_TEST_BIAS)
 		discard;
 
-	return float4(GetAlbedo(defaultSampler, input.uv), alpha);
+	return float4(GetAlbedo(DefaultSampler, input.uv), alpha);
 }
