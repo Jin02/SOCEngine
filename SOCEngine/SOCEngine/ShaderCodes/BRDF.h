3055c9bcbda07d593996c56e115703ce5f398007
Jin Park <quaggan02@gmail.com>
2016-06-19 22:30:17 +0900

    BRDF.h - fresnel0 버그 수정
    lightingParams.specularColor를 안쓰고 있었음

--- SOCEngine/SOCEngine/ShaderCodes/BRDF.h
+++ SOCEngine/SOCEngine/ShaderCodes/BRDF.h
@@ -244,7 +244,7 @@ void BRDFLighting(out float3 resultDiffuseColor, out float3 resultSpecularColor,
 	float VdotH		= saturate( dot(lightingParams.viewDir,	halfVector) );
 	float VdotL		= saturate( dot(lightingParams.viewDir,	commonParamas.lightDir) );
 
-	float3 fresnel0		= float3(0.05f, 0.05f, 0.05f); //float3(0.05f, 0.05f, 0.05f);
+	float3 fresnel0		= lightingParams.specularColor; //float3(0.05f, 0.05f, 0.05f);
 	float roughness		= lightingParams.roughness; //0.6f
 
 	float3 diffuseEnergyConservation = DiffuseEnergyConservation(fresnel0, NdotL);
