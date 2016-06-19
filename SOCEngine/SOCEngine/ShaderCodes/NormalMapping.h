728ae5523d2aa27b81334de1d7508410d62a59fa
Jin Park <quaggan02@gmail.com>
2016-06-19 13:55:06 +0900

    Update NormalMapping.h

--- SOCEngine/SOCEngine/ShaderCodes/NormalMapping.h
+++ SOCEngine/SOCEngine/ShaderCodes/NormalMapping.h
@@ -6,10 +6,7 @@
 float3 NormalMapping(float3 normalMapXYZ, float3 normal, float3 tangent, float2 uv)
 {
 	float3 binormal		= normalize( cross(normal, tangent) );
-	float3 decompression	= normalMapXYZ;
-
-	decompression		*= 2.0f;
-	decompression		-= float3(1.0f, 1.0f, 1.0f);
+	float3 decompression	= (normalMapXYZ * 2.0f) - float3(1.0f, 1.0f, 1.0f);
 
 	float3x3 TBN = float3x3(normalize(binormal), normalize(tangent), normalize(normal));
 	return normalize( mul(decompression, TBN) );
