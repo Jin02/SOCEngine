29deac841c8edfec760d8e0da7a8af2ace5a0c09
Jin Park <quaggan02@gmail.com>
2016-06-19 13:40:52 +0900

    Update PhysicallyBased_GBuffer_N_UV0.hlsl

--- SOCEngine/SOCEngine/ShaderCodes/PhysicallyBased_GBuffer_N_UV0.hlsl
+++ SOCEngine/SOCEngine/ShaderCodes/PhysicallyBased_GBuffer_N_UV0.hlsl
@@ -4,13 +4,13 @@ struct VS_INPUT
 {
 	float3 position 			: POSITION;
 	float3 normal				: NORMAL;
-	float2 uv					: TEXCOORD0;
+	float2 uv				: TEXCOORD0;
 };
 
 struct VS_OUTPUT
 {
 	float4 position 		 	: SV_POSITION;
-	float2 uv					: TEXCOORD0;
+	float2 uv				: TEXCOORD0;
 	float3 worldPos				: WORLD_POS;
 
 	float3 normal 				: NORMAL;
@@ -21,12 +21,12 @@ VS_OUTPUT VS( VS_INPUT input )
 	VS_OUTPUT ps;
 
 	float4 worldPos	= mul( float4(input.position, 1.0f),	transform_world );
-	ps.position 	= mul( worldPos,						camera_viewProjMat );
-	ps.worldPos		= worldPos.xyz;
+	ps.position 	= mul( worldPos,			camera_viewProjMat );
+	ps.worldPos	= worldPos.xyz;
 
-	ps.uv			= input.uv;
+	ps.uv		= input.uv;
 
-	ps.normal 		= mul(input.normal, (float3x3)transform_worldInvTranspose);
+	ps.normal 	= mul(input.normal, (float3x3)transform_worldInvTranspose);
  
     return ps;
 }
@@ -38,7 +38,7 @@ GBuffer PS( VS_OUTPUT input ) : SV_Target
 #ifdef ENABLE_ALPHA_TEST
 	float4 diffuseTex	= diffuseMap.Sample(GBufferDefaultSampler, input.uv);
 	float opacityTex	= 1.0f - opacityMap.Sample(GBufferDefaultSampler, input.uv).x;
-	float alpha			= diffuseTex.a * opacityTex * GetMaterialMainColor().a;
+	float alpha		= diffuseTex.a * opacityTex * GetMaterialMainColor().a;
 
 	if(alpha < ALPHA_TEST_BIAS)
 		discard;
