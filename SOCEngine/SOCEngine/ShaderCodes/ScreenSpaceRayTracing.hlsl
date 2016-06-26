7c8af0cf946786eaa7b5d9b96ff9f84b76ae99cf
Jin Park <quaggan02@gmail.com>
2016-06-26 23:13:51 +0900

    ScreenSpaceRayTracing.hlsl - 구현 중 #17

--- SOCEngine/SOCEngine/ShaderCodes/ScreenSpaceRayTracing.hlsl
+++ SOCEngine/SOCEngine/ShaderCodes/ScreenSpaceRayTracing.hlsl
@@ -1,1 +1,27 @@
+//EMPTY_META_DATA
 
+#include "FullScreenShader.h"
+#include "TBDRInput.h"
+
+
+float4 PS(PS_INPUT input) : SV_Target
+{
+    int3 screenPos = int3(input.position.xy, 0);
+
+#if (MSAA_SAMPLES_COUNT > 1) //MSAA
+    float3 worldNormal = GBufferNormal_roughness.Load( screenPos.xy, input.sampleIdx ).xyz;
+#else
+    float3 worldNormal = GBufferNormal_roughness.Load( screenPos ).xyz;
+#endif
+    
+    worldNormal *= 2.0f;
+    worldNormal -= float3(1.0f, 1.0f, 1.0f);
+    
+    float3 viewSpaceNormal = mul(float4(worldNormal, 0.0f), tbrParam_viewMat).xyz;
+
+    if( any(viewSpaceNormal) == 0.0f )
+        return float4(0.0f, 0.0f, 0.0f, 0.0f);
+        
+    float depth                 = FetchLinerDepthFromGBuffer(screenPos.xy);
+    float3 rayOriginViewSpace   = input.viewRay * depth;
+}
