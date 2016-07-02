3454d0e58062572f9089c08d5843bb6c4949a43c
Jin Park <quaggan02@gmail.com>
2016-07-02 23:30:07 +0900

    Update BlurTexture2D.hlsl
    IN_QUAD_SCREEN과 IN_COMPUTE_SHADER 추가.
    
    #17

--- SOCEngine/SOCEngine/ShaderCodes/BlurTexture2D.hlsl
+++ SOCEngine/SOCEngine/ShaderCodes/BlurTexture2D.hlsl
@@ -1,20 +1,49 @@
 //EMPTY_META_DATA
 //#16_0 기반으로 작업 중..
 
-//#include "FullScreenShader.h" 좀 범용적으로 사용하기 위해 이건 좀 다시 생각해봐야할 가치가 있음
-
+// Blurring Common
 static const float Weights[7]	= {0.001f, 0.028f, 0.233f, 0.474f, 0.233f, 0.028f, 0.001f};
 
 #ifdef	CONVOLVE_HORIZONTAL
-static const int Offsets[7]		= { {0, -3}, {0, -2}, {0, -1}, {0, 0}, {0, 1}, {0, 2}, {0, 3} };
+static const int Offsets[7]	= { {0, -3}, {0, -2}, {0, -1}, {0, 0}, {0, 1}, {0, 2}, {0, 3} };
 #elif	CONVOLVE_VERTICAL
-static const int Offsets[7]		= { {0, -3}, {0, -2}, {0, -1}, {0, 0}, {0, 1}, {0, 2}, {0, 3} };
+static const int Offsets[7]	= { {0, -3}, {0, -2}, {0, -1}, {0, 0}, {0, 1}, {0, 2}, {0, 3} };
 #endif
 
 
+Texture2D<float4>	InputMap : register( t0 );
 
-float4 Blur_InPS(VS_INPUT input) : SV_Target
+#ifdef	IN_QUAD_SCREEN
+#define USE_VIEW_INFORMATION
+#include "FullScreenShader.h"
+float4 BlurTexture2D_InFullScreen_PS(VS_INPUT input) : SV_Target
+
+#elif	IN_COMPUTE_SHADER
+RWTexture2D<float4>	OutMap : register( u0 );
+
+[numthreads(16, 16, 1)]
+void BlurTexture2D_InCS(uint3 globalIdx : SV_DispatchThreadID, 
+			uint3 localIdx	: SV_GroupThreadID,
+			uint3 groupIdx	: SV_GroupID)
+#endif
 {
-	int3 pixelPos = int3(input.uv * ??????, 0);
+	float4 resultColor = float4(0.0f, 0.0f, 0.0f, 0.0f);
 	
+#ifdef	IN_QUAD_SCREEN
+	float2 viewportSize	= GetViewportSize();
+	float2 uv		= input.uv * viewportSize;
+	int3 globalIdx		= int3(uv, 0);
+#endif
+
+	[unroll]
+	for(uint i=0; i<7; ++i)
+		resultColor += inputTex2D.Load( int3(globalIdx), Offsets[i] ) * Weights[i];
+
+#ifdef	IN_QUAD_SCREEN
+	return float4(resultColor.rgb, 1.0f);
+#elif	IN_COMPUTE_SHADER
+	OutMap[globalIdx.xy] = float4(resultColor.rgb, 1.0f);
+#endif
 }
+
+#endif
