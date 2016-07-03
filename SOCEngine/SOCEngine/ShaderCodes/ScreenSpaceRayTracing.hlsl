dcb79ea914133fdc20342e68f540f863e8c0a49a
Jin Park <quaggan02@gmail.com>
2016-07-03 23:04:45 +0900

    ScreenSpaceRayTracing.hlsl - 코드 정리
    viewNormal 계산하는 부분 간소화

--- SOCEngine/SOCEngine/ShaderCodes/ScreenSpaceRayTracing.hlsl
+++ SOCEngine/SOCEngine/ShaderCodes/ScreenSpaceRayTracing.hlsl
@@ -9,16 +9,7 @@ float4 SSRT_InFullScreen_PS(PS_INPUT input) : SV_Target
 {
     int3 screenPos = int3(input.position.xy, 0);
 
-#if (MSAA_SAMPLES_COUNT > 1) //MSAA
-    float3 worldNormal = GBufferNormal_roughness.Load( screenPos.xy, input.sampleIdx ).xyz;
-#else
-    float3 worldNormal = GBufferNormal_roughness.Load( screenPos ).xyz;
-#endif
-    
-    worldNormal *= 2.0f;
-    worldNormal -= float3(1.0f, 1.0f, 1.0f);
-    
-    float3 viewNormal = mul(float4(worldNormal, 0.0f), tbrParam_viewMat).xyz;
+    float3 viewNormal = ComputeViewNormalFromGBuffer(screenPos, input.sampleIdx);
 
     if( any(viewNormal) == 0.0f )
         return float4(0.0f, 0.0f, 0.0f, 0.0f);
