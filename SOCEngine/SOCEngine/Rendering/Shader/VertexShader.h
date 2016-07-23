1779d2172033f6ad3cf2dc2b72045b2deb665399
Jin Park <quaggan02@gmail.com>
2016-07-23 15:28:18 +0900

    VertexShader.cpp - BindTexture에서 Texture2D 대신 TextureForm을 사용하도록 수정 #32

--- SOCEngine/SOCEngine/Rendering/Shader/VertexShader.h
+++ SOCEngine/SOCEngine/Rendering/Shader/VertexShader.h
@@ -35,8 +35,8 @@ namespace Rendering
 			bool Create(const Device::DirectX* dx, const std::vector<D3D11_INPUT_ELEMENT_DESC>& vertexDeclations);
 			void Clear(ID3D11DeviceContext* context,
 				const std::vector<InputConstBuffer>*			constBuffers, 
-				const std::vector<InputTexture>*				textures,
-				const std::vector<InputShaderResourceBuffer>*	srBuffers);
+				const std::vector<InputTexture>*			textures,
+				const std::vector<InputShaderResourceBuffer>*		srBuffers);
 
 		public:
 			void BindShaderToContext(ID3D11DeviceContext* context);
@@ -47,7 +47,7 @@ namespace Rendering
 				const std::vector<InputTexture>*			textures,
 				const std::vector<InputShaderResourceBuffer>*		srBuffers);
 				
-			static void BindTexture(ID3D11DeviceContext* context, TextureBindIndex bind, const Texture::Texture2D* tex);
+			static void BindTexture(ID3D11DeviceContext* context, TextureBindIndex bind, const Texture::TextureForm* tex);
 			static void BindSamplerState(ID3D11DeviceContext* context, SamplerStateBindIndex bind, ID3D11SamplerState* samplerState);
 			static void BindConstBuffer(ID3D11DeviceContext* context, ConstBufferBindIndex bind, const Buffer::ConstBuffer* cb);
 			static void BindShaderResourceBuffer(ID3D11DeviceContext* context, TextureBindIndex bind, const Buffer::ShaderResourceBuffer* srBuffer);
