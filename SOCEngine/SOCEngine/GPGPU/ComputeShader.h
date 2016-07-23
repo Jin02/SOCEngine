b757f568285714ed5b20dd6c2e76591d3ec694dd
Jin Park <quaggan02@gmail.com>
2016-07-23 15:23:00 +0900

    ComputeShader - BindTexture에서 Texture2D 대신 TextureForm을 사용하도록 수정 #32

--- SOCEngine/SOCEngine/GPGPU/ComputeShader.h
+++ SOCEngine/SOCEngine/GPGPU/ComputeShader.h
@@ -38,7 +38,7 @@ namespace GPGPU
 			bool Initialize();
 			void Dispatch(ID3D11DeviceContext* context);
 			
-			static void BindTexture(ID3D11DeviceContext* context, TextureBindIndex bind, const Texture::Texture2D* tex);
+			static void BindTexture(ID3D11DeviceContext* context, TextureBindIndex bind, const Texture::TextureForm* tex);
 			static void BindSamplerState(ID3D11DeviceContext* context, SamplerStateBindIndex bind, ID3D11SamplerState* samplerState);
 			static void BindConstBuffer(ID3D11DeviceContext* context, ConstBufferBindIndex bind, const Buffer::ConstBuffer* cb);
 			static void BindShaderResourceBuffer(ID3D11DeviceContext* context, TextureBindIndex bind, const Buffer::ShaderResourceBuffer* srBuffer);
