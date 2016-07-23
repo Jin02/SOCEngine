7fabdc7a349a59537e56969a5cbf1fa1347cd520
Jin Park <quaggan02@gmail.com>
2016-07-23 14:22:00 +0900

    GeometryShader.h- Bind 추가 #32

--- SOCEngine/SOCEngine/Rendering/Shader/GeometryShader.h
+++ SOCEngine/SOCEngine/Rendering/Shader/GeometryShader.h
@@ -22,6 +22,12 @@ namespace Rendering
 			void BindShaderToContext(ID3D11DeviceContext* context);
 			void BindResourcesToContext(ID3D11DeviceContext* context, const std::vector<InputConstBuffer>* constBuffers, const std::vector<InputTexture>* textures, const std::vector<InputShaderResourceBuffer>* srBuffers);
 			void Clear(ID3D11DeviceContext* context, const std::vector<InputConstBuffer>* constBuffers, const std::vector<InputTexture>* textures, const std::vector<InputShaderResourceBuffer>* srBuffers);
+			
+		public:
+			static void BindTexture(ID3D11DeviceContext* context, TextureBindIndex bind, const Texture::Texture2D* tex);
+			static void BindSamplerState(ID3D11DeviceContext* context, SamplerStateBindIndex bind, ID3D11SamplerState* samplerState);
+			static void BindConstBuffer(ID3D11DeviceContext* context, ConstBufferBindIndex bind, const Buffer::ConstBuffer* cb);
+			static void BindShaderResourceBuffer(ID3D11DeviceContext* context, TextureBindIndex bind, const Buffer::ShaderResourceBuffer* srBuffer);
 		};
 	}
 }
