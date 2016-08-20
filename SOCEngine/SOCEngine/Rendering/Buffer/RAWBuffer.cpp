10d8b57e81935e9e00e8343a4c43cdd3f17e41ef
Jin <quaggan02@gmail.com>
2016-08-20 15:18:34 +0900

    fix error

--- SOCEngine/SOCEngine/Rendering/Buffer/RAWBuffer.cpp
+++ SOCEngine/SOCEngine/Rendering/Buffer/RAWBuffer.cpp
@@ -25,6 +25,7 @@ bool RAWBuffer::Initialize(uint stride, uint num)
 	desc.BindFlags				= D3D11_BIND_UNORDERED_ACCESS | D3D11_BIND_SHADER_RESOURCE;
 	desc.MiscFlags				= D3D11_RESOURCE_MISC_BUFFER_ALLOW_RAW_VIEWS;
 	desc.StructureByteStride	= stride;
+	desc.ByteWidth				= stride * num;
 
 	ID3D11Device* device = Director::SharedInstance()->GetDirectX()->GetDevice();
 	HRESULT hr = device->CreateBuffer(&desc, nullptr, &_buffer);
@@ -32,7 +33,7 @@ bool RAWBuffer::Initialize(uint stride, uint num)
 	ASSERT_COND_MSG(SUCCEEDED(hr), "Error!. Not create buffer");
 
 	if(_srv == nullptr)	_srv = new ShaderResourceView;
-	_srv->Initialize(_buffer, DXGI_FORMAT_R32_UINT, 0, D3D11_SRV_DIMENSION_BUFFER);
+	_srv->Initialize(_buffer, DXGI_FORMAT_R32_UINT, 0, D3D11_SRV_DIMENSION_BUFFER, num);
 
 	if(_uav == nullptr)	_uav = new UnorderedAccessView;
 	_uav->Initialize(DXGI_FORMAT_R32_TYPELESS, num, _buffer, D3D11_UAV_DIMENSION_BUFFER, 0, 0, D3D11_BUFFER_UAV_FLAG_RAW);
