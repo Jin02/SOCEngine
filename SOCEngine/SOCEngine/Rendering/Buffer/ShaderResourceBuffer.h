68b45ad6ff389e4884773608691666fc27b1a7da
Jin Park <quaggan02@gmail.com>
2016-08-31 16:03:28 +0900

    ShaderResourceBuffer - GetShaderResourceView 버그 수정

--- SOCEngine/SOCEngine/Rendering/Buffer/ShaderResourceBuffer.h
+++ SOCEngine/SOCEngine/Rendering/Buffer/ShaderResourceBuffer.h
@@ -20,7 +20,7 @@ namespace Rendering
 			void Initialize(uint stride, uint num, DXGI_FORMAT format, const void* sysMem, bool useMapWriteNoOverWrite, uint optionalBindFlag, D3D11_USAGE usage);
 
 		public:
-			GET_ACCESSOR(ShaderResourceView, ID3D11ShaderResourceView* const*, &_srv);
+			GET_ACCESSOR(ShaderResourceView, const View::ShaderResourceView*, _srv);
 		};
 	}
 }
