0986befcbe89cbbe2e16090dd21b1808f9ce8f8b
Jin Park <quaggan02@gmail.com>
2016-08-31 16:26:24 +0900

    RAWBuffer - Buffer 생성쪽 코드 정리

--- SOCEngine/SOCEngine/Rendering/Buffer/RAWBuffer.cpp
+++ SOCEngine/SOCEngine/Rendering/Buffer/RAWBuffer.cpp
@@ -18,20 +18,10 @@ RAWBuffer::~RAWBuffer()
 
 bool RAWBuffer::Initialize(uint stride, uint num)
 {
-	D3D11_BUFFER_DESC desc;
-	memset(&desc, 0, sizeof(D3D11_BUFFER_DESC));
-	desc.BindFlags			= D3D11_BIND_UNORDERED_ACCESS | D3D11_BIND_SHADER_RESOURCE;
-	desc.MiscFlags			= D3D11_RESOURCE_MISC_BUFFER_ALLOW_RAW_VIEWS;
-	desc.StructureByteStride	= stride;
-	desc.ByteWidth			= stride * num;
-
-	ID3D11Device* device = Director::SharedInstance()->GetDirectX()->GetDevice();
-	HRESULT hr = device->CreateBuffer(&desc, nullptr, &_buffer);
-
 	ASSERT_COND_MSG(SUCCEEDED(hr), "Error!. Not create buffer");
-
-	if(_srv == nullptr)	_srv = new ShaderResourceView;
-	_srv->Initialize(_buffer, DXGI_FORMAT_R32_UINT, 0, D3D11_SRV_DIMENSION_BUFFER, num, stride);
+	ShaderResourceBuffer::Initialize(stride, num,
+	DXGI_FORMAT_R32_UINT, nullptr, false,
+	D3D11_BIND_UNORDERED_ACCESS, D3D11_USAGE_DEFAULT, D3D11_RESOURCE_MISC_BUFFER_ALLOW_RAW_VIEWS);
 
 	if(_uav == nullptr)	_uav = new UnorderedAccessView;
 	_uav->Initialize(DXGI_FORMAT_R32_TYPELESS, num, _buffer, D3D11_UAV_DIMENSION_BUFFER, 0, 0, D3D11_BUFFER_UAV_FLAG_RAW);
