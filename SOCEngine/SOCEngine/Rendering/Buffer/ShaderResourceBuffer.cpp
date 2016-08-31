7d9fec3f53184ac375dbb9fa106882cb9714b1f9
Jin Park <quaggan02@gmail.com>
2016-08-31 16:24:31 +0900

    ShaderResourceBuffer - 임시로 miscFlag 추가

--- SOCEngine/SOCEngine/Rendering/Buffer/ShaderResourceBuffer.cpp
+++ SOCEngine/SOCEngine/Rendering/Buffer/ShaderResourceBuffer.cpp
@@ -20,7 +20,7 @@ ShaderResourceBuffer::~ShaderResourceBuffer()
 void ShaderResourceBuffer::Initialize(
 	uint stride, uint num, DXGI_FORMAT format,
 	const void* sysMem, bool useMapWriteNoOverWrite,
-	uint optionalBindFlag, D3D11_USAGE usage)
+	uint optionalBindFlag, D3D11_USAGE usage, D3D11_RESOURCE_MISC_FLAG miscFlag)
 {
 	D3D11_BUFFER_DESC desc;
 	memset(&desc, 0, sizeof(D3D11_BUFFER_DESC));
@@ -29,9 +29,12 @@ void ShaderResourceBuffer::Initialize(
 	desc.ByteWidth				= stride * num;
 	desc.BindFlags				= D3D11_BIND_SHADER_RESOURCE | optionalBindFlag;
 	desc.CPUAccessFlags			= useMapWriteNoOverWrite ? D3D11_CPU_ACCESS_WRITE : 0;
-	desc.StructureByteStride	= stride;
-//	desc.StructureByteStride	= optionalBindFlag & D3D11_BIND_UNORDERED_ACCESS ? 0 : stride;
-	desc.MiscFlags				= (format == DXGI_FORMAT_UNKNOWN) ? D3D11_RESOURCE_MISC_BUFFER_STRUCTURED : 0;
+	desc.StructureByteStride		= stride;
+
+	//아래 구문 수정해야함.
+	//misc를 처음부터 받아와서 구현하면 되지만, 어디서 수정되었는지를 현 상황에서 하나하나 찾기 힘듬..
+	if(miscFlag == 0)		desc.MiscFlags = (format == DXGI_FORMAT_UNKNOWN) ? D3D11_RESOURCE_MISC_BUFFER_STRUCTURED : 0;
+	else				desc.MiscFlags = miscFlag;
 
 	D3D11_SUBRESOURCE_DATA data;
 	data.pSysMem = sysMem;
