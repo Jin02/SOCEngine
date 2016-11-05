72175c0f214d50dd7e3e2786ab0ba129bc67bcca
Jin Park <quaggan02@gmail.com>
2016-11-05 22:32:30 +0900

    Component.h - TransformCB를 TransformCB.h로 옮김 #53

--- SOCEngine/SOCEngine/Core/Component/Component.h
+++ SOCEngine/SOCEngine/Core/Component/Component.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "Matrix.h"
+#include "TransformCB.h"
 #include "Common.h"
 #include <vector>
 
@@ -14,14 +14,6 @@ namespace Rendering
 	namespace Light
 	{
 		class LightForm;
-	};
-
-	struct TransformCB
-	{
-		Math::Matrix world;
-		Math::Matrix worldInvTranspose;
-
-		Math::Matrix prevWorld;
 	};
 };
 
