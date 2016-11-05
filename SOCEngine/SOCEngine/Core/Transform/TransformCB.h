7879d95ddc84f0bca98dd1a7211a1375aeaf484a
Jin Park <quaggan02@gmail.com>
2016-11-05 22:34:52 +0900

    TransformCB.h - ChangeState 추가 #53

--- SOCEngine/SOCEngine/Core/Transform/TransformCB.h
+++ SOCEngine/SOCEngine/Core/Transform/TransformCB.h
@@ -6,6 +6,14 @@ namespace Rendering
 {
 	struct TransformCB
 	{
+		enum class ChangeState : uint
+		{
+			No,
+			HasChanged,
+			HadChanged,
+			MAX
+		};
+		
 		Math::Matrix world;
 		Math::Matrix worldInvTranspose;
 
