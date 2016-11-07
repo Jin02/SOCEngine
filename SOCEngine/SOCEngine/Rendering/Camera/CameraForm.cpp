20b6e6ecee4fefda689f0006072f9125a9241694
Jin Park <quaggan02@gmail.com>
2016-11-07 22:55:20 +0900

    CameraForm.cpp - CullingWithUpdateCB -> _CullingWithUpdateCB

--- SOCEngine/SOCEngine/Rendering/Camera/CameraForm.cpp
+++ SOCEngine/SOCEngine/Rendering/Camera/CameraForm.cpp
@@ -167,7 +167,7 @@ void CameraForm::GetInvViewportMatrix(Math::Matrix& outMat, const Math::Rect<flo
 	Math::Matrix::Inverse(outMat, viewportMat);
 }
 
-bool CameraForm::CullingWithUpdateCB(const Device::DirectX* dx, const std::vector<Core::Object*>& objects, const LightManager* lightManager)
+bool CameraForm::_CullingWithUpdateCB(const Device::DirectX* dx, const std::vector<Core::Object*>& objects, const LightManager* lightManager)
 {
 	Matrix worldMat;
 	_owner->GetTransform()->FetchWorldMatrix(worldMat);
