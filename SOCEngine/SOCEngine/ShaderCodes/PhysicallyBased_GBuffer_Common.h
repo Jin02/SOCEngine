046d8c37b8ec084df2771cd5f9f9bde01c2faa73
Jin Park <quaggan02@gmail.com>
2016-11-09 23:31:09 +0900

    PhysicallyBased_GBuffer_Common.h - velocity 저장 추가 #53

--- SOCEngine/SOCEngine/ShaderCodes/PhysicallyBased_GBuffer_Common.h
+++ SOCEngine/SOCEngine/ShaderCodes/PhysicallyBased_GBuffer_Common.h
@@ -9,17 +9,17 @@
 
 struct GBuffer
 {
-	float4 albedo_occlusion					: SV_Target0;
+	float4 albedo_occlusion			: SV_Target0;
 	float4 motionXY_metallic_specularity	: SV_Target1;
-	float4 normal_roughness 				: SV_Target2;
-	float4 emission_materialFlag				: SV_Target3;
+	float4 normal_roughness 		: SV_Target2;
+	float4 emission_materialFlag		: SV_Target3;
 };
 
 SamplerState GBufferDefaultSampler 	: register( s0 );
 
 
-void MakeGBuffer(float3 worldNormal, float2 uv,
-				 out float4 albedo_occlusion, out float4 motionXY_metallic_specularity, out float4 normal_roughness, out float4 emission_materialFlag)
+void MakeGBuffer(float3 worldNormal, float2 uv, float2 velocity,
+		 out float4 albedo_occlusion, out float4 motionXY_metallic_specularity, out float4 normal_roughness, out float4 emission_materialFlag)
 {
 	float3 albedo = float3(0.0f, 0.0f, 0.0f);
 	{
@@ -49,12 +49,9 @@ void MakeGBuffer(float3 worldNormal, float2 uv,
 	}
 
 	float height = heightMap.Sample(GBufferDefaultSampler, uv).x;
-
-	float2 motion = float2(0.0f, 0.0f);
 	{
-
 	}
-	
+		
 	float metallic = 0.0f;
 	{
 		float metallicTex = metallicMap.Sample(GBufferDefaultSampler, uv).x;
@@ -63,15 +60,15 @@ void MakeGBuffer(float3 worldNormal, float2 uv,
 		metallic = lerp(mtlMetallic, metallicTex, HasMetallicMap());
 	}
 
-	albedo_occlusion.rgb				= albedo;
-	albedo_occlusion.a					= occlusion;
-	motionXY_metallic_specularity.rg	= motion;
+	albedo_occlusion.rgb			= albedo;
+	albedo_occlusion.a			= occlusion;
+	motionXY_metallic_specularity.rg	= velocity;
 	motionXY_metallic_specularity.b		= metallic;
 	motionXY_metallic_specularity.a		= specularity;
-	normal_roughness.rgb				= normal;
-	normal_roughness.a					= roughness;
-	emission_materialFlag.rgb			= emissiveColor;
-	emission_materialFlag.a				= float(GetMaterialFlag()) / 255.0f;
+	normal_roughness.rgb			= normal;
+	normal_roughness.a			= roughness;
+	emission_materialFlag.rgb		= emissiveColor;
+	emission_materialFlag.a			= float(GetMaterialFlag()) / 255.0f;
 }
 
 #endif
