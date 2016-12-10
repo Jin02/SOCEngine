29151bca716ed05f1173239be9baab585072f60d
Jin <quaggan02@gmail.com>
2016-12-10 15:00:11 +0900

    LightManager - UpdateBuffer에 getLightShaftIndex추가 및 코드 정리, 제대로 된 OptionalPackedParam 계산 추가 #54

--- SOCEngine/SOCEngine/Rendering/Light/LightManager.h
+++ SOCEngine/SOCEngine/Rendering/Light/LightManager.h
@@ -64,7 +64,8 @@ namespace Rendering
 
 		private:
 			void UpdateSRBufferUsingMapDiscard(	ID3D11DeviceContext* context,
-												const std::function<uint(const Light::LightForm*)>& getShadowIndexInEachShadowLights);
+												const std::function<ushort(const Light::LightForm*)>& getShadowIndexInEachShadowLights,
+												const std::function<uchar(const Light::LightForm*)>& getLightShaftIndex);
 			void UpdateSRBufferUsingMapNoOverWrite(ID3D11DeviceContext* context);
 
 		public:
@@ -73,7 +74,8 @@ namespace Rendering
 
 			uint Add(Light::LightForm*& light);
 			void UpdateSRBuffer(const Device::DirectX* dx,
-								const std::function<uint(const Light::LightForm*)>& getShadowIndexInEachShadowLights);
+								const std::function<ushort(const Light::LightForm*)>& getShadowIndexInEachShadowLights,
+								const std::function<uchar(const Light::LightForm*)>& getLightShaftIndex);
 			void ComputeDirectionalLightViewProj(const Intersection::BoundBox& sceneBoundBox, float directionalLightShadowMapResolution);
 
 			bool Has(Light::LightForm*& light) const;
