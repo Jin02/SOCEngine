28980c4e356d7105f6a945552021ac2ada31cbe9
Jin <quaggan02@gmail.com>
2016-12-11 15:36:38 +0900

    Bloom에 사용할 EyeAdaptation 추가 #44

---
+++ SOCEngine/SOCEngine/ShaderCodes/EyeAdaptation.hlsl
@@ -0,0 +1,20 @@
+//EMPTY_META_DATA
+
+#include "FullScreenShader.h"
+#include "ShaderCommon.h"
+#include "HDRCommon.h"
+
+Texture2D<float4>	CurColorMap				: register( t0 );
+Texture2D<float4>	PrevAdaptedLuminanceMap	: register( t1 );
+
+SamplerState		DefaultSampler			: register( s0 );
+
+float EyeAdaptation_InFullScreen_PS(PS_INPUT input) : SV_Target
+{
+	float curLum	= Luminance(CurColorMap.SampleLevel(DefaultSampler, float2(0.5f, 0.5f), 10).rgb);
+	float prevLum	= PrevAdaptedLuminanceMap.Load(uint3(0, 0, 0)).x;
+
+	float result	= prevLum + (curLum - prevLum) * (1.0f - exp(-hdr_timeDelta * GetExposureSpeed()));
+
+	return result;
+}
