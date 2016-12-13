e9c2ab43182f68e5e2bf6567a6f326294cbb3eb1
Jin <quaggan02@gmail.com>
2016-12-13 13:04:05 +0900

    EyeAdaption - miplevel 11로 설정

--- SOCEngine/SOCEngine/ShaderCodes/EyeAdaptation.hlsl
+++ SOCEngine/SOCEngine/ShaderCodes/EyeAdaptation.hlsl
@@ -11,7 +11,7 @@ SamplerState		DefaultSampler			: register( s0 );
 
 float EyeAdaptation_InFullScreen_PS(PS_INPUT input) : SV_Target
 {
-	float curLum	= Luminance(CurColorMap.SampleLevel(DefaultSampler, float2(0.5f, 0.5f), 10).rgb);
+	float curLum	= Luminance(CurColorMap.SampleLevel(DefaultSampler, float2(0.5f, 0.5f), 11).rgb);
 	float prevLum	= PrevAdaptedLuminanceMap.Load(uint3(0, 0, 0)).x;
 
 	float result	= prevLum + (curLum - prevLum) * (1.0f - exp(-hdr_timeDelta * hdr_exposureSpeed));
