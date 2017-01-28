fd1c19aff8b2a2c850cb45e5c5f748bc4fb5e02b
Jin Park <quaggan02@gmail.com>
2017-01-29 00:01:24 +0900

    Update and rename LightGPUUploadBufferInterface.h to ILightGPUUploadBuffer.h

---
+++ SOCEngine/SOCEngine/Rendering/Light/ILightGPUUploadBuffer.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace Rendering
+{
+	namespace Manager
+	{
+		class ILightGPUUploadBuffer
+		{		
+		public:
+			ILightGPUUploadBuffer() {}
+			virtual ~ILightGPUUploadBuffer() {}
+		};
+	}
+}
