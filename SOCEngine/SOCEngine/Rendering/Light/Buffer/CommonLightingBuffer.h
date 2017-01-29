a1efc4265748ac24dec1ee40a25c068d05ba9936
Jin Park <quaggan02@gmail.com>
2017-01-30 01:37:39 +0900

    CommonLightingBuffer.h - cpp 추가

--- SOCEngine/SOCEngine/Rendering/Light/Buffer/CommonLightingBuffer.h
+++ SOCEngine/SOCEngine/Rendering/Light/Buffer/CommonLightingBuffer.h
@@ -19,70 +19,18 @@ namespace Rendering
 				OptionalParamIndexBuffer*							_optionalParamIndexBuffer;
 			
 			public:
-				CommonLightingBuffer()
-					: _colorBuffer(nullptr), _optionalParamIndexBuffer(nullptr)
-				{
-				}
-				
-				~CommonLightingBuffer()
-				{
-					SAFE_DELETE(_colorBuffer);
-					SAFE_DELETE(_optionalParamIndexBuffer);
-				}
+				CommonLightingBuffer();	
+				~CommonLightingBuffer();
 			
-			protected:
-				void Initialize(uint count)
-				{
-					_colorBuffer				= ColorBuffer::Create(count,				DXGI_FORMAT_R8G8B8A8_UNORM,			dummyData);
-					_optionalParamIndexBuffer	= OptionalParamIndexBuffer::Create(count,	DXGI_FORMAT_R32_UINT,				dummyData);	
-				}
-				
-				void Destroy()
-				{
-					_colorBuffer->Destroy();
-					_optionalParamIndexBuffer->Destroy();
-				}
+			public:
+				void Initialize(uint count);				
+				void Destroy();
 				
 				void UpdateBuffer(const Light::LightForm* light,
 									const std::function<uchar(const Light::LightForm*)>& getShadowIndex,
-									const std::function<uchar(const Light::LightForm*)>& getLightShaftIndex)
-				{
-						address key = reinterpret_cat<address>(light);
-						
-						// Compute Optional Param Index
-						ushort	shadowIndex				= getShadowIndex(light);
-						uchar	lightShaftIndex			= getLightShaftIndex(light) & 0x7f;
-						uchar	lightFlag				= light->GetFlag();
-						uint	optionalParamIndex		= (shadowIndex << 16) | (lightFlag << 8) | lightShaftIndex;
-						
-						uint	uintColor				= light->Get32BitMainColor();
-						
-						uint* existColor = _colorBuffer->Find(key);
-						if(existColor == nullptr)
-						{
-							_colorBuffer->Add(key, uintColor);
-							_optionalParamIndexBuffer->Add(key, optionalParamIndex);
-						}
-						else	// existColor != nullptr
-						{
-							(*existColor)							= uintColor;
-							(*_optionalParamIndexBuffer->Find(key))	= optionalParamIndex;
-						}
-				}
-				
-				void UpdateSRBuffer(ID3D11DeviceContext* context)
-				{
-					_colorBuffer->UpdateSRBuffer(context);
-					_optionalParamIndexBuffer->UpdateSRBuffer(context);
-				}
-				
-				void Delete(const Light::LightForm* light)
-				{
-					address key = reinterpret_cast<address>(light);
-					
-					_colorBuffer->Delete(key);
-					_optionalParamIndexBuffer->Delete(key);				
-				}
+									const std::function<uchar(const Light::LightForm*)>& getLightShaftIndex);				
+				void UpdateSRBuffer(ID3D11DeviceContext* context);				
+				void Delete(const Light::LightForm* light);
 			};
 		}
 	}
