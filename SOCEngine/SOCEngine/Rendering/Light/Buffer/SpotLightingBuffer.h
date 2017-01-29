6dbb323cfcf4d20bd64ac723d8ad9cd4bba84117
Jin Park <quaggan02@gmail.com>
2017-01-30 08:59:49 +0900

    SpotLightingBuffer.h - DeleteAll() 추가

--- SOCEngine/SOCEngine/Rendering/Light/Buffer/SpotLightingBuffer.h
+++ SOCEngine/SOCEngine/Rendering/Light/Buffer/SpotLightingBuffer.h
@@ -16,9 +16,9 @@ namespace Rendering
 				typedef Buffer::GPUUploadBuffer<address, LightForm::LightTransformBuffer>	TransformBuffer;
 				typedef Buffer::GPUUploadBuffer<address, Light::SpotLight::Param>		ParamBuffer;
 
-				TransformBuffer*				_transformBuffer;
-				ParamBuffer*					_paramBuffer;
-				CommonLightingBuffer				_commonBuffer;
+				TransformBuffer*								_transformBuffer;
+				ParamBuffer*									_paramBuffer;
+				CommonLightingBuffer								_commonBuffer;
 				
 			public:
 				SpotLightingBuffer();
@@ -40,6 +40,7 @@ namespace Rendering
 							const std::function<uchar(const Light::LightForm*)>& getLightShaftIndex,
 							bool forcedUpdate = false	);				
 				void Delete(const Light::SpotLight* light);
+				void DeleteAll();
 			};
 		}
 	}
