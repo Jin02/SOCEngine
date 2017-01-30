4b47f12dce5f67dd2f10601d447936de3afc51a6
Jin Park <quaggan02@gmail.com>
2017-01-30 23:10:37 +0900

    PointLightingBuffer.h - cpp와 분리

--- SOCEngine/SOCEngine/Rendering/Light/Buffer/PointLightingBuffer.h
+++ SOCEngine/SOCEngine/Rendering/Light/Buffer/PointLightingBuffer.h
@@ -22,28 +22,10 @@ namespace Rendering
 				inline void Destroy()		{ Parent::Destroy(); }
 				
 			private:
-				virtual TransformBuffer* InitializeTransformBuffer(uint maxLightCount)
-				{
-					const __int32 dummy[maxLightCount * sizeof(LightForm::LightTransformBuffer) / 4] = {0, };
-					return TransformBuffer::Create(maxLightCount, DXGI_FORMAT_R32G32B32A32_FLOAT, dummy);
-				}
-				
+				virtual TransformBuffer* InitializeTransformBuffer(uint maxLightCount);
 				virtual bool UpdateBuffer(const LightWithPrevUpdateCounter& lightWithPrevUC,
 							const std::function<uchar(const Light::LightForm*)>& getShadowIndex,
-							const std::function<uchar(const Light::LightForm*)>& getLightShaftIndex)
-				{
-					auto _UpdateBuffer = [&](const LightForm* light) -> void
-					{
-						address key = reinterpret_cat<address>(light);
-						
-						LightForm::LightTransformBuffer	transform;
-						static_cast<const PointLight*>(light)->MakeParam(transform);
-						
-						Parent::UpdateTransformBuffer(key, transform);
-					};
-					
-					return lightWithPrevUC->UpdateBuffer(_UpdateBuffer);
-				}
+							const std::function<uchar(const Light::LightForm*)>& getLightShaftIndex);
 				virtual void UpdateAdditionalSRBuffer(ID3D11DeviceContext* context) { }
 			};
 		}
