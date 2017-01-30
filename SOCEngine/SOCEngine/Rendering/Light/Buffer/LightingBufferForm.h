767348afc77ee89a15ea856362b1e9b2249c3db8
Jin Park <quaggan02@gmail.com>
2017-01-30 12:30:12 +0900

    Create LightingBufferForm.h

---
+++ SOCEngine/SOCEngine/Rendering/Light/Buffer/LightingBufferForm.h
@@ -0,0 +1,88 @@
+#pragma once
+
+#include "GPUUploadBuffer.h"
+#include "LightWithPrevUpdateCounter.h"
+#include "CommonLightingBuffer.h"
+
+namespace Rendering
+{
+	namespace Light
+	{
+		namespace LightingBuffer
+		{
+			template<typename TransformType>
+			class LightingBufferForm
+			{
+			private:
+				typedef Buffer::GPUUploadBuffer<address, TransformType>	TransformBuffer;
+
+				TransformBuffer*					_transformBuffer;
+				CommonLightingBuffer					_commonBuffer;
+				
+			public:
+				LightingBufferForm() : _transformBuffer(nullptr), _commonBuffer()
+				{
+				}
+				virtual ~LightingBufferForm()
+				{
+					Destroy();	
+					SAFE_DELETE(_transformBuffer);
+				}
+				
+			public:
+				void Initialize(uint maxLightCount)
+				{
+					const __int32 dummyData[maxLightCount * sizeof(TransformType) / 4] = {0, };
+
+					_transformBuffer = InitializeTransformBuffer(maxLightCount);
+					_commonBuffer.Initialize(maxLightCount);
+				}
+
+				void Destroy()
+				{
+					_commonBuffer.Destroy();
+					_transformBuffer->Destroy();
+				}
+				
+			private:
+				virtual TransformBuffer* InitializeTransformBuffer(uint maxLightCount) = 0;
+				virtual bool UpdateBuffer(const LightWithPrevUpdateCounter& lightWithPrevUC,
+							const std::function<uchar(const Light::LightForm*)>& getShadowIndex,
+							const std::function<uchar(const Light::LightForm*)>& getLightShaftIndex) = 0;
+				virtual void UpdateAdditionalSRBuffer(ID3D11DeviceContext* context) = 0;
+				
+			public:
+				void UpdateSRBuffer(ID3D11DeviceContext* context,
+								const LightWithPrevUpdateCounter& lightWithPrevUC,
+								const std::function<uchar(const Light::LightForm*)>& getShadowIndex,
+								const std::function<uchar(const Light::LightForm*)>& getLightShaftIndex,
+								bool forcedUpdate = false)
+				{
+					bool isNeedToUpdate = UpdateBuffer(lightWithPrevUC, getShadowIndex, getLightShaftIndex);
+
+					if((isNeedToUpdate || forcedUpdate) == false)
+						return;
+					
+					_transformBuffer->UpdateSRBuffer(context);					
+					_commonBuffer.UpdateSRBuffer(context);
+					
+					UpdateAdditionalSRBuffer(context);
+				}
+
+				void Delete(const Light::PointLightingBuffer* light)
+				{
+					address key = reinterpret_cast<address>(light);
+					
+					_transformBuffer->Delete(key);
+					_commonBuffer.Delete(key);
+				}
+				
+				void DeleteAll()
+				{
+					_transformBuffer->DeleteAll();
+					_commonBuffer.DeleteAll();					
+				}
+			};
+		}
+	}
+}
