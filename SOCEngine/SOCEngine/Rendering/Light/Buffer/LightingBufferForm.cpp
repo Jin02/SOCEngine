3609be65d3a61a53bb2ff28eab4e72354945b6bd
Jin Park <quaggan02@gmail.com>
2017-01-30 09:12:33 +0900

    Update and rename LightingBuffer.cpp to LightingBufferForm.cpp

--- SOCEngine/SOCEngine/Rendering/Light/Buffer/LightingBufferForm.cpp
+++ SOCEngine/SOCEngine/Rendering/Light/Buffer/LightingBufferForm.cpp
@@ -1,32 +1,33 @@
-#include "CommonLightingBuffer.h"
+#include "LightingBufferForm.h"
 
 using namespace Rednering;
 using namespace Rednering::Light;
+using namespace Rendering::Light::LightingBuffer;
 
-LightingBuffer::LightingBuffer()
+LightingBufferForm::LightingBufferForm()
   : _colorBuffer(nullptr), _optionalParamIndexBuffer(nullptr)
 {
 }
 
-LightingBuffer::~LightingBuffer()
+LightingBufferForm::~LightingBufferForm()
 {
 	SAFE_DELETE(_colorBuffer);
 	SAFE_DELETE(_optionalParamIndexBuffer);
 }
 
-void LightingBuffer::Initialize(uint count)
+void LightingBufferForm::Initialize(uint count)
 {
 	_colorBuffer			= ColorBuffer::Create(count,			DXGI_FORMAT_R8G8B8A8_UNORM,	dummyData);
 	_optionalParamIndexBuffer	= OptionalParamIndexBuffer::Create(count,	DXGI_FORMAT_R32_UINT,		dummyData);	
 }
 
-void LightingBuffer::Destroy()
+void LightingBufferForm::Destroy()
 {
 	_colorBuffer->Destroy();
 	_optionalParamIndexBuffer->Destroy();
 }
 
-void LightingBuffer::UpdateBuffer(const Light::LightForm* light,
+void LightingBufferForm::UpdateBuffer(const Light::LightForm* light,
 				  const std::function<uchar(const Light::LightForm*)>& getShadowIndex,
 				  const std::function<uchar(const Light::LightForm*)>& getLightShaftIndex,
 				  bool existElem)
@@ -53,13 +54,13 @@ void LightingBuffer::UpdateBuffer(const Light::LightForm* light,
 	}
 }
 
-void LightingBuffer::UpdateSRBuffer(ID3D11DeviceContext* context)
+void LightingBufferForm::UpdateSRBuffer(ID3D11DeviceContext* context)
 {
 	_colorBuffer->UpdateSRBuffer(context);
 	_optionalParamIndexBuffer->UpdateSRBuffer(context);
 }
 
-void LightingBuffer::Delete(const Light::LightForm* light)
+void LightingBufferForm::Delete(const Light::LightForm* light)
 {
 	address key = reinterpret_cast<address>(light);
 	
@@ -67,7 +68,7 @@ void LightingBuffer::Delete(const Light::LightForm* light)
 	_optionalParamIndexBuffer->Delete(key);	
 }
 
-void LightingBuffer::DeleteAll()
+void LightingBufferForm::DeleteAll()
 {
 	_colorBuffer->DeleteAll();
 	_optionalParamIndexBuffer->DeleteAll();
