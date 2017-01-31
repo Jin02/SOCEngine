7e46200dba4f219db19ac2e906e0074e3202c881
Jin Park <quaggan02@gmail.com>
2017-01-31 23:34:09 +0900

    PointLightingBuffer.cpp - UpdateBuffer 제거 및 버그 수정

--- SOCEngine/SOCEngine/Rendering/Light/Buffer/PointLightingBuffer.cpp
+++ SOCEngine/SOCEngine/Rendering/Light/Buffer/PointLightingBuffer.cpp
@@ -14,23 +14,6 @@ PointLightingBuffer::~PointLightingBuffer()
 
 TransformBuffer* PointLightingBuffer::InitializeTransformBuffer(uint maxLightCount)
 {
-	const __int32 dummy[maxLightCount * sizeof(LightForm::LightTransformBuffer) / 4] = {0, };
+	const __int32 dummy[maxLightCount * sizeof(LightingBufferForm::TransformType) / 4] = {0, };
 	return TransformBuffer::Create(maxLightCount, DXGI_FORMAT_R32G32B32A32_FLOAT, dummy);
-}
-
-bool PointLightingBuffer::UpdateBuffer(	const LightWithPrevUpdateCounter& lightWithPrevUC,
-					const std::function<uchar(const LightForm*)>& getShadowIndex,
-					const std::function<uchar(const LightForm*)>& getLightShaftIndex)
-{
-	auto _UpdateBuffer = [&](const LightForm* light) -> void
-	{
-		address key = reinterpret_cat<address>(light);
-		
-		LightForm::LightTransformBuffer	transform;
-		static_cast<const PointLight*>(light)->MakeParam(transform);
-		
-		Parent::UpdateTransformBuffer(key, transform);
-	};
-	
-	return lightWithPrevUC->UpdateBuffer(_UpdateBuffer);
 }
