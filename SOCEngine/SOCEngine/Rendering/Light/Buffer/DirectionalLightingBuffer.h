08e015df9bc1585e0f656c5d169aebd4a3aa1b41
Jin Park <quaggan02@gmail.com>
2017-01-31 23:36:53 +0900

    DirectionalLightingBuffer.h - 재구현

--- SOCEngine/SOCEngine/Rendering/Light/Buffer/DirectionalLightingBuffer.h
+++ SOCEngine/SOCEngine/Rendering/Light/Buffer/DirectionalLightingBuffer.h
@@ -8,50 +8,22 @@ namespace Rendering
 	{
 		namespace LightingBuffer
 		{
-			class DirectionalLightingBuffer : public LightingBufferForm<DirectionalLight::DirXYHalf>
+			class DirectionalLightingBuffer : public LightingBufferForm<DirectionalLight>
 			{
 			private:
-				typedef LightingBufferForm<DirectionalLight::DirXYHalf> Parent;
-			
+				typedef LightingBufferForm<DirectionalLight> Parent;
+
 			public:
-				DirectionalLightingBuffer() : Parent()
-				{
-				}
-				
-				~DirectionalLightingBuffer()
-				{
-				}
+				DirectionalLightingBuffer();
+				~DirectionalLightingBuffer();
 				
 			public:
 				inline void Initialize()	{ Parent::Initialize(DIRECTIONAL_LIGHT_BUFFER_MAX_NUM); }				
 				inline void Destroy()		{ Parent::Destroy(); }
 				
 			private:
-				virtual TransformBuffer* InitializeTransformBuffer(uint maxLightCount)
-				{
-					const __int32 dummy[maxLightCount * sizeof(DirectionalLight::DirXYHalf) / 4] = {0, };
-					return TransformBuffer::Create(maxLightCount, DXGI_FORMAT_R16G16_FLOAT, dummy);				
-				}
-				
-				virtual bool UpdateBuffer(const LightWithPrevUpdateCounter& lightWithPrevUC,
-							const std::function<uchar(const Light::LightForm*)>& getShadowIndex,
-							const std::function<uchar(const Light::LightForm*)>& getLightShaftIndex)
-				{
-					auto _UpdateBuffer = [&](const LightForm* light) -> void
-					{
-						address key = reinterpret_cat<address>(light);
-						
-						DirectionalLight::DirXYHalf dirXY;
-						static_cast<const DirectionalLight*>(light)->MakeParam(dirXY);
-						
-						Parent::UpdateTransformBuffer(key, dirXY);
-					};
-					
-					return lightWithPrevUC->UpdateBuffer(_UpdateBuffer);					
-				}
-							
-				virtual void UpdateAdditionalSRBuffer(ID3D11DeviceContext* context) { }			
-			}
+				virtual TransformBuffer* InitializeTransformBuffer(uint maxLightCount);
+			};
 		}
 	}
 }
