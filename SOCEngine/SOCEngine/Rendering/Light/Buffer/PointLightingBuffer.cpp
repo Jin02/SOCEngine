46acbc011b8192a0342182e5611f5649f725884a
Jin Park <quaggan02@gmail.com>
2017-01-31 23:38:20 +0900

    PointLightingBuffer.cpp - LightingBufferForm::TransformType -> Parent::TransformType

--- SOCEngine/SOCEngine/Rendering/Light/Buffer/PointLightingBuffer.cpp
+++ SOCEngine/SOCEngine/Rendering/Light/Buffer/PointLightingBuffer.cpp
@@ -14,6 +14,6 @@ PointLightingBuffer::~PointLightingBuffer()
 
 TransformBuffer* PointLightingBuffer::InitializeTransformBuffer(uint maxLightCount)
 {
-	const __int32 dummy[maxLightCount * sizeof(LightingBufferForm::TransformType) / 4] = {0, };
+	const __int32 dummy[maxLightCount * sizeof(Parent::TransformType) / 4] = {0, };
 	return TransformBuffer::Create(maxLightCount, DXGI_FORMAT_R32G32B32A32_FLOAT, dummy);
 }
