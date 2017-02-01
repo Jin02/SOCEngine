6858c80ab9cb7f3286fbc85b91aad1c25492be02
Jin Park <quaggan02@gmail.com>
2017-02-01 23:09:05 +0900

    DirectionalLightingBuffer.cpp - Create 추가

--- SOCEngine/SOCEngine/Rendering/Light/Buffer/DirectionalLightingBuffer.cpp
+++ SOCEngine/SOCEngine/Rendering/Light/Buffer/DirectionalLightingBuffer.cpp
@@ -17,3 +17,11 @@ TransformBuffer* DirectionalLightingBuffer::InitializeTransformBuffer(uint maxLi
 	const __int32 dummy[maxLightCount * sizeof(Parent::TransformType) / 4] = {0, };
 	return TransformBuffer::Create(maxLightCount, DXGI_FORMAT_R16G16_FLOAT, dummy);
 }
+
+DirectionalLightingBuffer* DirectionalLightingBuffer::Create()
+{
+	DirectionalLightingBuffer* buff = new DirectionalLightingBuffer;
+	buff->Initialize();
+	
+	return buff;
+}
