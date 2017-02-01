f13e606a4395f032f5764415966122a06c48601d
Jin Park <quaggan02@gmail.com>
2017-02-01 15:03:19 +0900

    SpotLightingBuffer.cpp - 재구현, 미완성

--- SOCEngine/SOCEngine/Rendering/Light/Buffer/SpotLightingBuffer.cpp
+++ SOCEngine/SOCEngine/Rendering/Light/Buffer/SpotLightingBuffer.cpp
@@ -12,91 +12,32 @@ SpotLightingBuffer::SpotLightingBuffer()
 SpotLightingBuffer::~SpotLightingBuffer()
 {
 	Destroy();
-	
-	SAFE_DELETE(_transformBuffer);
 	SAFE_DELETE(_paramBuffer);
 }
 
 void SpotLightingBuffer::Initialize()
 {
-	const __int32 dummyData[SPOT_LIGHT_BUFFER_MAX_NUM * sizeof(LightForm::LightTransformBuffer) / 4] = {0, };
+	const __int32 dummyData[SPOT_LIGHT_BUFFER_MAX_NUM * sizeof(SpotLight::Param) / 4] = {0, };
+	_paramBuffer = ParamBuffer::Create(SPOT_LIGHT_BUFFER_MAX_NUM,  DXGI_FORMAT_R16G16B16A16_FLOAT, dummyData);
 	
-	_transformBuffer	= TransformBuffer::Create(SPOT_LIGHT_BUFFER_MAX_NUM,	DXGI_FORMAT_R32G32B32A32_FLOAT,		dummyData);
-	_paramBuffer		= ParamBuffer::Create(SPOT_LIGHT_BUFFER_MAX_NUM, 	DXGI_FORMAT_R16G16B16A16_FLOAT,		dummyData);
-
-	_commonBuffer.Initialize(SPOT_LIGHT_BUFFER_MAX_NUM);
+	Parent::Initialize(SPOT_LIGHT_BUFFER_MAX_NUM);
 }
 
 void SpotLightingBuffer::Destroy()
 {
-	_transformBuffer->Destroy();
 	_paramBuffer->Destroy();
-	
-	_commonBuffer.Destroy();
+	Parent::Destroy();
 }
 
-bool SpotLightingBuffer::UpdateBuffer(	const LightWithPrevUpdateCounter& lightWithPrevUC,
-					const std::function<uchar(const LightForm*)>& getShadowIndex,
-					const std::function<uchar(const LightForm*)>& getLightShaftIndex)
-{
-	auto _UpdateBuffer = [&](const LightForm* light) -> void
-	{
-		address key = reinterpret_cat<address>(light);
-		
-		LightForm::LightTransformBuffer	transform;
-		SpotLight::Param		param;
-		static_cast<const SpotLight*>(light)->MakeParam(transform, param);
-								
-		// 하나만 검색해도 됨
-		LightForm::LightTransformBuffer* existTarnsform = _transformBuffer->Find(key);
-		if( existTarnsform == nullptr )
-		{
-			_transformBuffer->Add(key, transform);
-			_paramBuffer->Add(key, param);
-		}
-		else // existTarnsform != nullptr
-		{
-			(*existTarnsform)		= transform;
-			(*_paramBuffer->Find(key))	= param;
-		}
-		
-		_commonBuffer.UpdateBuffer(light, getShadowIndex, getLightShaftIndex, existTarnsform != nullptr);
-	};
-	
-	return lightWithPrevUC->UpdateBuffer(_UpdateBuffer);
-}
+/*
+				virtual void UpdateAdditionalBuffer(const LightType* light, bool existElem) {}
+				virtual void UpdateAdditionalSRBuffer(ID3D11DeviceContext* context) {}
+				virtual void OnDelete(const LightType* light) { }
+				virtual void OnDeleteAll() {}
+*/
 
-void SpotLightingBuffer::UpdateSRBuffer(ID3D11DeviceContext* context,
-					const LightWithPrevUpdateCounter& lightWithPrevUC,
-					const std::function<uchar(const LightForm*)>& getShadowIndex,
-					const std::function<uchar(const LightForm*)>& getLightShaftIndex,
-				   	bool forcedUpdate)
+TransformBuffer* SpotLightingBuffer::InitializeTransformBuffer(uint maxLightCount)
 {
-	bool isNeedToUpdate = UpdateBuffer(lightWithPrevUC, getShadowIndex, getLightShaftIndex);
-
-	if((isNeedToUpdate || forcedUpdate) == false)
-		return;
-	
-	_transformBuffer->UpdateSRBuffer(context);
-	_paramBuffer->UpdateSRBuffer(context);
-	
-	_commonBuffer.UpdateSRBuffer(context);
-}
-
-void SpotLightingBuffer::Delete(const SpotLight* light)
-{
-	address key = reinterpret_cast<address>(light);
-	
-	_transformBuffer->Delete(key);
-	_paramBuffer->Delete(key);
-	
-	_commonBuffer.Delete(key);
-}
-
-void SpotLightingBuffer::DeleteAll()
-{
-	_transformBuffer->DeleteAll();
-	_paramBuffer->DeleteAll();
-	
-	_commonBuffer.DeleteAll();
+	const __int32 dummy[maxLightCount * sizeof(Parent::TransformType) / 4] = {0, };
+	return TransformBuffer::Create(maxLightCount, DXGI_FORMAT_R32G32B32A32_FLOAT, dummy);
 }
