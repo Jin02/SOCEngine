3db2e9355f8ce33ed47c469b7c264b15ea2fe21a
Jin Park <quaggan02@gmail.com>
2017-02-05 16:18:57 +0900

    EfficientUpdater.hpp - getCounter 수정

--- SOCEngine/SOCEngine/Utility/EfficientUpdater.hpp
+++ SOCEngine/SOCEngine/Utility/EfficientUpdater.hpp
@@ -6,19 +6,19 @@ namespace Utility
 	class EfficientUpdater
 	{
 	private:
-		const T* 	_target;
-		uint 		_prevUpdateCounter;
+		const T* 			_target;
+		uint 				_prevUpdateCounter;
 	
-		const std::function<uint(const T* target)>& _getCounter;
+		const std::function<uint()>&	_getCounter;
 		
 	public:
-		EfficientUpdater(const T* target, const std::function<uint(const T* target)>& getUpdateCounter)
-			: _target(target), _prevUpdateCounter(getUpdateCounter(target) - 1), _getCounter(getUpdateCounter) { }
+		EfficientUpdater(const T* target, const std::function<uint()>& getUpdateCounter)
+			: _target(target), _prevUpdateCounter(getUpdateCounter() - 1), _getCounter(getUpdateCounter) { }
 		~EfficientUpdater() {}
 		
 		bool UpdateTarget(const std::function<void(const T* target)>& updateCallback)
 		{
-			uint curCounter = _getCounter(_target);
+			uint curCounter = _getCounter();
 	
 			bool isChanged = curCounter != _prevUpdateCounter;
 			if(isChanged)
