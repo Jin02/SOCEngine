8f97b3676c13433ab5b03993faebd995273ab90b
Jin <quaggan02@gmail.com>
2017-03-12 03:20:03 +0900

    Rect bug fix

--- SOCEngine/SOCEngine/Math/Rect.h
+++ SOCEngine/SOCEngine/Math/Rect.h
@@ -14,7 +14,7 @@ template <typename Type>
 struct Rect
 {
 public:
-	Rect() : x(0), y(0) { }
+	Rect() : x(0), y(0), size(0, 0) { }
 	Rect(Type _x, Type _y, Type _w, Type _h) : x(_x), y(_y), size(_w, _h) { }
 
 public:
