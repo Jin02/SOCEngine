a496c50464ffe783d78ab0bf015ccfd62f615968
Jin <quaggan02@gmail.com>
2017-03-12 02:54:02 +0900

    테스트용 main만 추가

--- SOCTestProject/SOCTestProject/Code/main.cpp
+++ SOCTestProject/SOCTestProject/Code/main.cpp
@@ -1,14 +1,6 @@
 #include <Windows.h>
-#include <Director.h>
-#include "TestScene.h"
 
 INT WINAPI wWinMain( HINSTANCE hInst, HINSTANCE, LPWSTR, INT )
 {
-	auto rect = Math::Rect<unsigned int>(0, 0, 1600, 900);
-	Device::Director::SharedInstance()->Initialize(rect, rect, hInst, "Test", true, false, 0, false);
-	Device::Director::SharedInstance()->SetCurrentScene(new TestScene);
-	Device::Director::SharedInstance()->Run();
-	Device::Director::Destroy();
-
 	return 0;
 }
