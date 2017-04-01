54a8314d18fa2fa9d235297a887daa43a21b11c2
Jin Park <quaggan02@gmail.com>
2017-04-01 22:26:57 +0900

    Update Sphere.cpp

--- SOCEngine/SOCEngine/Intersection/Sphere.cpp
+++ SOCEngine/SOCEngine/Intersection/Sphere.cpp
@@ -3,25 +3,12 @@
 using namespace Intersection;
 using namespace Math;
 
-Sphere::Sphere(void)
-	: center(0.0f, 0.0f, 0.0f), radius(0.0f)
-{
-}
-
-Sphere::Sphere(Vector3 center, float radius) :
-	center(center), radius(radius)
-{
-}
-
 bool Sphere::Intersects(const Sphere& sphere1, const Sphere& sphere2)
 {
 	Vector3 between = sphere1.center - sphere2.center;
 	float distance = between.Length();
 
-	if (distance <= (sphere1.radius + sphere2.radius))
-		return true;
-
-	return false;
+	return distance <= (sphere1.radius + sphere2.radius);
 }
 
 bool Sphere::Intersects(const Sphere &sphere)
