47efd0cc1592293046ae31f08a829bab79029244
Jin Park <quaggan02@gmail.com>
2017-04-01 22:10:02 +0900

    Update Half.h

--- SOCEngine/SOCEngine/Common/Half.h
+++ SOCEngine/SOCEngine/Common/Half.h
@@ -7,12 +7,11 @@ class Half final
 public:
 	explicit Half(ushort);
 	explicit Half(float);
+	
 	GET_CONST_ACCESSOR(Value, ushort, _value);
+
 	operator float() const;
-	Half operator=(float f) const
-	{
-		return Half(f);
-	}
+	Half operator=(float f) const { return Half(f); }
 
 private:
 	ushort _value;
