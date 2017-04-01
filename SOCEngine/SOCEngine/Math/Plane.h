fef9dd1e2117b64a9c0e0bbb81d761c8f5a09cfc
Jin Park <quaggan02@gmail.com>
2017-04-01 22:53:57 +0900

    Update Plane.h

--- SOCEngine/SOCEngine/Math/Plane.h
+++ SOCEngine/SOCEngine/Math/Plane.h
@@ -8,26 +8,21 @@ namespace Math
 	{
 	public:
 		enum class Direction { FRONT, BACK, SAME };
+		Plane(float a = 0.0f, float b = 0.0f, float c = 0.0f, float d = 0.0f);
 
-	public:
-		Plane();
-		Plane(float a, float b, float c, float d);
-
-	public:
 		const Plane		Normalized() const;
 		float			DistancePoint(const Vector3& p) const;
 		float			GetDistancePoint(const Vector3& v) const;
 		bool			SameSide(const Vector3& v) const;
 		Direction		GetSide(const Vector3& v) const;
 
-	public:
-		static void			FromPoints(Plane& out, const Vector3& v1, const Vector3& v2, const Vector3& v3);
-		static void			FromPointNormal(Plane& out, const Vector3& point, const Vector3& normal);
+		static void		FromPoints(Plane& out, const Vector3& v1, const Vector3& v2, const Vector3& v3);
+		static void		FromPointNormal(Plane& out, const Vector3& point, const Vector3& normal);
 		static float		DotCoord(const Plane& p, const Vector3& v);
 		static float		DotNoraml(const Plane& p, const Vector3& v);
-		static void			Normalize(Plane& out, const Plane& p);
+		static void		Normalize(Plane& out, const Plane& p);
 		static float		ComputeDistanceWithPoint(const Plane& p, const Vector3& v);
-		static bool			SameSide(const Plane& p, const Vector3& v);
+		static bool		SameSide(const Plane& p, const Vector3& v);
 		static Direction	GetSide(const Plane& p, const Vector3& v);
 
 	public:
