e72c64603c7cd90aa8efe14dea7c3496ef8cac76
Jin Park <quaggan02@gmail.com>
2017-04-01 22:15:18 +0900

    Update DXResource.h

--- SOCEngine/SOCEngine/Device/DXResource.h
+++ SOCEngine/SOCEngine/Device/DXResource.h
@@ -7,53 +7,21 @@ template <typename Resource>
 class DXResource final
 {
 public:
-	DXResource() : _resource(nullptr)
-	{
-	}
+	DXResource() : _resource(nullptr) { }
+	explicit DXResource(Resource* resource) : _resource(resource, Release) { }
 
-	explicit DXResource(Resource* resource)
-		: _resource(resource, Release)
-	{
-	}
 	static void Release(Resource* resource)
 	{
 		resource->Release();
 	}
 
-	inline const Resource* GetRaw() const
-	{
-		return _resource.get();
-	}
-
-	inline Resource* GetRaw()
-	{
-		return _resource.get();
-	}
-
-	inline const std::shared_ptr<Resource>& Get() const
-	{
-		return _resource;
-	}
-
-	inline std::shared_ptr<Resource>& Get()
-	{
-		return _resource;
-	}
-
-	bool IsCanUse() const
-	{
-		return _resource != nullptr;
-	}
-
-	void Destroy()
-	{
-		_resource.reset();
-	}
-
-	std::shared_ptr<Resource>& operator->()
-	{
-		return _resource;
-	}
+	inline const Resource* GetRaw() const			{ return _resource.get();	}
+	inline Resource* GetRaw() 				{ return _resource.get();	}
+	inline const std::shared_ptr<Resource>& Get() const	{ return _resource;		}
+	inline std::shared_ptr<Resource>& Get()			{ return _resource;		}
+	inline bool IsCanUse() const				{ return _resource != nullptr;	}
+	std::shared_ptr<Resource>& operator->()			{ return _resource;		}
+	void Destroy()						{ _resource.reset();		}
 
 private:
 	std::shared_ptr<Resource> _resource;
