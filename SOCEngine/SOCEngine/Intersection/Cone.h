dec7538d221873e0923ed73d13cac18f38fb7bdf
Jin Park <quaggan02@gmail.com>
2017-04-01 22:44:48 +0900

    Update Cone.h

--- SOCEngine/SOCEngine/Intersection/Cone.h
+++ SOCEngine/SOCEngine/Intersection/Cone.h
@@ -9,18 +9,17 @@ namespace Intersection
 	class Cone
 	{
 	public:
-		Cone(void);
+		Cone(void) = default;
 		Cone(float degree, float range, const Math::Vector3 &axis, const Math::Vector3 &vertex);
 
-	public:
 		static bool Intersects(const Sphere &sphere, const Cone &cone);
 		bool Intersects(const Sphere &sphere);
 
 	public:
-		float degree;
-		float range;
-		Math::Vector3 axis;
-		Math::Vector3 vertex;
+		float degree		= 0.0f;
+		float range		= 0.0f;
+		Math::Vector3 axis	= Math::Vector3(0.0f, 0.0f, 0.0f;
+		Math::Vector3 vertex	Math::Vector3(0.0f,;
 	};
 
 }
