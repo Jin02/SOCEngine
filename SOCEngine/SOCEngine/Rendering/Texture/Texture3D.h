1e57008ffcd7faebf63b2f87ebf095d7d15d9188
Jin Park <quaggan02@gmail.com>
2017-04-01 23:05:20 +0900

    Update Texture3D.h

--- SOCEngine/SOCEngine/Rendering/Texture/Texture3D.h
+++ SOCEngine/SOCEngine/Rendering/Texture/Texture3D.h
@@ -12,20 +12,20 @@ namespace Rendering
 			Texture3D() = default;
 
 			void Initialize(Device::DirectX& dx,
-							uint width, uint height, uint depth,
-							DXGI_FORMAT typelessFormat, DXGI_FORMAT srvFormat, DXGI_FORMAT uavFormat,
-							uint optionBindFlag, uint mipLevels);
-			void Destroy();
+					uint width, uint height, uint depth,
+					DXGI_FORMAT typelessFormat, DXGI_FORMAT srvFormat, DXGI_FORMAT uavFormat,
+					uint optionBindFlag, uint mipLevels);
 
-			GET_CONST_ACCESSOR(Texture,				const DXResource<ID3D11Texture3D>&,			_texture);
+			GET_CONST_ACCESSOR(Texture,		const DXResource<ID3D11Texture3D>&,		_texture);
 			GET_CONST_ACCESSOR(RenderTargetView,	const DXResource<ID3D11RenderTargetView>&,	_rtv);
-			GET_CONST_ACCESSOR(Size,				const Math::Vector3&,						_size);
+			GET_CONST_ACCESSOR(Size,		const Math::Vector3&,				_size);
 
 		private:
-			TextureGPUView							_base;
-			DXResource<ID3D11Texture3D>				_texture;
-			Math::Vector3							_size = Math::Vector3(0.0f, 0.0f, 0.0f);
+			DXResource<ID3D11Texture3D>			_texture;
+			Math::Vector3					_size = Math::Vector3(0.0f, 0.0f, 0.0f);
 			DXResource<ID3D11RenderTargetView>		_rtv;
+			View::ShaderResourceView			_srv;
+			View::UnorderedAccessView			_uav;
 		};
 	}
 }
