fbad9450e56da4dfbd152789c9071d3da4b1380a
Jin Park <quaggan02@gmail.com>
2017-04-01 22:52:02 +0900

    Update Quaternion.cpp

--- SOCEngine/SOCEngine/Math/Quaternion.cpp
+++ SOCEngine/SOCEngine/Math/Quaternion.cpp
@@ -7,11 +7,6 @@
 
 using namespace Math;
 
-Quaternion::Quaternion() 
-	: x(0.0f), y(0.0f), z(0.0f), w(0.0f)
-{
-}
-
 Quaternion::Quaternion(float _x, float _y, float _z, float _w)
 	: x(_x), y(_y), z(_z), w(_w)
 {
