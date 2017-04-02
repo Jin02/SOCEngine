fa68e5fd432ee47819b66a236d2025f3dbe44cc3
Jin Park <quaggan02@gmail.com>
2017-04-02 22:47:02 +0900

    Update VertexShader.h

--- SOCEngine/SOCEngine/Rendering/Shader/VertexShader.h
+++ SOCEngine/SOCEngine/Rendering/Shader/VertexShader.h
@@ -13,9 +13,9 @@ namespace Rendering
 		public:
 			struct SemanticInfo
 			{
-				std::string	name;
-				uint		semanticIndex;
-				uint		size;
+				std::string	name = "";
+				uint		semanticIndex = -1;
+				uint		size = 0;
 			};
 
 			VertexShader(const DXResource<ID3DBlob>& blob, const std::string& key);
@@ -29,20 +29,18 @@ namespace Rendering
 			void UnBindShaderToContext(Device::DirectX& dx);
 			void UnBindInputLayoutToContext(Device::DirectX& dx);
 
-			static void BindTexture(Device::DirectX& dx, TextureBindIndex bind, const Texture::TextureGPUView& tex);
+			static void BindShaderResourceView(Device::DirectX& dx, TextureBindIndex bind, const View::ShaderResourceView& view);
 			static void BindSamplerState(Device::DirectX& dx, SamplerStateBindIndex bind, DXResource<ID3D11SamplerState>& samplerState);
 			static void BindConstBuffer(Device::DirectX& dx, ConstBufferBindIndex bind, const Buffer::ConstBuffer& cb);
-			static void BindShaderResourceBuffer(Device::DirectX& dx, TextureBindIndex bind, const Buffer::ShaderResourceBuffer& srBuffer);
 
-			static void UnBindTexture(Device::DirectX& dx, TextureBindIndex bind);
+			static void UnBindShaderResourceView(Device::DirectX& dx, TextureBindIndex bind);
 			static void UnBindSamplerState(Device::DirectX& dx, SamplerStateBindIndex bind);
 			static void UnBindConstBuffer(Device::DirectX& dx, ConstBufferBindIndex bind);
-			static void UnBindShaderResourceBuffer(Device::DirectX& dx, TextureBindIndex bind);
 
 			static constexpr const char* GetCompileCode() { return "vs"; }
 
 		private:
-			BaseShader						_baseShader;
+			BaseShader			_baseShader;
 			DXResource<ID3D11VertexShader>	_shader;
 			DXResource<ID3D11InputLayout>	_layout;
 
