a088b0f46f179665b0285cb0835c9278ee6bd048
Jin Park <quaggan02@gmail.com>
2017-04-02 16:02:59 +0900

    Update PointLight.h

--- SOCEngine/SOCEngine/Rendering/Light/PointLight.h
+++ SOCEngine/SOCEngine/Rendering/Light/PointLight.h
@@ -11,12 +11,10 @@ namespace Rendering
 		{
 		public:
 			using TransformType = Math::Vector4;
-			struct Param {};
-
-			PointLight(Core::ObjectId);
+			PointLight(Core::ObjectId id) : _base(id) {};
 
 			bool Intersect(const Intersection::Sphere &sphere, const Core::Transform& transform) const;
-			void MakeTransform(TransformType& out, const Core::Transform& transform) const;
+			TransformType MakeTransform(const Core::Transform& transform) const;
 
 			GET_ACCESSOR(Base, BaseLight&, _base);
 			GET_CONST_ACCESSOR(ObjectId, const Core::ObjectId&, _base.GetObjectId());
