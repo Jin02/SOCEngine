58826bf98b71e6fe10eb4ba3f2e622a3622445e6
Jin Park <quaggan02@gmail.com>
2017-04-02 22:44:16 +0900

    Update PixelShader.h

--- SOCEngine/SOCEngine/Rendering/Shader/PixelShader.h
+++ SOCEngine/SOCEngine/Rendering/Shader/PixelShader.h
@@ -16,22 +16,20 @@ namespace Rendering
 			void BindShaderToContext(Device::DirectX& dx);
 			void UnBindShaderToContext(Device::DirectX& dx);
 			
-			static void BindTexture(Device::DirectX& dx,					TextureBindIndex bind,		const Texture::TextureGPUView& tex);
-			static void BindSamplerState(Device::DirectX& dx,				SamplerStateBindIndex bind,	DXResource<ID3D11SamplerState>& samplerState);
-			static void BindConstBuffer(Device::DirectX& dx,				ConstBufferBindIndex bind,	const Buffer::ConstBuffer& cb);
-			static void BindShaderResourceBuffer(Device::DirectX& dx,		TextureBindIndex bind,		const Buffer::ShaderResourceBuffer& srBuffer);
+			static void BindShaderResourceView(Device::DirectX& dx,	TextureBindIndex bind, const View::ShaderResourceView& view);
+			static void BindSamplerState(Device::DirectX& dx, SamplerStateBindIndex bind, DXResource<ID3D11SamplerState>& samplerState);
+			static void BindConstBuffer(Device::DirectX& dx, ConstBufferBindIndex bind, const Buffer::ConstBuffer& cb);
 
-			static void UnBindTexture(Device::DirectX& dx,				TextureBindIndex bind);
-			static void UnBindSamplerState(Device::DirectX& dx,			SamplerStateBindIndex bind);
-			static void UnBindConstBuffer(Device::DirectX& dx,			ConstBufferBindIndex bind);
-			static void UnBindShaderResourceBuffer(Device::DirectX& dx,	TextureBindIndex bind);
+			static void UnBindShaderResourceView(Device::DirectX& dx, TextureBindIndex bind);
+			static void UnBindSamplerState(Device::DirectX& dx, SamplerStateBindIndex bind);
+			static void UnBindConstBuffer(Device::DirectX& dx, ConstBufferBindIndex bind);
 
 			static constexpr const char* GetCompileCode() { return "ps"; }
 			GET_CONST_ACCESSOR(Key, const std::string&, _baseShader.GetKey());
 
 		private:
-			BaseShader						_baseShader;
-			DXResource<ID3D11PixelShader>	_shader;
+			BaseShader _baseShader;
+			DXResource<ID3D11PixelShader> _shader;
 		};
 	}
 }
