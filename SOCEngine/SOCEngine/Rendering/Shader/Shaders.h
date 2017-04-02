cde11fe5e63e64d4b966eaaa89d2d2fffd58aa93
Jin Park <quaggan02@gmail.com>
2017-04-02 23:24:53 +0900

    Update Shaders.h

--- SOCEngine/SOCEngine/Rendering/Shader/Shaders.h
+++ SOCEngine/SOCEngine/Rendering/Shader/Shaders.h
@@ -10,40 +10,20 @@ namespace Rendering
 {
 	namespace Shader
 	{
-		enum class ShaderUsage : uint
-		{
-			RenderScene,
-			DepthWrite,
-			AlphaTestWithDiffuse,
-			Num
-		};
-
 		struct ShaderGroup
 		{
-			std::shared_ptr<VertexShader>		vs;
-			std::shared_ptr<PixelShader>		ps;
-			std::shared_ptr<GeometryShader>		gs;
+			std::shared_ptr<VertexShader>		vs = nullptr;
+			std::shared_ptr<PixelShader>		ps = nullptr;
+			std::shared_ptr<GeometryShader>		gs = nullptr;
 
-			ShaderGroup() : vs(nullptr), ps(nullptr), gs(nullptr)
-			{
-
-			}
+			ShaderGroup() = default;
 			ShaderGroup(const std::shared_ptr<VertexShader>& _vs,
-						const std::shared_ptr<PixelShader>& _ps,
-						const std::shared_ptr<GeometryShader>& _gs)
-				: vs(_vs), ps(_ps), gs(_gs)
-			{
+				    const std::shared_ptr<PixelShader>& _ps,
+				    const std::shared_ptr<GeometryShader>& _gs)
+				: vs(_vs), ps(_ps), gs(_gs) { }
 
-			}
-
-			const bool ableRender() const
-			{
-				return (vs != nullptr) && (ps != nullptr);
-			}
-			const bool IsAllEmpty() const
-			{
-				return (!vs && !ps && !gs);
-			}
+			inline bool ableRender() const { return (vs != nullptr) & (ps != nullptr); }
+			inline bool IsAllEmpty() const { return (!vs & !ps & !gs); }
 		};
 
 		template <typename Resource>
@@ -52,10 +32,7 @@ namespace Rendering
 		public:
 			BindShaderData() = default;
 			BindShaderData(const Resource& _resource, uint _bindIndex, bool _useVS, bool _useGS, bool _usePS, bool _useCS)
-				: resource(_resource), useVS(_useVS), useGS(_useGS), usePS(_usePS), useCS(_useCS)
-			{
-
-			}
+				: resource(_resource), useVS(_useVS), useGS(_useGS), usePS(_usePS), useCS(_useCS) { }
 		public:
 			Resource resource;
 
