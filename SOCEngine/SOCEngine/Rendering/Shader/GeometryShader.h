302406db36f5dd712e3be942e5558bb7b189d231
Jin Park <quaggan02@gmail.com>
2017-04-02 22:49:40 +0900

    Update GeometryShader.h

--- SOCEngine/SOCEngine/Rendering/Shader/GeometryShader.h
+++ SOCEngine/SOCEngine/Rendering/Shader/GeometryShader.h
@@ -16,15 +16,13 @@ namespace Rendering
 			void BindShaderToContext(Device::DirectX& dx);
 			void UnBindShaderToContext(Device::DirectX& dx);
 			
-			static void BindTexture(Device::DirectX& dx, TextureBindIndex bind, const Texture::TextureGPUView& tex);
+			static void BindShaderResourceView(Device::DirectX& dx, TextureBindIndex bind, const View::ShaderResourceView& srv);
 			static void BindSamplerState(Device::DirectX& dx, SamplerStateBindIndex bind, DXResource<ID3D11SamplerState>& samplerState);
 			static void BindConstBuffer(Device::DirectX& dx, ConstBufferBindIndex bind, const Buffer::ConstBuffer& cb);
-			static void BindShaderResourceBuffer(Device::DirectX& dx, TextureBindIndex bind, const Buffer::ShaderResourceBuffer& srBuffer);
 
-			static void UnBindTexture(Device::DirectX& dx, TextureBindIndex bind);
+			static void UnBindShaderResourceView(Device::DirectX& dx, TextureBindIndex bind);
 			static void UnBindSamplerState(Device::DirectX& dx, SamplerStateBindIndex bind);
 			static void UnBindConstBuffer(Device::DirectX& dx, ConstBufferBindIndex bind);
-			static void UnBindShaderResourceBuffer(Device::DirectX& dx, TextureBindIndex bind);
 
 			static constexpr const char* GetCompileCode() { return "gs"; }
 			GET_CONST_ACCESSOR(Key, const std::string&, _baseShader.GetKey());
