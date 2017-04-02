df9237ffffad07f65d6cd78c9e75ce0cd172377b
Jin Park <quaggan02@gmail.com>
2017-04-02 12:24:07 +0900

    Update GPUUploadBuffer.hpp

--- SOCEngine/SOCEngine/Rendering/Buffer/GPUUploadBuffer.hpp
+++ SOCEngine/SOCEngine/Rendering/Buffer/GPUUploadBuffer.hpp
@@ -15,44 +15,34 @@ namespace Rendering
 		public:
 			GPUUploadBuffer() = default;
 
-		public:
 			void Initialize(Device::DirectX& dx, uint count, DXGI_FORMAT format, const void* dummy = nullptr)
 			{
-				Destroy();
-
-				_srBuffer.Destroy();
 				_srBuffer.Initialize(dx, sizeof(T), count, format, dummy, false, 0, D3D11_USAGE_DYNAMIC);
-			}
-
-			void Destroy()
-			{
-				_srBuffer.Destroy();
-				_base.DeleteAll();
 			}
 
 			// _srBuffer
 			void UpdateSRBuffer(Device::DirectX& dx)
 			{
-				uint count = _base.GetSize();
-				const void* raw = _base.GetVector().data();
+				uint count = _pool.GetSize();
+				const void* raw = _pool.GetVector().data();
 
 				_srBuffer.UpdateResourceUsingMapUnMap(dx, raw, count * sizeof(T));
 			}
 
 			// _buffer
-			void Add(const KeyType& key, const T& object)	{ _base.Add(key, object);		}
-			void Delete(const KeyType& key)					{ _base.Delete(key);			}
-			void DeleteAll()								{ _base.DeleteAll();			}
-			auto Find(const KeyType& key) const				{ return _base.Find(key);		}
-			auto Find(const KeyType& key)					{ return _base.Find(key);		}
-			bool Has(const KeyType& key) const				{ return _base.Has(key);		}
-			T& Get(uint index)								{ return _base.Get(index);	}
+			void Add(const KeyType& key, const T& object)	{ _pool.Add(key, object);		}
+			void Delete(const KeyType& key)					{ _pool.Delete(key);			}
+			void DeleteAll()								{ _pool.DeleteAll();			}
+			auto Find(const KeyType& key) const				{ return _pool.Find(key);		}
+			auto Find(const KeyType& key)					{ return _pool.Find(key);		}
+			bool Has(const KeyType& key) const				{ return _pool.Has(key);		}
+			T& Get(uint index)								{ return _pool.Get(index);	}
 
-			GET_CONST_ACCESSOR(Size, const uint, _base.GetSize());
+			GET_CONST_ACCESSOR(Size, const uint, _pool.GetSize());
 			GET_CONST_ACCESSOR(ShaderResourceBuffer, const ShaderResourceBuffer&, _srBuffer);
 
 		private:
-			Core::VectorHashMap<KeyType, T>			_base;
+			Core::VectorHashMap<KeyType, T>			_pool;
 			ShaderResourceBuffer					_srBuffer;
 		};
 	}
