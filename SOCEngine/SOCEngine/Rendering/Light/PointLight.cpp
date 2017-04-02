164913ee6823293c2c81d11dd41fc07024e584e1
Jin Park <quaggan02@gmail.com>
2017-04-02 16:02:54 +0900

    Update PointLight.cpp

--- SOCEngine/SOCEngine/Rendering/Light/PointLight.cpp
+++ SOCEngine/SOCEngine/Rendering/Light/PointLight.cpp
@@ -7,11 +7,6 @@ using namespace Rendering::Light;
 using namespace Math;
 using namespace Core;
 
-PointLight::PointLight(Core::ObjectId id)
-	:_base(id)
-{
-}
-
 bool PointLight::Intersect(const Sphere &sphere, const Transform& transform) const
 {
 	assert(transform.GetObjectId() == _base.GetObjectId());
@@ -20,10 +15,10 @@ bool PointLight::Intersect(const Sphere &sphere, const Transform& transform) con
 	return Sphere::Intersects(sphere, Sphere(wp, _base.GetRadius()));
 }
 
-void PointLight::MakeTransform(TransformType& outTransform, const Transform& transform) const
+TransformType PointLight::MakeTransform(const Transform& transform) const
 {
 	assert(transform.GetObjectId() == _base.GetObjectId());
 	Vector3 wp = transform.GetWorldPosition();
 
-	outTransform = Vector4(wp.x, wp.y, wp.z, _base.GetRadius());
+	return Vector4(wp.x, wp.y, wp.z, _base.GetRadius());
 }
