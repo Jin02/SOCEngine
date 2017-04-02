f79da8095e0f85547100961b96da370a918fe3a2
Jin Park <quaggan02@gmail.com>
2017-04-02 22:26:37 +0900

    Update and rename ShaderMacro.h to ShaderMacro.hpp

---
+++ SOCEngine/SOCEngine/Rendering/Shader/ShaderMacro.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Common.h"
+#include <d3d11.h>
+#include <string>
+
+namespace Rendering
+{
+	namespace Shader
+	{
+		class ShaderMacro
+		{
+		public:
+			ShaderMacro() = default;
+			ShaderMacro(const std::string& name, const std::string& definition) : _name(name), _definition(definition) {}
+
+			GET_SET_ACCESSOR(Name,		const std::string&,	_name);
+			GET_SET_ACCESSOR(Definition,	const std::string&,	_definition);
+
+		private:
+			std::string		_name		= "";
+			std::string		_definition	= "";
+		};
+	}
+}
