4fa56cb1549e7428f122807a77972bd49bbdb35b
Jin <quaggan02@gmail.com>
2017-05-14 11:37:06 +0900

    최적화

--- SOCEngine/SOCEngine/Device/DXResource.h
+++ SOCEngine/SOCEngine/Device/DXResource.h
@@ -16,8 +16,8 @@ public:
 	inline Resource* GetRaw() 							{ return _resource.get();		}
 	inline const std::shared_ptr<Resource>& Get() const	{ return _resource;				}
 	inline std::shared_ptr<Resource>& Get()				{ return _resource;				}
-	std::shared_ptr<Resource>& operator->()				{ return _resource;				}
-	const std::shared_ptr<Resource>& operator->() const	{ return _resource;				}
+	Resource* operator->()								{ return _resource.get();		}
+	const Resource* operator->() const					{ return _resource.get();		}
 	inline bool IsCanUse() const						{ return _resource != nullptr;	}
 	void Destroy()										{ _resource.reset();			}
 
