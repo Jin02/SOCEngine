924511246dc3b201161106358f6a4d2f5f543d70
Jin <quaggan02@gmail.com>
2017-05-14 10:53:44 +0900

    Object - 간단하게 몇몇 기능 구현, AddComponent, AddChild 등등

--- SOCEngine/SOCEngine/Core/Object.hpp
+++ SOCEngine/SOCEngine/Core/Object.hpp
@@ -3,71 +3,85 @@
 #include "ObjectId.hpp"
 #include <memory>
 #include <string>
-#include "Engine.h"
+#include <cassert>
+
+#include "MainSystemHeader.h"
 
 namespace Core
 {
 	class Object final
 	{
 	public:
-		Object(ObjectId id, Engine* engine)
-			: _id(id), _engine(engine)
+		Object(ObjectId id, DefaultComponentSystem& dcs, TransformPool& tfp)
+			: _id(id), _componentSystem(&dcs), _transformPool(&tfp)
 		{
+			_transformPool->Add(id);
+		}
 
+		~Object()
+		{
+			_transformPool->Delete(_id);
 		}
 
 		/* Child */
 		void AddChild(Object& child)
 		{
+			ObjectId childId = child.GetId();
+			auto found = _transformPool->GetIndexer().Has(childId);
+			assert(found);
+
+			_transformPool->Find(_id)->AddChild(childId);
 		}
 
-		void HasChild(Object& child)
+		bool HasChild(const Object& child) const
 		{
-
+			return _transformPool->Find(_id)->HasChild(child.GetId());
 		}
 
 		void DeleteChild(Object& child)
 		{
-
+			_transformPool->Find(_id)->DeleteChild(child.GetId());
 		}
 
-		void FindChild(Object& child)
+		auto FindChild(Object& child)
 		{
-
+			return HasChild(child) ? _transformPool->Find(child.GetId()) : nullptr;
 		}
 
-		void GetChild(uint index)
+		auto GetChild(uint index)
 		{
-
+			ObjectId id = _transformPool->Find(_id)->GetChild(index);			
+			return _transformPool->Find(id);
 		}
-
 
 		/* Component */
 		template <class Component>
 		Component& AddComponent()
 		{
-			return _engine->GetComponentSystem().Add<Component>(_id);
+			return _componentSystem->Add<Component>(_id);
 		}
 
 		template <class Component>
 		void DeleteComponet()
 		{
-			_engine->GetComponentSystem().Delete<Component>(_id);
+			_componentSystem->Delete<Component>(_id);
 		}
 
 		template <class Component>
 		bool HasComponent() const
 		{
-			return _engine->GetComponentSystem().Has<Component>(_id);
+			return _componentSystem->Has<Component>(_id);
 		}
 
 	public:
 		GET_SET_ACCESSOR(Name, const std::string&, _name);
-		GET_CONST_ACCESSOR(Id, const ObjectId&, _id);
+		GET_CONST_ACCESSOR(Id, ObjectId, _id);
 
 	private:
 		ObjectId				_id;
 		std::string				_name = "";
-		Engine*					_engine;
+
+		DefaultComponentSystem*	_componentSystem;
+		TransformPool*			_transformPool;
 	};
 }
