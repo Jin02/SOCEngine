48b16700372c4e9864782b39fe4f904e27efa488
Jin <quaggan02@gmail.com>
2017-06-14 04:43:43 +0900

    LightId - LightIdManager 코드 정리

--- SOCEngine/SOCEngine/Rendering/Light/LightId.hpp
+++ SOCEngine/SOCEngine/Rendering/Light/LightId.hpp
@@ -10,30 +10,12 @@ namespace Rendering
 		class LightId : public Core::UniqueId
 		{
 		public:
-			using UniqueId::UniqueId;
+			using Core::UniqueId::UniqueId;
 
 		public:
 			friend class LightIdManager;
 		};
 
-		class LightIdManager : public Core::UniqueIdManager
-		{
-		public:
-			LightId Acquire()
-			{
-				uint literalId = UniqueIdManager::Acquire();
-				return LightId(literalId);
-			}
-
-			bool Has(LightId id)
-			{
-				return UniqueIdManager::Has(id.Literal());
-			}
-
-			void Delete(LightId id)
-			{
-				UniqueIdManager::Delete(id.Literal());
-			}
-		};
+		class LightIdManager final : public Core::IdManagerForm<LightId> {};
 	}
 }
