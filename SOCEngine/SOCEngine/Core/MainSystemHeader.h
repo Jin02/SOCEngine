b2fdede3fd5d1aa10a5f785c3d7f4115d5b2ee8a
Jin <quaggan02@gmail.com>
2017-07-01 16:44:23 +0900

    delete MainSYstemHeader

--- SOCEngine/SOCEngine/Core/MainSystemHeader.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "ComponentSystem.hpp"
-#include "Transform.h"
-#include "MeshManager.hpp"
-
-using DefaultComponentSystem = Core::ComponentSystem<Rendering::Geometry::Mesh>;
