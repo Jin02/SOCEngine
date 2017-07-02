dc469497b7c5910079e7f0a7f580116dc513d8c6
Jin Park <quaggan02@gmail.com>
2017-07-02 18:21:58 +0900

    Update Transform.cpp

--- SOCEngine/SOCEngine/Core/Transform.cpp
+++ SOCEngine/SOCEngine/Core/Transform.cpp
@@ -190,8 +190,8 @@ void Transform::_ComputeWorldMatrix(TransformPool& pool)
 	{
 		auto child = pool.Find(childId.Literal());
 		assert(child);
-
-		child->_worldMat = child->ComputeLocalMatrix() * _worldMat;
+				// parent   <-   child
+		child->_worldMat = _worldMat * child->ComputeLocalMatrix();
 		child->_ComputeWorldMatrix(pool);
 	}
 }
