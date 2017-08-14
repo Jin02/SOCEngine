dc527ee4af2ff8fd6ca6942235767798ea8e8358
Jin <quaggan02@gmail.com>
2017-08-14 21:12:00 +0900

    ComponentSystem - Shadow 추가

--- SOCEngine/SOCEngine/Core/ComponentSystem.hpp
+++ SOCEngine/SOCEngine/Core/ComponentSystem.hpp
@@ -4,6 +4,8 @@
 #include "MeshManager.hpp"
 #include "LightManager.h"
 #include "CameraManager.h"
+#include "ShadowManager.h"
+#include <assert.h>
 
 namespace Core
 {
@@ -13,11 +15,19 @@ namespace Core
 		using Mesh = Rendering::Geometry::Mesh;
 		using MainCamera = Rendering::Camera::MainCamera;
 
+		using DL		= Rendering::Light::DirectionalLight;
+		using DLShadow	= Rendering::Shadow::DirectionalLightShadow;
+		using PL		= Rendering::Light::PointLight;
+		using PLShadow	= Rendering::Shadow::PointLightShadow;
+		using SL		= Rendering::Light::SpotLight;
+		using SLShadow	= Rendering::Shadow::SpotLightShadow;
+
 	public:
 		ComponentSystem() = default;
 
 		DISALLOW_ASSIGN(ComponentSystem);
 		DISALLOW_COPY_CONSTRUCTOR(ComponentSystem);
+
 
 #pragma region MultiTypeComponents
 		template <class Component>
@@ -45,7 +55,64 @@ namespace Core
 			auto& mgr = GetManager<Component>();
 			return mgr.Find<Component>(id);
 		}
-#pragma endregion Light,Etc..
+#pragma endregion Light, Shadow, Etc..
+
+#pragma region Light
+		template <> void Delete<DL>(ObjectId id)
+		{
+			auto& shadowMgr = GetManager<DL::ShadowType>();
+			if (shadowMgr.Has<DL::ShadowType>(id))
+				shadowMgr.Delete<DL::ShadowType>(id);
+
+			auto& lightMgr = GetManager<DL>();
+			lightMgr.Delete<DL>(id);
+		}
+		template <> void Delete<PL>(ObjectId id)
+		{
+			auto& shadowMgr = GetManager<PL::ShadowType>();
+			if (shadowMgr.Has<PL::ShadowType>(id))
+				shadowMgr.Delete<PL::ShadowType>(id);
+
+			auto& lightMgr = GetManager<PL>();
+			lightMgr.Delete<PL>(id);
+		}
+		template <> void Delete<SL>(ObjectId id)
+		{
+			auto& shadowMgr = GetManager<SL::ShadowType>();
+			if (shadowMgr.Has<SL::ShadowType>(id))
+				shadowMgr.Delete<SL::ShadowType>(id);
+
+			auto& lightMgr = GetManager<SL>();
+			lightMgr.Delete<SL>(id);
+		}
+#pragma endregion Light
+
+#pragma region Shadow
+		template <> DLShadow& Add<DLShadow>(ObjectId id)
+		{
+			auto& lightMgr = GetManager<DLShadow::LightType>();
+			assert(lightMgr.Has<DLShadow::LightType>(id));
+
+			auto& shadowMgr = GetManager<DLShadow>();
+			return shadowMgr.Acquire<DLShadow>(id);
+		}
+		template <> PLShadow& Add<PLShadow>(ObjectId id)
+		{
+			auto& lightMgr = GetManager<PLShadow::LightType>();
+			assert(lightMgr.Has<PLShadow::LightType>(id));
+
+			auto& shadowMgr = GetManager<PLShadow>();
+			return shadowMgr.Acquire<PLShadow>(id);
+		}
+		template <> SLShadow& Add<SLShadow>(ObjectId id)
+		{
+			auto& lightMgr = GetManager<SLShadow::LightType>();
+			assert(lightMgr.Has<SLShadow::LightType>(id));
+
+			auto& shadowMgr = GetManager<SLShadow>();
+			return shadowMgr.Acquire<SLShadow>(id);
+		}
+#pragma endregion Shadow
 
 #pragma region Mesh
 		template <> Mesh& Add<Mesh>(ObjectId id)
@@ -115,6 +182,7 @@ namespace Core
 	private:
 		std::tuple<	Rendering::Manager::MeshManager,
 					Rendering::Manager::LightManager,
-					Rendering::Manager::CameraManager	>	_componentMgrs;
+					Rendering::Manager::CameraManager,
+					Rendering::Manager::ShadowManager >	_componentMgrs;
 	};
 }
