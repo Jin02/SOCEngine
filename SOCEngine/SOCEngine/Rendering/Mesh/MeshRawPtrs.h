c40b3cb5df0a814f0334103dee8d7b3355a5acae
Jin Park <quaggan02@gmail.com>
2017-10-07 09:36:20 +0900

    MeshRawPtrs - 코드 정리 및 non-const Find,Get 제거

--- SOCEngine/SOCEngine/Rendering/Mesh/MeshRawPtrs.h
+++ SOCEngine/SOCEngine/Rendering/Mesh/MeshRawPtrs.h
@@ -16,18 +16,12 @@ namespace Rendering
 			MeshRawPtrs() = default;
 			MeshRawPtrs(const std::initializer_list<PtrType>& meshes);
 
-			PtrType&	Add(PtrType& mesh)			{ _meshes.push_back(mesh);	return _meshes.back();	}
-			void		Delete(PtrType& mesh)		{ Delete(mesh->GetObjectID().Literal());			}
-			bool		Has(PtrType& mesh)	const	{ Has(mesh->GetObjectID().Literal());				}
+			PtrType		Add(PtrType mesh)			{ _meshes.push_back(mesh);	return _meshes.back();	}
+			void		Delete(PtrType mesh)		{ Delete(mesh->GetObjectID().Literal());			}
+			bool		Has(PtrType mesh)	const	{ Has(mesh->GetObjectID().Literal());				}
 
-			PtrType&		Get(uint index)			{ return _meshes[index]; }
-			const PtrType&	Get(uint index)	const	{ return _meshes[index]; }
-
-			const PtrType* Find(Core::ObjectID::LiteralType id) const;
-			PtrType* Find(Core::ObjectID::LiteralType id)
-			{
-				return const_cast<PtrType*>( static_cast<const MeshRawPtrs*>(this)->Find(id) );
-			}
+			const PtrType Get(uint index) const		{ return _meshes[index]; }
+			const PtrType Find(Core::ObjectID::LiteralType id) const;
 
 			void Delete(Core::ObjectID::LiteralType id);
 			bool Has(Core::ObjectID::LiteralType id) const;
