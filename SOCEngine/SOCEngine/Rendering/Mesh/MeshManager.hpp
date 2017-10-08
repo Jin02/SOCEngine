1fe3eefe2d46abdccbbb6339da30728e022e55d7
Jin Park <quaggan02@gmail.com>
2017-10-08 15:15:43 +0900

    MeshManager.h - Ptr 타입 제거

--- SOCEngine/SOCEngine/Rendering/Mesh/MeshManager.hpp
+++ SOCEngine/SOCEngine/Rendering/Mesh/MeshManager.hpp
@@ -5,22 +5,18 @@
 #include "ObjectID.hpp"
 #include <tuple>
 #include <assert.h>
-#include "VBSortedMeshes.hpp"
+#include "VBSortedMeshpool.h"
+#include "OpaqueMeshRenderQueue.h"
 
 namespace Rendering
 {
 	namespace Geomtry
 	{
 		using TransparentMeshPool	= Core::VectorHashMap<Core::ObjectID::LiteralType, Mesh>;
-		using TransparentMeshPtrs	= std::vector<const Mesh*>;
-
-		using OpaqueMeshPool		= VBSortedMeshes<MeshRawPool>;
-		using OpaqueMeshPtrs		= VBSortedMeshes<MeshRawPtrs>;
-
+		using OpaqueMeshPool		= VBSortedMeshPool;
 		using AlphaBlendMeshPool	= OpaqueMeshPool;
-		using AlphaBlendMeshPtrs	= OpaqueMeshPtrs;		
 	};
-	
+		
 	namespace Manager
 	{
 		class MeshManager final
