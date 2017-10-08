83f573d5b8514abe052564a5a9423fdf87bb69c5
Jin Park <quaggan02@gmail.com>
2017-10-08 22:20:24 +0900

    Create MeshRenderQueue.h

---
+++ SOCEngine/SOCEngine/Rendering/Mesh/MeshRenderQueue.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "OpaqueMeshRenderQueue.h"
+
+namespace Rendering
+{
+	namespace RenderQueue
+	{
+		using TransparentMeshRenderQueue = std::vector<const Geometry::Mesh*>;
+		using AlphaBlendMeshRenderQueue = OpaqueMeshRenderQueue;		
+	}
+}
