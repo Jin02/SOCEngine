b7fcbcf0af855b13a0625be63a10b97b559900a7
Jin <quaggan02@gmail.com>
2017-10-29 15:43:29 +0900

    TemporaryPtr - IsNull 추가

--- SOCEngine/SOCEngine/Common/TemporaryPtr.hpp
+++ SOCEngine/SOCEngine/Common/TemporaryPtr.hpp
@@ -4,11 +4,14 @@ template <typename Type>
 class TemporaryPtr
 {
 public:
+	TemporaryPtr() = default;
 	explicit TemporaryPtr(Type* type) : _type(type) {	}
 
 	const Type* operator->() const	{ return _type; }
 	Type* operator->()				{ return _type; }
 
+	bool IsNull() const { return _type != nullptr; }
+
 private:
-	Type* _type;
+	Type* _type = nullptr;
 };
