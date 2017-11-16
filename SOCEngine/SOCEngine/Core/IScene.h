84f9df25b31dc4cfcf5ce8d4d7252d6b39372bef
Jin <quaggan02@gmail.com>
2017-11-16 16:20:52 +0900

    IScene - OnInitialize, OnDestroy, RegistRenderSetting 추가

--- SOCEngine/SOCEngine/Core/IScene.h
+++ SOCEngine/SOCEngine/Core/IScene.h
@@ -1,28 +1,35 @@
 #pragma once
 
-#include "Common.h"
+#include "RenderSetting.h"
+#include <string>
 
 namespace Core
 {
+	class Engine;
+
 	class IScene
 	{
 	public:
-		DISALLOW_ASSIGN(IScene);
-		DISALLOW_COPY_CONSTRUCTOR(IScene);
+		virtual void OnRenderPreview()								= 0;
+		virtual void OnUpdate()										= 0;
+		virtual void OnRenderPost()									= 0;
+		virtual void OnInput()										= 0;
 
-	public:
-		virtual void OnRenderPreview()		= 0;
-		virtual void OnUpdate()				= 0;
-		virtual void OnRenderPost()			= 0;
-		virtual void OnInput()				= 0;
+		virtual void OnInitialize(Engine& engine)					= 0;
+		virtual void OnDestroy(Engine& engine)						= 0;
+		virtual RenderSetting RegistRenderSetting(Engine& engine)	= 0;
 	};
 
 	class NullScene : public IScene
 	{
 	public:
-		virtual void OnRenderPreview()	override {/* NULL */ }
-		virtual void OnUpdate()			override {/* NULL */ }
-		virtual void OnRenderPost()		override {/* NULL */ }
-		virtual void OnInput()			override {/* NULL */ }
+		virtual void OnRenderPreview()								override {/* NULL */}
+		virtual void OnUpdate()										override {/* NULL */}
+		virtual void OnRenderPost()									override {/* NULL */}
+		virtual void OnInput()										override {/* NULL */}
+
+		virtual void OnInitialize(Engine& engine)					override {/* NULL */}
+		virtual void OnDestroy(Engine& engine)						override {/* NULL */}
+		virtual RenderSetting RegistRenderSetting(Engine& engine)	override { return RenderSetting(); }
 	};
 }
