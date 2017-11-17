2266155352badfa5525c26e5320c6ca44d040b66
Jin <quaggan02@gmail.com>
2017-11-17 09:51:20 +0900

    CoreConnector - cantUseTFPool 추가

--- SOCEngine/SOCEngine/Core/CoreConnector.h
+++ SOCEngine/SOCEngine/Core/CoreConnector.h
@@ -15,20 +15,24 @@ namespace Core
 		friend class Object;
 		using Singleton<CoreConnector>::SharedInstance;
 
-		void Connect(TransformPool* tf, ObjectManager* obj, ComponentSystem* cs)
+		void Connect(TransformPool* tf,  TransformPool* cantUseTF, ObjectManager* obj, ComponentSystem* cs)
 		{
 			_tfPool			= tf;
+			_cantUseTFPool	= cantUseTF;
 			_objMgr			= obj;
 			_compoSystem	= cs;
 		}
 
 	private:
-		GET_ACCESSOR(TransformPool,		auto,	_tfPool);
-		GET_ACCESSOR(ObjectManager,		auto,	_objMgr);
-		GET_ACCESSOR(ComponentSystem,	auto,	_compoSystem);
+		GET_ACCESSOR(TransformPool,			auto,	_tfPool);
+		GET_ACCESSOR(CantUseTransformPool,	auto,	_cantUseTFPool);
+		GET_ACCESSOR(ObjectManager,			auto,	_objMgr);
+		GET_ACCESSOR(ComponentSystem,		auto,	_compoSystem);
 
 	private:
 		TransformPool*			_tfPool			= nullptr;
+		TransformPool*			_cantUseTFPool	= nullptr;
+
 		ObjectManager*			_objMgr			= nullptr;
 		ComponentSystem*		_compoSystem	= nullptr;
 	};
