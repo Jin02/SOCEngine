dc227c1d4dcb8a6e9795e4b81840ab4aaf99422f
Jin <quaggan02@gmail.com>
2017-12-10 15:07:26 +0900

    BasicGeometryGenerator - 모두 static함수로 변경

--- SOCEngine/SOCEngine/Rendering/Mesh/BasicGeometryGenerator.h
+++ SOCEngine/SOCEngine/Rendering/Mesh/BasicGeometryGenerator.h
@@ -19,17 +19,15 @@ namespace Rendering
 		class BasicGeometryGenerator
 		{
 		public:
-			BasicGeometryGenerator() = default;
-
-			void CreateBox(std::function<void(const Mesh::CreateFuncArguments&)> createMeshCallback,		const Math::Vector3& size, uint defautVertexInputTypeFlag);
-			void CreateSphere(std::function<void(const Mesh::CreateFuncArguments&)> createMeshCallback,		float radius, uint sliceCount, uint stackCount, uint defautVertexInputTypeFlag);
-			void CreateCylinder(std::function<void(const Mesh::CreateFuncArguments&)> createMeshCallback,	float botRadius, float topRadius, float height, uint sliceCount, uint stackCount, uint defautVertexInputTypeFlag);
-			void CreatePlane(std::function<void(const Mesh::CreateFuncArguments&)> createMeshCallback,		float width, float height, uint widthVertexCount, uint heightVertexCount, uint defautVertexInputTypeFlag);
-
-			void CreateBox(Core::Object& targetObj, Core::Engine& engine, const Math::Vector3& size, uint defautVertexInputTypeFlag);
-			void CreateSphere(Core::Object& targetObj, Core::Engine& engine, float radius, uint sliceCount, uint stackCount, uint defautVertexInputTypeFlag);
-			void CreateCylinder(Core::Object& targetObj, Core::Engine& engine, float botRadius, float topRadius, float height, uint sliceCount, uint stackCount, uint defautVertexInputTypeFlag);
-			void CreatePlane(Core::Object& targetObj, Core::Engine& engine, float width, float height, uint widthVertexCount, uint heightVertexCount, uint defautVertexInputTypeFlag);
+			static void CreateBox(std::function<void(const Mesh::CreateFuncArguments&)> createMeshCallback,		const Math::Vector3& size, uint defautVertexInputTypeFlag);
+			static void CreateSphere(std::function<void(const Mesh::CreateFuncArguments&)> createMeshCallback,		float radius, uint sliceCount, uint stackCount, uint defautVertexInputTypeFlag);
+			static void CreateCylinder(std::function<void(const Mesh::CreateFuncArguments&)> createMeshCallback,	float botRadius, float topRadius, float height, uint sliceCount, uint stackCount, uint defautVertexInputTypeFlag);
+			static void CreatePlane(std::function<void(const Mesh::CreateFuncArguments&)> createMeshCallback,		float width, float height, uint widthVertexCount, uint heightVertexCount, uint defautVertexInputTypeFlag);
+		
+			static void CreateBox(Core::Object& targetObj, Core::Engine& engine, const Math::Vector3& size, uint defautVertexInputTypeFlag);
+			static void CreateSphere(Core::Object& targetObj, Core::Engine& engine, float radius, uint sliceCount, uint stackCount, uint defautVertexInputTypeFlag);
+			static void CreateCylinder(Core::Object& targetObj, Core::Engine& engine, float botRadius, float topRadius, float height, uint sliceCount, uint stackCount, uint defautVertexInputTypeFlag);
+			static void CreatePlane(Core::Object& targetObj, Core::Engine& engine, float width, float height, uint widthVertexCount, uint heightVertexCount, uint defautVertexInputTypeFlag);
 
 		private:
 			struct MeshInfo
@@ -37,12 +35,12 @@ namespace Rendering
 				uint											stride;
 				std::vector<Shader::VertexShader::SemanticInfo>	semantics;
 			};
-			std::string UintToStr(uint data);
-			void AppendVertexData(std::vector<float>& inoutVertexDatas,
+			static std::string UintToStr(uint data);
+			static void AppendVertexData(std::vector<float>& inoutVertexDatas,
 				const Math::Vector3& pos, const Math::Vector3& normal,
 				const Math::Vector3& tangent, const Math::Vector2& uv, uint flag);
-			bool HasFlag(uint vtxInputTypeFlag, DefaultVertexInputTypeFlag flag);
-			void MakeMeshInfo(MeshInfo& outInfo, uint vtxInputTypeFlag);
+			static bool HasFlag(uint vtxInputTypeFlag, DefaultVertexInputTypeFlag flag);
+			static void MakeMeshInfo(MeshInfo& outInfo, uint vtxInputTypeFlag);
 		};
 	}
 }
