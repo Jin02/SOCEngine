5e0336dcdc05ab11b6777ddd7e1ed3199fcd3fe0
Jin <quaggan02@gmail.com>
2017-12-13 01:24:43 +0900

    test

--- SOCTestProject/SOCTestProject/Code/TestScene.cpp
+++ SOCTestProject/SOCTestProject/Code/TestScene.cpp
@@ -1,4 +1,5 @@
 #include "TestScene.h"
+#include "BasicGeometryGenerator.h"
 
 using namespace Core;
 using namespace Math;
@@ -6,12 +7,13 @@ using namespace Rendering;
 using namespace Rendering::Camera;
 using namespace Rendering::Light;
 using namespace Rendering::Shadow;
+using namespace Rendering::Geometry;
 
 RenderSetting TestScene::RegistRenderSetting(Engine& engine)
 {
 	return RenderSetting("MainCamera",
 		engine.GetDirectX().GetBackBufferRect().Cast<uint>(),
-		256, 256, 15.0f
+		128, 256, 15.0f
 	);
 }
 
@@ -23,14 +25,17 @@ void TestScene::OnInitialize(Engine& engine)
 	bool hasMainCam = mainCamObj->HasComponent<MainCamera>();
 	assert(hasMainCam); // ????
 
-	Object* box = engine.LoadMesh("./Resources/CornellBox/box.obj");
-	assert(box);
+//	Object sphere = engine.GetObjectManager().Add("Sphere");
+//	BasicGeometryGenerator::CreateBox(sphere, engine, Vector3(1.0f, 1.0f, 1.0f), uint(DefaultVertexInputTypeFlag::UV0) | uint(DefaultVertexInputTypeFlag::NORMAL));
+//	engine.AddRootObject(sphere);
+
+	Object* box = engine.LoadMesh("./Resources/CornellBox/box.obj"); assert(box);
+	engine.AddRootObject(*box);
 
 	Transform& tf = box->FetchTransform();
 	tf.UpdatePosition(Vector3(0.3f, -4.7f, 17.7f));
 	tf.UpdateEulerAngle(Vector3(-90.0f, 0.0f, 180.0f));
 	tf.UpdateScale(Vector3(5.0f, 5.0f, 5.0f));
-	engine.AddRootObject(*box);
 
 	Object& light = engine.GetObjectManager().Add("PointLight");
 	light.FetchTransform().UpdatePosition(Vector3(0.0f, 2.0f, 16.0f));
