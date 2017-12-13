de943f999ec0f94b2205833bad8284ef5ad5201a
Jin <quaggan02@gmail.com>
2017-12-13 20:27:42 +0900

    NormalMapping - NormalMapping -> UnpackNormal

--- SOCEngine/SOCEngine/ShaderCodes/NormalMapping.h
+++ SOCEngine/SOCEngine/ShaderCodes/NormalMapping.h
@@ -3,14 +3,14 @@
 #ifndef __SOC_NORMAL_MAPPING_H__
 #define __SOC_NORMAL_MAPPING_H__
 
-float3 NormalMapping(float3 normalMapXYZ, float3 normal, float3 tangent)
+float3 UnpackNormal(float3 normalMapXYZ, float3 normal, float3 tangent)
 {
 	float3 binormal = normalize( cross(normal, tangent) );
 
 	float3x3 TBN = float3x3(normalize(tangent), normalize(binormal), normalize(normal));
 	TBN = transpose(TBN);
 
-	float3 tangentNormal = normalize(normalMapXYZ * 2 - 1);
+	float3 tangentNormal = normalize(normalMapXYZ * 2.0f - 1.0f);
 	return mul(TBN, tangentNormal);
 }
 
