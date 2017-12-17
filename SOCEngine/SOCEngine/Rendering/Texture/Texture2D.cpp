f758735eb4b8dd6f8095e6ac99881e9445ad7579
Jin <quaggan02@gmail.com>
2017-12-17 13:27:31 +0900

    Texture2D - 초기화시 srv가 설정되지 않던 문제 수정

--- SOCEngine/SOCEngine/Rendering/Texture/Texture2D.cpp
+++ SOCEngine/SOCEngine/Rendering/Texture/Texture2D.cpp
@@ -6,7 +6,7 @@ using namespace Rendering::Texture;
 using namespace Rendering::View;
 
 Texture2D::Texture2D(const ShaderResourceView& srv, const DXSharedResource<ID3D11Texture2D>& tex, bool hasAlpha, const Size<uint>& size)
-	: _texture(tex), _size(size)
+	: _srv(srv), _texture(tex), _size(size)
 {
 }
 
