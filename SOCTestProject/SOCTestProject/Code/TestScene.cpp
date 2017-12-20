38e5fa9c7ec06e0c1d69eb2585730dc4f7df435a
Jin <quaggan02@gmail.com>
2017-12-21 04:38:04 +0900

    test

--- SOCTestProject/SOCTestProject/Code/TestScene.cpp
+++ SOCTestProject/SOCTestProject/Code/TestScene.cpp
@@ -1,6 +1,9 @@
 #include "TestScene.h"
 #include "BasicGeometryGenerator.h"
 
+//#define GI_TEST
+#define SKYBOX_ON
+
 using namespace Core;
 using namespace Math;
 using namespace Rendering;
@@ -11,23 +14,23 @@ using namespace Rendering::Geometry;
 
 RenderSetting TestScene::RegistRenderSetting(Engine& engine)
 {
-	engine.GetRenderingSystem().GetPostProcessPipeline().SetUseDoF(true);
-	engine.GetRenderingSystem().GetPostProcessPipeline().SetUseSSAO(true);
+//	engine.GetRenderingSystem().GetPostProcessPipeline().SetUseDoF(true);
+//	engine.GetRenderingSystem().GetPostProcessPipeline().SetUseSSAO(true);
 
 	GIInitParam param;
 	{
+#ifdef GI_TEST
 		param.vxgiParam = GIInitParam::VXGIParam(256, 15.0f);
+#else
+		param.vxgiParam = GIInitParam::VXGIParam(0, 0);
+#endif
 	}
 	return RenderSetting("MainCamera", engine.GetDirectX().GetBackBufferRect().Cast<uint>(), 512, param);
 }
 
 void TestScene::OnInitialize(Engine& engine)
 {
-	Object* mainCamObj = engine.GetObjectManager().Find("MainCamera");
-	assert(mainCamObj);
-	MainCamera* mainCam = mainCamObj->GetComponent<MainCamera>();
-	MainCamera& mainCam2 = engine.GetComponentSystem().GetMainCamera();
-
+#ifdef SKYBOX_ON
 	// Load SkyBox
 	{
 		auto cubeMap = engine.GetRenderingSystem().GetTexture2DManager().LoadTextureFromFile(engine.GetDirectX(), "Resources/CubeMap/desertcube1024.dds", false);
@@ -37,14 +40,12 @@ void TestScene::OnInitialize(Engine& engine)
 		skyMat.UpdateCubeMap(*cubeMap);
 		skyMat.Initialize(engine.GetDirectX(), engine.GetRenderingSystem().GetShaderManager());
 
-		auto key = engine.GetRenderingSystem().GetMaterialManager().Add(skyMat.GetName(), skyMat).first;
-		mainCam2.SetSkyBoxMaterialID(key);
+		auto key = engine.GetRenderingSystem().GetMaterialManager().Add(skyMat).first;
+		engine.GetObjectManager().Find("MainCamera")->GetComponent<MainCamera>()->SetSkyBoxMaterialID(key);
 	}
+#endif
 
-//	Object sphere = engine.GetObjectManager().Add("Sphere");
-//	BasicGeometryGenerator::CreateBox(sphere, engine, Vector3(1.0f, 1.0f, 1.0f), uint(DefaultVertexInputTypeFlag::UV0) | uint(DefaultVertexInputTypeFlag::NORMAL));
-//	engine.AddRootObject(sphere);
-
+#ifdef GI_TEST
 	Object* box = engine.LoadMesh("./Resources/CornellBox/box.obj"); assert(box);
 	engine.AddRootObject(*box);
 
@@ -53,7 +54,7 @@ void TestScene::OnInitialize(Engine& engine)
 	tf.UpdateEulerAngle(Vector3(-90.0f, 0.0f, 180.0f));
 	tf.SetLocalScale(Vector3(5.0f, 5.0f, 5.0f));
 
-	Object& light = engine.GetObjectManager().Add("PointLight");
+	Object& light = engine.GetObjectManager().Acquire("PointLight");
 	light.FetchTransform().SetLocalPosition(Vector3(0.0f, 2.0f, 16.0f));
 
 	PointLight& pl = light.AddComponent<PointLight>();
@@ -64,10 +65,53 @@ void TestScene::OnInitialize(Engine& engine)
 	pls.GetBase()->SetUnderScanSize( Half(1.0f) );
 
 	engine.AddRootObject(light);
+
 	engine.UpdateWorldMatrix();
 
 	auto v = box->FetchTransform().GetWorldPosition() + Vector3(0, 5, 0);
 	engine.GetRenderingSystem().GetMainRenderer().GetGlobalIllumination().SetVXGI_VoxelizeCenterPos(v);
+#else
+	Object* house = engine.LoadMesh("./Resources/House/SanFranciscoHouse.fbx"); assert(house);
+	{
+		engine.AddRootObject(*house);
+	
+		Transform& tf = house->FetchTransform();
+		tf.SetLocalPosition(Vector3(0, -5, 20));
+		tf.UpdateEulerAngle(Vector3(-90, -90, 0));
+	}
+
+	Object plane = engine.GetObjectManager().Acquire("Plane");
+	{
+		constexpr uint flag =	uint(DefaultVertexInputTypeFlag::UV0) |
+								uint(DefaultVertexInputTypeFlag::NORMAL);
+		BasicGeometryGenerator::CreatePlane(plane, engine, 20.0f, 20.0f, 4, 4, flag);
+
+		engine.AddRootObject(plane);
+
+		plane.FetchTransform().SetLocalPosition(Vector3(0, -5, 20));
+		plane.FetchTransform().UpdateEulerAngle(Vector3(0, 0, 0));
+
+		Material::PhysicallyBasedMaterial pbr("PlaneMaterial");
+		pbr.Initialize(engine.GetDirectX());
+		auto key = engine.GetRenderingSystem().GetMaterialManager().Add(pbr).first;
+		auto mesh = plane.GetComponent<Mesh>();
+		mesh->SetPBRMaterialID(key);
+	}
+
+	Object& light = engine.GetObjectManager().Acquire("Light");
+	{
+		light.FetchTransform().SetLocalPosition(Vector3(0, 0, 0));
+		light.FetchTransform().UpdateEulerAngle(Vector3(120.0f, 30.0f, 0.0f));
+	
+		light.AddComponent<DirectionalLight>().GetBase()->SetIntensity(20.0f);
+		light.AddComponent<DirectionalLightShadow>();
+	
+		engine.AddRootObject(light);
+	}
+
+	auto mainCamObj = engine.GetObjectManager().Find("MainCamera");
+	mainCamObj->FetchTransform().SetLocalPosition(Vector3(0,0,-5));
+#endif
 }
 
 void TestScene::OnDestroy(Engine&)
