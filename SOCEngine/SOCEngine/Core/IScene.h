fc19f1750089d4e01d1c97240513051d54097394
Jin <quaggan02@gmail.com>
2017-12-21 21:48:38 +0900

    IScene - 모든 On 관련 함수 인자에 Engine& 추가

--- SOCEngine/SOCEngine/Core/IScene.h
+++ SOCEngine/SOCEngine/Core/IScene.h
@@ -10,26 +10,26 @@ namespace Core
 	class IScene
 	{
 	public:
-		virtual void OnRenderPreview()								= 0;
-		virtual void OnUpdate()										= 0;
-		virtual void OnRenderPost()									= 0;
-		virtual void OnInput()										= 0;
+		virtual void OnRenderPreview(Engine&)				= 0;
+		virtual void OnUpdate(Engine&)						= 0;
+		virtual void OnRenderPost(Engine&)					= 0;
+		virtual void OnInput(Engine&)						= 0;
 
-		virtual void OnInitialize(Engine& engine)					= 0;
-		virtual void OnDestroy(Engine& engine)						= 0;
-		virtual RenderSetting RegistRenderSetting(Engine& engine)	= 0;
+		virtual void OnInitialize(Engine&)					= 0;
+		virtual void OnDestroy(Engine&)						= 0;
+		virtual RenderSetting RegistRenderSetting(Engine&)	= 0;
 	};
 
 	class NullScene : public IScene
 	{
 	public:
-		virtual void OnRenderPreview()								override {/* NULL */}
-		virtual void OnUpdate()										override {/* NULL */}
-		virtual void OnRenderPost()									override {/* NULL */}
-		virtual void OnInput()										override {/* NULL */}
+		virtual void OnRenderPreview(Engine&)						override {/* NULL */}
+		virtual void OnUpdate(Engine&)								override {/* NULL */}
+		virtual void OnRenderPost(Engine&)							override {/* NULL */}
+		virtual void OnInput(Engine&)								override {/* NULL */}
 
-		virtual void OnInitialize(Engine& engine)					override {/* NULL */}
-		virtual void OnDestroy(Engine& engine)						override {/* NULL */}
-		virtual RenderSetting RegistRenderSetting(Engine& engine)	override { return RenderSetting(); }
+		virtual void OnInitialize(Engine&)							override {/* NULL */}
+		virtual void OnDestroy(Engine&)								override {/* NULL */}
+		virtual RenderSetting RegistRenderSetting(Engine&)			override { return RenderSetting(); }
 	};
 }
