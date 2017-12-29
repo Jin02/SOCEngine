c5ba5d6df7707a6cf452c8b823f699cf98f8dd93
Jin <quaggan02@gmail.com>
2017-12-29 12:38:26 +0900

    ShadowAtlasMap - ShadowAtlasMap<DirectionalLight> 추가, 여기서 viewDepthMap도 기록하도록 함

--- SOCEngine/SOCEngine/Rendering/Shadow/ShadowAtlasMap.h
+++ SOCEngine/SOCEngine/Rendering/Shadow/ShadowAtlasMap.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "DepthMap.h"
+#include "RenderTexture.h"
 
 namespace Rendering
 {
@@ -32,5 +33,47 @@ namespace Rendering
 			uint					_resolution		= 0;
 			uint					_capacity		= 0;
 		};
+
+		class DirectionalLightShadow;
+		template <> class ShadowAtlasMap<DirectionalLightShadow> : public Texture::DepthMap
+		{
+		public:
+			using Parent = Texture::DepthMap;
+			using Parent::Parent;
+
+			void Initialize(Device::DirectX& dx, const Size<uint>& size,
+				uint resolution, uint capacity)
+			{
+				Parent::Initialize(dx, size, true, 1);
+
+				_viewDepthMap.Initialize(dx, size, DXGI_FORMAT_R32_FLOAT, DXGI_FORMAT_R32_FLOAT, DXGI_FORMAT_UNKNOWN, 0, 1, 1);
+
+				_resolution	= resolution;
+				_capacity	= capacity;
+			}
+
+			void Destroy()
+			{
+				Parent::Destroy();
+				_viewDepthMap.Destroy();
+
+				_resolution = _capacity = 0;
+			}
+
+			GET_CONST_ACCESSOR(Resolution,	uint,	_resolution);
+			GET_CONST_ACCESSOR(Capacity,	uint,	_capacity);
+
+		private:
+			using Parent::Initialize;
+			using Parent::Destroy;
+
+		private:
+			Texture::RenderTexture	_viewDepthMap;
+
+			uint					_resolution		= 0;
+			uint					_capacity		= 0;
+		};
+
+
 	}
 }
