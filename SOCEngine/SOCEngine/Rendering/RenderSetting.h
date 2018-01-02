035bf39a3d832998029077437888ee49d617b0dd
Jin <quaggan02@gmail.com>
2018-01-03 01:23:41 +0900

    RenderSetting - useBloom 추가

--- SOCEngine/SOCEngine/Rendering/RenderSetting.h
+++ SOCEngine/SOCEngine/Rendering/RenderSetting.h
@@ -12,10 +12,11 @@ struct RenderSetting
 	Rect<uint>			renderRect			= Rect<uint>(0, 0, 0, 0);
 	uint				shadowMapResolution	= 1024;
 	GIInitParam			giParam;
+	bool				useBloom			= false;
 
 	RenderSetting() = default;
-	RenderSetting(const std::string& _mainCamName, const Rect<uint>& _renderRect, uint _shadowMapResolution, const GIInitParam& _giParam)
-		: mainCamName(_mainCamName), renderRect(_renderRect), shadowMapResolution(_shadowMapResolution), giParam(_giParam)
+	RenderSetting(const std::string& _mainCamName, const Rect<uint>& _renderRect, uint _shadowMapResolution, const GIInitParam& _giParam, bool _useBloom)
+		: mainCamName(_mainCamName), renderRect(_renderRect), shadowMapResolution(_shadowMapResolution), giParam(_giParam), useBloom(_useBloom)
 	{
 	}
 };
