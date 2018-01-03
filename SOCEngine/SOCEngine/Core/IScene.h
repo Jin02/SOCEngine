75868aa8f0b10f8101555f0c5b606c82b170933a
Jin <quaggan02@gmail.com>
2018-01-04 00:36:01 +0900

    IScene - EngineUtility도 같이 넘겨줌

--- SOCEngine/SOCEngine/Core/IScene.h
+++ SOCEngine/SOCEngine/Core/IScene.h
@@ -6,30 +6,31 @@
 namespace Core
 {
 	class Engine;
+	class EngineUtility;
 
 	class IScene
 	{
 	public:
-		virtual void OnRenderPreview(Engine&)				= 0;
-		virtual void OnUpdate(Engine&)						= 0;
-		virtual void OnRenderPost(Engine&)					= 0;
-		virtual void OnInput(Engine&)						= 0;
+		virtual void OnRenderPreview(Engine&, EngineUtility&)				= 0;
+		virtual void OnUpdate(Engine&, EngineUtility&)						= 0;
+		virtual void OnRenderPost(Engine&, EngineUtility&)					= 0;
+		virtual void OnInput(Engine&, EngineUtility&)						= 0;
 
-		virtual void OnInitialize(Engine&)					= 0;
-		virtual void OnDestroy(Engine&)						= 0;
-		virtual RenderSetting RegistRenderSetting(Engine&)	= 0;
+		virtual void OnInitialize(Engine&, EngineUtility&)					= 0;
+		virtual void OnDestroy(Engine&, EngineUtility&)						= 0;
+		virtual RenderSetting RegistRenderSetting(Engine&, EngineUtility&)	= 0;
 	};
 
 	class NullScene : public IScene
 	{
 	public:
-		virtual void OnRenderPreview(Engine&)						override {/* NULL */}
-		virtual void OnUpdate(Engine&)								override {/* NULL */}
-		virtual void OnRenderPost(Engine&)							override {/* NULL */}
-		virtual void OnInput(Engine&)								override {/* NULL */}
+		virtual void OnRenderPreview(Engine&, EngineUtility&)						override {/* NULL */}
+		virtual void OnUpdate(Engine&, EngineUtility&)								override {/* NULL */}
+		virtual void OnRenderPost(Engine&, EngineUtility&)							override {/* NULL */}
+		virtual void OnInput(Engine&, EngineUtility&)								override {/* NULL */}
 
-		virtual void OnInitialize(Engine&)							override {/* NULL */}
-		virtual void OnDestroy(Engine&)								override {/* NULL */}
-		virtual RenderSetting RegistRenderSetting(Engine&)			override { return RenderSetting(); }
+		virtual void OnInitialize(Engine&, EngineUtility&)							override {/* NULL */}
+		virtual void OnDestroy(Engine&, EngineUtility&)								override {/* NULL */}
+		virtual RenderSetting RegistRenderSetting(Engine&, EngineUtility&)			override { return RenderSetting(); }
 	};
 }
