5a3a828dc649cd77795c6c21f5f4a68332475f81
Jin Park <quaggan02@gmail.com>
2018-04-17 15:11:29 +0900

    MeshRenderer - RenderOpaqueMeshes에서 불필요한 IASetBuffer 호출 수정

--- SOCEngine/SOCEngine/Rendering/Renderer/MeshRenderer.h
+++ SOCEngine/SOCEngine/Rendering/Renderer/MeshRenderer.h
@@ -25,7 +25,7 @@ namespace Rendering
 			static void RenderWithoutIASetVB(Device::DirectX& dx, Param param, DefaultRenderType renderType, const Geometry::Mesh& mesh);
 
 			template <class CallPreRender, class CallPostRender>
-			static void RenderTransparentMeshes(Device::DirectX& dx, Param param, DefaultRenderType renderType, const Rendering::RenderQueue::TransparentMeshRenderQueue& meshes, CallPreRender func, CallPostRender postFunc)
+			static void RenderTransparentMeshes(Device::DirectX& dx, Param param, DefaultRenderType renderType, const Rendering::RenderQueue::TransparentMeshRenderQueue& meshes, CallPreRender preCall, CallPostRender postCall)
 			{
 				const auto& vbPool			= param.bufferMgr.GetPool<Buffer::VertexBuffer>();
 
@@ -34,36 +34,39 @@ namespace Rendering
 					const auto* vertexBuffer	= vbPool.Find(meshPtr->GetVBKey()); assert(vertexBuffer);
 
 					vertexBuffer->IASetBuffer(dx);
-					func(meshPtr);
 
+					preCall(meshPtr);
 					RenderWithoutIASetVB(dx, param, renderType, *meshPtr);
+					postCall();
 				}
-
-				postFunc();
 			}
 
 			template <class CallPreRender, class CallPostRender>
 			static void RenderOpaqueMeshes(Device::DirectX& dx, Param param, DefaultRenderType renderType, const Rendering::RenderQueue::OpaqueMeshRenderQueue& meshes, CallPreRender preRenderCall, CallPostRender postCall)
 			{
-				meshes.Iterate(
-					[&dx, &param, renderType, &preRenderCall, &postCall,
-					 &vbPool = param.bufferMgr.GetPool<Buffer::VertexBuffer>()](const Mesh* mesh)
+				auto& vbPool				= param.bufferMgr.GetPool<Buffer::VertexBuffer>();
+				const VertexBuffer* prevVB	= nullptr;				
+				for (const auto meshPtr : meshes)
+				{
+					const VertexBuffer* curVB = vbPool.Find(mesh->GetVBKey());
+					assert(curVB);
+					
+					if(curVB != prevVB)
 					{
-						const auto* vb		= vbPool.Find(mesh->GetVBKey()); assert(vb);
-
-						vb->IASetBuffer(dx);
-						preRenderCall(mesh);
-
-						RenderWithoutIASetVB(dx, param, renderType, *mesh);
-						postCall();
+						prevVB = curVB;
+						curVB->IASetBuffer(dx);
 					}
-				);
+		
+					preRenderCall(mesh);
+					RenderWithoutIASetVB(dx, param, renderType, *mesh);
+					postCall();
+				}
 			}
 
 			template <class CallPreRender, class CallPostRender>
-			static void RenderAlphaTestMeshes(Device::DirectX& dx, Param param, DefaultRenderType renderType, const Rendering::RenderQueue::AlphaTestMeshRenderQueue& meshes, CallPreRender&& cullFunc, CallPostRender&& postCall)
+			static void RenderAlphaTestMeshes(Device::DirectX& dx, Param param, DefaultRenderType renderType, const Rendering::RenderQueue::AlphaTestMeshRenderQueue& meshes, CallPreRender&& preCall, CallPostRender&& postCall)
 			{
-				RenderOpaqueMeshes(dx, param, renderType, meshes, cullFunc, postCall);
+				RenderOpaqueMeshes(dx, param, renderType, meshes, preCall, postCall);
 			}
 		};
 	}
