7de3905ec56a7af9f0cd2b2ce203f8c912622f3f
Jin02 <jin0x44e@gmail.com>
2018-07-22 21:03:39 +0900

    HDRCommon - GetTimeDelta() -> GetDeltaTime으로 변경 및 GetInvDeltaTime() 추가

--- SOCEngine/SOCEngine/ShaderCodes/EyeAdaptation.hlsl
+++ SOCEngine/SOCEngine/ShaderCodes/EyeAdaptation.hlsl
@@ -14,7 +14,7 @@ float EyeAdaptation_InFullScreen_PS(PS_INPUT input) : SV_Target
 	float curLum	= Luminance(CurColorMap.SampleLevel(LinearSampler, float2(0.5f, 0.5f), 11).rgb);
 	float prevLum	= PrevAdaptedLuminanceMap.Load(uint3(0, 0, 0)).x;
 
-	float result	= prevLum + (curLum - prevLum) * (1.0f - exp(-GetTimeDelta() * GetExposureSpeed()));
+	float result	= prevLum + (curLum - prevLum) * (1.0f - exp(-GetDeltaTime() * GetExposureSpeed()));
 
 	return result;
 }
